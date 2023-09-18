40b0c140ce0939dd3ecf3e3e82c2d8809418032f
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-09-17 22:42:38 -0300

    Criando a estrutura inicial do projeto #3

---
+++ academic_dev_flow/academic_dev_flow/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for academic_dev_flow project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academic_dev_flow.settings')
+
+application = get_wsgi_application()
