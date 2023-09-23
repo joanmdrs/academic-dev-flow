5611a34c5b29f04485784e9de22f39d6e32f273e
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-09-23 16:14:16 -0300

    organizando as pastas do projeto #4

--- academic_dev_flow/settings.py
+++ academic_dev_flow/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r+wj9^cv0h)-jq!z_b^n#lo3bm*=r@c^p(ivm_e&sxqnp3u=!z'
+SECRET_KEY = 'django-insecure-ug($fm-0lbb+48r4y=q63#byfced4uzi_jjax3_g$3g@62)uji'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,7 +83,6 @@ DATABASES = {
         'PORT': '',           # Deixe em branco para usar a porta padrão (5432)
     }
 }
-
 
 
 # Password validation
