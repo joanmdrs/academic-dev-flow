12dba6b7058c9244e4966b9b0533897642dba61d
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-09-23 21:45:51 -0300

    excluindo o app projeto #5

--- projeto/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProjetoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'projeto'
--- projeto/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Projeto
-
-class ProjetoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Projeto
-        fields = '__all__'  # Inclui todos os campos do modelo
