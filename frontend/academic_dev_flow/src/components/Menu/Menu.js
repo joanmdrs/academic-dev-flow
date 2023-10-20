a3a34e9dcd82258ba2861fe571eb2e13ffd07099
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-10-20 18:46:53 -0300

    adicionando items ao componente Menu #12

--- frontend/academic_dev_flow/src/components/Menu/Menu.js
+++ frontend/academic_dev_flow/src/components/Menu/Menu.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './Menu.css'; 
-import icon from "../../assets/icone-projeto.png"
+import icon_projeto from "../../assets/icone-projeto.png"
+import icon_fluxo from "../../assets/icone-fluxo.png"
 
 const Menu = () => {
   return (
     <div className="menu">
         <div className='item-menu'>
-            <img src={icon} alt='Ícone de Projeto'/>
+            <img src={icon_projeto} alt='Ícone de Projeto'/>
+
             <a href="/projetos">Projetos</a>
         </div>
+        <div className='item-menu'>
+            <img src={icon_fluxo} alt='Ícone de Projeto'/>
+
+            <a href="/fluxos"> Fluxos</a>
+        </div>
+        
       
     </div>
   );
