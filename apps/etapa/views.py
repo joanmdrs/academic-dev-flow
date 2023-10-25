7f4f0d7609f4bd0d2412773d31042f66c6b67c7d
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-10-25 08:05:24 -0300

    implementando a view cadastrar etapa #12

--- apps/etapa/views.py
+++ apps/etapa/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from .models import Etapa
+from .serializers import EtapaSerializer
 
-# Create your views here.
+class CadastrarEtapaView(APIView):
+    def post(self, request):
+        serializer = EtapaSerializer(Etapa, data=request.data);
+        
+        if(serializer.is_valid(raise_exception=True)):
+            serializer.save()
+
