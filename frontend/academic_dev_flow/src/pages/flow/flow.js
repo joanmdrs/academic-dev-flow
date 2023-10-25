9dbc68a648ec184337bbd3d70e5c45f8e3357a8e
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-10-25 07:48:53 -0300

    Criando o componente principal de fluxo #12

--- frontend/academic_dev_flow/src/pages/flow/flow.js
+++ frontend/academic_dev_flow/src/pages/flow/flow.js
@@ -1,14 +1,21 @@
 import React from "react";
+import "./flow.css";
+import Title from "../../components/Title/Title";
+import SearchFlow from "../../components/Flow/SearchFlow/SearchFlow";
+import TemplateFlow from "../../components/Flow/TemplateFlow/TemplateFlow";
 
 const Flow = () => {
 
     return (
-        <>
+        <div className="flow">
         
-            <h1>
-                Fluxos de desenvolvimento
-            </h1>
-        </>
+            <Title 
+                title="Fluxos de Desenvolvimento" 
+                paragraph=""
+            />
+            <SearchFlow />
+            <TemplateFlow />
+        </div>
     )
 
 }
