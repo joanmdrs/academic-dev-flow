0d3ea26386887367a6faf517b91d38463520ab8c
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-10-25 07:49:22 -0300

    chamando a rota de fluxo #12

--- frontend/academic_dev_flow/src/routes.js
+++ frontend/academic_dev_flow/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes as Switch, Route} from 'react-router-dom';
 import Projeto from './pages/projeto/projeto';
 import Flow from './pages/flow/flow';
+import AddFlow from './pages/flow/addFlow';
 
 function Routes() {
   return (
@@ -9,6 +10,7 @@ function Routes() {
         <Switch>
             <Route Component={Projeto} path='/projetos' exact/>
             <Route Component={Flow} path='/fluxos' exact/>
+            <Route Component={AddFlow} path='/fluxos/novo' exact/>
         </Switch>
         
     </BrowserRouter>
