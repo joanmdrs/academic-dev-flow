a7433804a8ec83c53be6f9ded2499d1d139733bc
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-10-25 08:05:09 -0300

    implementando a view cadastrar fluxo #12

--- apps/flow/views.py
+++ apps/flow/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from .models import Flow
+from .serializers import FlowSerializer
+from apps.etapa.views import CadastrarEtapaView 
 
-# Create your views here.
+class CadastrarFluxoView(APIView):
+    def post(self, request):
+        serializer = FlowSerializer(data=request.data)
+        
+        if(serializer.is_valid(raise_exception=True)):
+            serializer.save()
+            CadastrarEtapaView.as_view()
+            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
