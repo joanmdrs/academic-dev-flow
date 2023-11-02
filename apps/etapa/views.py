28776963e282aa45397e06c29f962b9e2d1c84d9
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-11-02 20:34:30 -0300

    criando a view de cadastrar etapa #12

--- apps/etapa/views.py
+++ apps/etapa/views.py
@@ -8,8 +8,24 @@ from .serializers import EtapaSerializer
 
 class CadastrarEtapaView(APIView):
     def post(self, request):
-        serializer = EtapaSerializer(Etapa, data=request.data);
-        
+        data = request.data
+        flow_id = data.get('flow_id')  
+        flow = Flow.objects.get(pk=flow_id)  
+
+        etapa_data = {
+            'nome': data['nome'],
+            'descricao': data['descricao'],
+            'data_inicio': data['data_inicio'],
+            'data_fim': data['data_fim'],
+            'status': data['status'],
+            'flow': flow  
+        }
+
+        serializer = EtapaSerializer(data=etapa_data)
         if(serializer.is_valid(raise_exception=True)):
             serializer.save()
+           
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
