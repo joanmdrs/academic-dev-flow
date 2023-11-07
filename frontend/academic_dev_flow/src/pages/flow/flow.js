95568a211ecb64948088eb232a9c0a2ee2bfcabd
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-11-07 19:07:36 -0300

    chamando as funções de buscar e repassando os dados para os respectivos componentes #12

--- frontend/academic_dev_flow/src/pages/flow/flow.js
+++ frontend/academic_dev_flow/src/pages/flow/flow.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./flow.css";
 import Title from "../../components/Title/Title";
 import SearchFlow from "../../components/Flow/SearchFlow/SearchFlow";
 import TemplateFlow from "../../components/Flow/TemplateFlow/TemplateFlow";
+import { buscar_fluxo } from "../../services/flow_service";
 
 const Flow = () => {
+
+    const [fluxos, setFluxos] = useState([])
+
+    const handleSearchFlow = async (query) => {
+        const response = await buscar_fluxo(query);
+        setFluxos(response.data)
+    }
 
     return (
         <div className="flow">
@@ -13,8 +21,8 @@ const Flow = () => {
                 title="Fluxos de Desenvolvimento" 
                 paragraph=""
             />
-            <SearchFlow />
-            <TemplateFlow />
+            <SearchFlow buscar_fluxos = {handleSearchFlow} />
+            <TemplateFlow fluxos={fluxos}/>
         </div>
     )
 
