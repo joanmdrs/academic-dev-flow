311b5a1f7742527a3aeef2e3b1f072f65b19a94f
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-11-07 19:05:20 -0300

    chamando as funções de busca no componente de buscar fluxo #12

--- frontend/academic_dev_flow/src/components/Flow/SearchFlow/SearchFlow.js
+++ frontend/academic_dev_flow/src/components/Flow/SearchFlow/SearchFlow.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchFlow.css";
 import { Input } from 'antd';
 const { Search } = Input;
 
-const SearchFlow = () => {
+const SearchFlow = ({buscar_fluxos}) => {
+
+    const [query, setQuery] = useState('');
+
+    const handleQueryChange = (event) => {
+        setQuery(event.target.value);
+        console.log(query)
+    };
+
     return (
         <div className="search-flow">
-            <Search className="input-search-flow" placeholder="Digite aqui" enterButton="Buscar" size="large" />
+            <Search 
+                className="input-search-flow" 
+                placeholder="Digite aqui" enterButton="Buscar" 
+                size="large" 
+                onChange={handleQueryChange}
+                onSearch= { async () => {
+                    await buscar_fluxos(query)
+                }}
+            />
         </div>
     )
 }
