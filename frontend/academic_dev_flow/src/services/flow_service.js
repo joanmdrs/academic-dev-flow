bb84822d73c10255c557eb354dbb13914b6c830d
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-11-07 19:04:25 -0300

    implementando o service de buscar fluxo #12

--- frontend/academic_dev_flow/src/services/flow_service.js
+++ frontend/academic_dev_flow/src/services/flow_service.js
@@ -8,3 +8,12 @@ export const cadastrar_fluxo = (data) => {
         console.log("Algo deu errado !", error);
     }
 }
+
+export const buscar_fluxo = async (query) => {
+    try {
+        const response = await api.get(`fluxos/buscar/?name_flow=${encodeURIComponent(query)}`)
+        return response;
+    } catch (error) {
+        console.log("Erro ao buscar dados:", error);
+    }
+}
