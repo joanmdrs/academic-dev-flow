41d9618ecf88df2001e7b60230f02ebdbc5bfcf3
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-11-16 20:41:56 -0300

    finalizando a listagem dos detalhes do fluxo e suas etapas #12

--- frontend/academic_dev_flow/src/components/Flow/ViewFlow/ViewFlow.js
+++ frontend/academic_dev_flow/src/components/Flow/ViewFlow/ViewFlow.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./ViewFlow.css";
 import { useParams } from 'react-router-dom';
 import { buscar_fluxo_pelo_id } from "../../../services/flow_service";
-import { Descriptions, Spin } from "antd";
+import { Badge, Descriptions, Spin } from "antd";
 import { buscar_etapas_por_id_fluxo } from "../../../services/etapa_service";
 
 const ViewFlow = () => {
     const { id } = useParams();
     const [fluxo, setFluxo] = useState({});
-    const {etapas, setEtapas} = useState([]);
+    const [etapas, setEtapas] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
@@ -20,7 +20,7 @@ const ViewFlow = () => {
 
                 setFluxo(dados_fluxos.data);
                 setEtapas(dados_etapas.data);
-
+                console.log(dados_etapas.data)
                 
                 
             } catch (error) {
@@ -52,14 +52,25 @@ const ViewFlow = () => {
     return (
         <div className="view-flow-content">
 
-            <div className="flow-details">
-                <Descriptions title="Detalhes do fluxo">
-                    <Descriptions.Item label="Nome do fluxo">{fluxo.nome}</Descriptions.Item>
+            <div>
+                <h4>Detalhes do fluxo</h4>
+                <Descriptions bordered className="flow-content">
+                    <Descriptions.Item  label="Nome" span={3}>{fluxo.nome}</Descriptions.Item>
                     <Descriptions.Item label="Descrição">{fluxo.descricao}</Descriptions.Item>
                 </Descriptions>
             </div>
             <div className="etapa-details">
-
+                
+                <h4>Detalhamento das etapas</h4>
+                    {etapas.map(etapa => (
+                       <Descriptions bordered className="etapa-content">
+                            <Descriptions.Item span={3} label="Fase">{etapa.nome}</Descriptions.Item>
+                            <Descriptions.Item label="Início">{etapa.data_inicio}</Descriptions.Item>
+                            <Descriptions.Item label="Fim">{etapa.data_fim}</Descriptions.Item>
+                            <Descriptions.Item label="Status">{etapa.status}</Descriptions.Item>
+                            <Descriptions.Item label="Descrição" span={3}>{etapa.descricao}</Descriptions.Item>
+                       </Descriptions>
+                    ))}    
             </div>
 
         </div>
