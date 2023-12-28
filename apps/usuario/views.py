6f5d5fb6fee07f552d5d9840f356062ae12c58bb
Joan Medeiros <joanmedeiros2018@gmail.com>
2023-12-28 16:55:29 -0300

    implementando o CRUD de views do app Usuario #18

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import HttpResponseNotAllowed, JsonResponse
+from .models import Usuario
+from .serializers import UsuarioSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class CadastrarUsuarioVie(APIView):
+    def post(self, request):
+        serializer = UsuarioSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class BuscarUsuarioPorIdView(APIView):
+    def get(self, request, id):
+        usuario = get_object_or_404(Usuario, id = id)
+        
+        if usuario is not None: 
+            serializer = UsuarioSerializer(usuario)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({"detail": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+    
+class ExcluirUsuarioView(APIView):
+    def delete(self, request, id):
+        usuario = get_object_or_404(Membro, id = id)
+        
+        if usuario is not None:
+            usuario.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT);
+        
+        else:
+            return Response({"detail": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+    
+class AtualizarUsuarioView(APIView):
+    def patch(self, request, id):
+        usuario = get_object_or_404(Usuario, id = id)
+        
+        if usuario is not None:
+            serializer = UsuarioSerializer(usuario, data=request.data)
+            
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else: 
+                return JsonResponse({'error': 'Dados inválidos'}, status=400)
+            
+        else: 
+            return Response({"detail": "Usuário não encontrado."}, status=status.HTTP_404_NOT_FOUND)
+            
+            
