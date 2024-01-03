7c3ea1cbd511ba3d73ec6a1afd67392a2acdd91b
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-03 16:52:33 -0300

    criando as funções de serviço do frontend do app membro #18

---
+++ frontend/academic_dev_flow/src/services/membro_service.js
@@ -0,0 +1,43 @@
+import api from "./api"
+
+export const criarMembro = async (data) => {
+    try {
+        let response = await api.post('membro/cadastrar/', {body: data})
+        return response
+        
+    } catch (error) {
+        console.error('Erro ao criar um membro:', error)
+        throw new Error('Falha ao criar um membro. Por favor, tente novamente.')   
+    }
+}
+
+export const buscarMembroPeloNome = async (query) => {
+    try {
+        let response = await api.get(`membro/buscar/?name=${encodeURIComponent(query)}`)
+        return response 
+
+    } catch (error) {
+        console.error('Erro ao buscar o membro pelo nome:', error)
+        throw new Error('Falha ao buscar um membro. Por favor, tente novamente.')
+    }
+}
+
+export const excluirMembro = async (id) => {
+    try {
+        let response = await api.delete(`membro/excluir/${encodeURIComponent(id)}`)
+        return response
+    } catch (error) {
+        console.error('Erro ao excluir o membro', error)
+        throw new Error('Falha ao excluir um membro. Por favor, tente novamente.')
+    }
+}
+
+export const atualizarMembro = async (id) => {
+    try {
+        let response = await api.patch(`membro/atualizar/${encodeURIComponent(id)}`)
+        return response
+    } catch (error) {
+        console.error('Erro ao atualizar o membro', error)
+        throw new Error('Falha ao atualizar um membro. Por favor, tente novamente.')
+    }
+}
