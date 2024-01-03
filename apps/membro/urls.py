d5cebad885fc501f34009222c047ab973a974c9b
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-03 14:05:31 -0300

    criando as rotas de membro e adicionando no arquivo de rotas principal do projeto #18

---
+++ apps/membro/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('membros/cadastrar/', CadastrarMembroView.as_view(), name='cadastrar_membro'),
+    path('membros/buscar/', BuscarMembrosPorNomeView.as_view(), name='buscar_membro'),
+    path('membros/<int:id>/excluir/', ExcluirMembroView.as_view(), name='excluir_membro'),
+    path('membros/<int:id>/atualizar/', AtualizarMembroView.as_view(), name='atualizar_membro'),
+    
+]
