28317c84542a0b007a99ec7bdf6ba6be1ddcbf57
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-04 14:21:40 -0300

    modificando a url de criar usuário #18

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -6,8 +6,8 @@ from .views import *
 app_name = 'usuario'
 
 urlpatterns = [
-    path('cadastrar', CadastrarUsuarioView.as_view(), name='cadastrar_usuario'),
-    path('buscar/<int:id>', BuscarUsuarioPorIdView.as_view(), name='buscar_usuario_pelo_id'),
-    path('excluir/<int:id>', ExcluirUsuarioView.as_view(), name='excluir_usuario'),
-    path('atualizar/<int:id>', AtualizarUsuarioView.as_view(), name='atualizar_usuario'),
+    path('cadastrar/', CadastrarUsuarioView.as_view(), name='cadastrar_usuario'),
+    path('buscar/<int:id>/', BuscarUsuarioPorIdView.as_view(), name='buscar_usuario_pelo_id'),
+    path('excluir/<int:id>/', ExcluirUsuarioView.as_view(), name='excluir_usuario'),
+    path('atualizar/<int:id>/', AtualizarUsuarioView.as_view(), name='atualizar_usuario'),
 ]
