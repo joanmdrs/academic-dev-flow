bf680254b7a493547b8cac72950cd08639441fbc
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-04 15:22:29 -0300

    modificando o nome do campo membro do model usuario #18

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -8,7 +8,7 @@ class Usuario(models.Model):
     senha = models.CharField(max_length=100)
     grupo = models.CharField(max_length=50)
     data_criacao = models.DateTimeField(auto_now_add=True)
-    membro = models.OneToOneField(Membro, on_delete=models.CASCADE, related_name='usuario', null=True, blank=True)
+    membro_id = models.OneToOneField(Membro, on_delete=models.CASCADE, related_name='usuario', null=True, blank=True)
 
     def __str__(self):
         return self.usuario
