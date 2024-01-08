06cc07f4aef02b738315a0b7e63a3ed96f3486fd
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-08 15:44:45 -0300

    feat: criando o cabeçalho da página principal do app artefato #25

--- frontend/academic_dev_flow/src/pages/artefato/PaginaArtefato.js
+++ frontend/academic_dev_flow/src/pages/artefato/PaginaArtefato.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { NotificationContainer } from "react-notifications";
+import Add from "../../components/Buttons/Add/Add";
+import Delete from "../../components/Buttons/Delete/Delete";
+import Search from "../../components/Search/Search";
+import Title from "../../components/Title/Title";
 
 const PaginaArtefato = () => {
 
 
     return (
-        <div> Página de Gerenciamento dos artefatos</div>
+        <div>
+            <NotificationContainer />
+            <Title 
+                title='Artefatos'
+                paragraph='Artefatos > Gerenciar artefatos'
+            />
+        </div>
 
     )
 }
