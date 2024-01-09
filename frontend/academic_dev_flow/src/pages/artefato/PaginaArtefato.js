6784889ac40b708a4102f1b9225a1ef40b748872
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-09 13:03:20 -0300

    feat: criando a página de cadastro dos artefatos e modificando a lógica de renderização dos componentes na página principal do app artefato #25

--- frontend/academic_dev_flow/src/pages/artefato/PaginaArtefato.js
+++ frontend/academic_dev_flow/src/pages/artefato/PaginaArtefato.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NotificationContainer } from "react-notifications";
 import Add from "../../components/Buttons/Add/Add";
 import Delete from "../../components/Buttons/Delete/Delete";
@@ -8,29 +8,75 @@ import FormBuscarArtefato from "../../components/Forms/FormBuscarArtefato/FormBu
 import BotaoAtualizar from "../../components/Buttons/BotaoAtualizar/BotaoAtualizar";
 import "./PaginaArtefato.css";
 import BotaoFiltrar from "../../components/Buttons/BotaoFiltrar/BotaoFiltrar";
+import ListaDeArtefatos from "../../components/Listas/ListaDeArtefatos/ListaDeArtefatos";
+import PaginaCadastrarArtefato from "./PaginaCadastrarArtefato";
 
 const PaginaArtefato = () => {
+
+    const [mostrarFormulario, setMostrarFormulario] = useState(false);
+
+    const colunas_tabela_artefatos = [
+        {
+            title: "Código",
+            key: "codigo",
+            dataIndex: "id",
+        },
+        {
+            title: "Nome",
+            dataIndex: "nome",
+            key: "nome",
+            render: (text, record) => (
+                <span
+                    style={{ color: "blue", cursor: "pointer" }}
+                >
+                {text}
+                </span>
+            ),
+        },
+        {
+            title: "Descrição",
+            dataIndex: "descricao",
+            key: "descricao",
+        },
+    ];
+
+    const mostrarFormularioAdicao = () => {
+        setMostrarFormulario(true);
+    };
+
 
 
     return (
         <div>
             <NotificationContainer />
-            <Title 
-                title='Artefatos'
-                paragraph='Artefatos > Gerenciar artefatos'
-            />
 
-            <div className="botoes-de-acao">
-                <div id="botao-filtrar"> 
-                    <BotaoFiltrar />
-                </div>
-                <div id="botao-adicionar-atualizar-deletar"> 
-                    <Add />
-                    <BotaoAtualizar />
-                    <Delete />
-                </div>
-            </div>
-            <FormBuscarArtefato />
+            {mostrarFormulario ? (
+
+                <PaginaCadastrarArtefato onCancel={() => setMostrarFormulario(false)}/>
+
+            ) : (
+                <>
+                    <Title 
+                        title='Artefatos'
+                        paragraph='Artefatos > Gerenciar artefatos'
+                    />
+
+                    <div className="botoes-de-acao">
+                        <div id="botao-filtrar"> 
+                            <BotaoFiltrar />
+                        </div>
+                        <div id="botao-adicionar-atualizar-deletar"> 
+                            <Add onClick={mostrarFormularioAdicao}/>
+                            <BotaoAtualizar />
+                            <Delete />
+                        </div>
+                    </div>
+                    <FormBuscarArtefato />
+
+                    <ListaDeArtefatos colunas={colunas_tabela_artefatos} />
+                </>
+            )}
+            
         </div>
 
     )
