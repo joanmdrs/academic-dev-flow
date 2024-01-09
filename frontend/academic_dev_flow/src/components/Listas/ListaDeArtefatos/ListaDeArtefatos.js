887968c5f90d0888ab97fb00fa1ece72dee4d7c5
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-09 16:13:37 -0300

    feat: aplicando uma função de clique nas linhas da tabela da listagem de artefatos #25

--- frontend/academic_dev_flow/src/components/Listas/ListaDeArtefatos/ListaDeArtefatos.js
+++ frontend/academic_dev_flow/src/components/Listas/ListaDeArtefatos/ListaDeArtefatos.js
@@ -2,19 +2,23 @@ import { Table } from "antd";
 import React, { useState } from "react";
 import "./ListaDeArtefatos.css";
 
-const ListaDeArtefatos = ({colunas, dados}) => {
+const ListaDeArtefatos = ({colunas, dados, onClickRow}) => {
 
     const [data, setData] = useState([])
 
     return (
         <div> 
-            {data.length == 0 ? (
+            {data.length === 0 ? (
                 <>
                     <Table 
                         className="tabela-lista-de-artefatos" 
                         dataSource={dados} 
                         columns={colunas} 
-                        rowKey="id"/>
+                        rowKey="id"
+                        onRow={(linha_dados) => ({
+                            onClick: () => onClickRow(linha_dados),
+                          })}
+                        />
                 </>
             ) : null}
         </div>
