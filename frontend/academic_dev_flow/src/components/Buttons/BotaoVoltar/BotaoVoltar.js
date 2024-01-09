702659150be77118448e1bdc4f09c2d1436e21b0
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-09 13:48:20 -0300

    style: estilizando o botão voltar #25

--- frontend/academic_dev_flow/src/components/Buttons/BotaoVoltar/BotaoVoltar.js
+++ frontend/academic_dev_flow/src/components/Buttons/BotaoVoltar/BotaoVoltar.js
@@ -1,15 +1,18 @@
 import { Button } from "antd";
 import React from "react";
 import { IoMdArrowBack } from "react-icons/io";
+import "./BotaoVoltar.css";
 
 const BotaoVoltar = ({onClick}) => {
 
     return (
         <Button 
+            id="botao-voltar"
             type="default" 
             size="large"
             icon={<IoMdArrowBack />}
-            onClick={onClick}     
+            onClick={onClick}
+            
         >
         </Button>
         
