d3277b367fd4662e5974a59d3a42533a67fc701d
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-09 14:08:24 -0300

    feat: definindo as funções de service no frontend do app artefato #25

---
+++ frontend/academic_dev_flow/src/services/artefato_service.js
@@ -0,0 +1,24 @@
+import api from "./api";
+
+export const criarArtefato = async (dados) => {
+
+    const resposta = await api.post("/artefato/cadastrar/", {data: dados})
+    return resposta
+}
+
+export const buscarArtefatoPeloNome =  async (parametro) => {
+
+    const resposta = await api.get(`/artefato/buscar/?name=${encodeURIComponent(parametro)}/`)
+    return resposta
+}
+
+export const atualizarArtefato = async (dados, id) => {
+
+    const resposta = await api.patch(`/artefato/atualizar/${encodeURIComponent(id)}`, {data: dados})
+    return resposta
+}
+
+export const excluirArtefato = async (id) => {
+    const resposta = await api.delete(`/artefato/excluir/${encodeURIComponent(id)}`)
+    return resposta
+}
