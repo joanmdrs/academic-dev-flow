bf1c74f66ea3af5f99b2c4435abf86a026768cd2
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-10 14:26:47 -0300

    refactor: refatorando as funções de services da page usuario #31

--- frontend/academic_dev_flow/src/services/usuario_service.js
+++ frontend/academic_dev_flow/src/services/usuario_service.js
@@ -1,44 +1,27 @@
 import api from "./api";
 
 export const criarUsuario = async (dados, id_membro) => {
-
     const dados_usuario = {
         usuario : dados.usuario,
         senha : dados.senha, // Necessário encriptar a senha
         grupo: dados.grupo,
         membro_id: id_membro
     }
-    try {
-        let response = await api.post('/usuario/cadastrar/', dados_usuario)
-        return response
-    } catch (error) {
-        console.error('Erro ao cadastrar o usuário', error)
-        throw new Error('Falha ao cadastrar o usuário. Por favor, tente novamente.')
-    }
+    const resposta = await api.post('/usuario/cadastrar/', dados_usuario)
+    return resposta
 }
 
 export const buscarUsuarioPeloIdMembro = async (id) => {
-    try {
-        let resposta = await api.get(`/usuario/buscar_usuario/${encodeURIComponent(id)}/`)
-        return resposta
-    } catch (error) {
-        console.error('Erro ao buscar o usuário', error)
-        throw new Error('Falha ao buscar o usuário. Por favor, tente novamente.')
-    }
+    const resposta = await api.get(`/usuario/buscar_usuario/${encodeURIComponent(id)}/`)
+    return resposta
 }
 
 export const atualizarUsuario = async (dados, id_membro) => {
-
     const dados_usuario = {
         usuario : dados.usuario,
         senha : dados.senha, // Necessário encriptar a senha
         grupo: dados.grupo,
     }
-    try {
-        let response = await api.patch(`/usuario/atualizar/${encodeURIComponent(id_membro)}/`, dados_usuario)
-        return response
-    } catch (error) {
-        console.error('Erro ao atualizar o usuário:', error)
-        throw new Error('Falha ao atualizar o usuário. Por favor, tente novamente.')
-    }
+    const resposta = await api.patch(`/usuario/atualizar/${encodeURIComponent(id_membro)}/`, dados_usuario)
+    return resposta
 }
