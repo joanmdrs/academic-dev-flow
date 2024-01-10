2243e4e02cd984437ee55d911336799e3df661bb
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-10 13:44:56 -0300

    refactor: adicionando try e except nas views do app etapa #31

--- apps/etapa/views.py
+++ apps/etapa/views.py
@@ -9,36 +9,42 @@ from .serializers import EtapaSerializer
 
 class CadastrarEtapaView(APIView):
     def post(self, request):
-        data = request.data
-        fluxo_id = data.get('fluxo')
+        try:
+            data = request.data
+            fluxo_id = data.get('fluxo')
 
-        fluxo = Fluxo.objects.get(pk=fluxo_id)  
+            fluxo = Fluxo.objects.get(pk=fluxo_id)  
 
-        etapas_data = data.get('etapas', [])  # Obtém a lista de etapas do payload
+            etapas_data = data.get('etapas', [])  # Obtém a lista de etapas do payload
 
-        etapas_cadastradas = []  # Lista para armazenar as etapas cadastradas
+            etapas_cadastradas = []  # Lista para armazenar as etapas cadastradas
 
-        for etapa_data in etapas_data:
-            etapa_data['fluxo'] = fluxo_id  # Adiciona o ID do fluxo a cada etapa
-            serializer = EtapaSerializer(data=etapa_data)
-            
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                etapas_cadastradas.append(serializer.data)
+            for etapa_data in etapas_data:
+                etapa_data['fluxo'] = fluxo_id  # Adiciona o ID do fluxo a cada etapa
+                serializer = EtapaSerializer(data=etapa_data)
+                
+                if serializer.is_valid(raise_exception=True):
+                    serializer.save()
+                    etapas_cadastradas.append(serializer.data)
 
-        return Response(etapas_cadastradas, status=status.HTTP_201_CREATED)
+            return Response(etapas_cadastradas, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class BuscarEtapaPorIdFluxoView(APIView):
     def get(self, request):
-        parametro = request.GET.get('fluxo_id', None)
-        
-        if parametro is not None:
-            etapas = Etapa.objects.filter(fluxo=parametro)
+        try:
+            parametro = request.GET.get('fluxo_id', None)
             
-        else:
-            etapas = None
+            if parametro is not None:
+                etapas = Etapa.objects.filter(fluxo=parametro)
+                
+            else:
+                etapas = None
+                
+            serializer = EtapaSerializer(etapas, many=True)
             
-        serializer = EtapaSerializer(etapas, many=True)
-        
-        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+            return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             
