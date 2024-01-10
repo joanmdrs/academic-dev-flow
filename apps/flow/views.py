116d052a44f3cb0002e627a9f0bf9ed73a78d733
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-10 13:00:44 -0300

    refactor: adicionando try e except nas views do app fluxo #31

--- apps/flow/views.py
+++ apps/flow/views.py
@@ -9,35 +9,42 @@ from apps.etapa.views import CadastrarEtapaView
 
 class CadastrarFluxoView(APIView):
     def post(self, request):
-        serializer = FlowSerializer(data=request.data)
-        
-        if(serializer.is_valid(raise_exception=True)):
-            serializer.save()
+        try:
+            serializer = FlowSerializer(data=request.data)
             
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+            if(serializer.is_valid(raise_exception=True)):
+                serializer.save()
+                
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 class BuscarFluxoView(APIView):
     def get(self, request):
-        parametro = request.GET.get('name_flow', None)
-           
-        if parametro is not None:
-            fluxos = Flow.objects.filter(nome__icontains=parametro)
-        
-        else: 
+        try:
+            parametro = request.GET.get('name_flow', None)
+               
+            if parametro is not None:
+                fluxos = Flow.objects.filter(nome__icontains=parametro)
             
-            fluxos = Flow.objects.all()
+            else: 
+                fluxos = Flow.objects.all()
+                
+            serializer = FlowSerializer(fluxos, many=True)
             
-        serializer = FlowSerializer(fluxos, many=True)
-        
-        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
-    
-    
+            return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 class BuscarFluxoPeloIdView(APIView):
     def get(self, request, flow_id):
-        fluxo = get_object_or_404(Flow, pk=flow_id)
-        print(fluxo)
-        
-        serializer = FlowSerializer(fluxo, many=False)
-        
-        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        try:
+            fluxo = get_object_or_404(Flow, pk=flow_id)
+            print(fluxo)
+            
+            serializer = FlowSerializer(fluxo, many=False)
+            
+            return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
