f9d954f6b79bc481c5372d1076709b78f4111f90
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-11 15:14:04 -0300

    refactor: modificando o model do app projeto #31

--- apps/projeto/models.py
+++ apps/projeto/models.py
@@ -5,6 +5,6 @@ class Projeto(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.TextField()
     status = models.CharField(max_length=20)
-    data_inicio = models.DateTimeField("Data Início")
-    data_fim = models.DateTimeField("Data Fim")
+    data_inicio = models.DateField("Data Início")
+    data_fim = models.DateField("Data Fim")
     
