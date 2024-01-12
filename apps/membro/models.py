268fb2ecfbc9f67b9162227f5c449210f69b92f4
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-12 13:15:34 -0300

    refactor: refatorando o model de Membro #31

--- apps/membro/models.py
+++ apps/membro/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Membro(models.Model):
     
     nome = models.CharField(max_length=200)
-    cpf = models.CharField(max_length=14)
+    cpf = models.CharField(max_length=20)
     data_nascimento = models.DateField("Data de nascimento")
     sexo = models.CharField(max_length=20)
-    telefone = models.CharField(max_length=14)
+    telefone = models.CharField(max_length=20)
     email = models.EmailField(max_length=200)
     
 
