1da582f68166d128822e6719334053962a8caab9
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-16 17:40:57 -0300

    refactor: refatorando o componente Lista Etapas #31

--- frontend/academic_dev_flow/src/pages/Etapa/components/ListaEtapas/ListaEtapas.js
+++ frontend/academic_dev_flow/src/pages/Etapa/components/ListaEtapas/ListaEtapas.js
@@ -1,40 +1,52 @@
-import { Button, List } from "antd";
+import { Button, Empty } from "antd";
 import React from "react";
+import "./ListaEtapas.css";
 import { AiFillEdit } from "react-icons/ai";
 import { IoMdTrash } from "react-icons/io";
+import { useFormContext } from "../../../Fluxo/context/Provider/FormProvider";
 
-const ListaEtapas = ({dadosEtapas, funcaoBotaoEditar, funcaoBotaoExcluir }) => {
+const ListaEtapas = ({ funcaoBotaoEditar, funcaoBotaoExcluir }) => {
+  const { hasDadosEtapas } = useFormContext();
 
-    return (
-        <div>
-            <List
-                size="small"
-                bordered
-                dataSource={dadosEtapas}
-                renderItem={(item, index) => (
-                    <List.Item 
-                        actions={
-                            [ 
-                                <Button onClick={ () => {
-                                    funcaoBotaoEditar(item, index)
-                                }}> 
-                                    <AiFillEdit />
-                                </Button>, 
-                                <Button onClick={ () => {
-                                    funcaoBotaoExcluir(item)
-                                }}>
-                                    <IoMdTrash />
-                                </Button>
-                            ]
-                        }
-                    >
-                        {item.nome}
+  if (!hasDadosEtapas || hasDadosEtapas.length === 0) {
+    return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} className="form-box" />;
+  }
 
-                    </List.Item>
-                )}
-            />
+  return (
+    <div className="component-lista-etapas">
+      {hasDadosEtapas.map((etapa, id) => (
+        <div key={id} className="item-etapa">
+          <div className="campo-item-etapa">
+            <h5> Nome: </h5>
+            <p> {etapa.nome} </p>
+          </div>
+          <div className="campos-agrupados">
+            <div className="campo-item-etapa">
+              <h5> Data de início:</h5>
+              <p> {etapa.data_inicio} </p>
+            </div>
+            <div className="campo-item-etapa">
+              <h5> Data final:</h5>
+              <p> {etapa.data_fim} </p>
+            </div>
+            <div className="campo-item-etapa">
+              <h5> Status:</h5>
+              <p> {etapa.status} </p>
+            </div>
+          </div>
+          <div className="campo-item-etapa">
+            <h5>Descrição: </h5>
+            <p> {etapa.descricao} </p>
+          </div>
+          <div className="campo-item-etapa botoes-acao">
+            <h5> Ações </h5>
+            <Button icon={<AiFillEdit />} onClick={() => funcaoBotaoEditar(etapa)}> Editar</Button>
+            <Button icon={<IoMdTrash />} onClick={() => funcaoBotaoExcluir(etapa)}> Excluir </Button>
+          </div>
         </div>
-    );   
-}
+      ))}
+    </div>
+  );
+};
 
 export default ListaEtapas;
