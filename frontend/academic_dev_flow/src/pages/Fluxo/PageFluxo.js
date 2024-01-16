ae468ccc072b6a2b90b42871d25f30385755f44a
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-16 12:30:29 -0300

    refactor: refatorando a page de fluxo #31

---
+++ frontend/academic_dev_flow/src/pages/Fluxo/PageFluxo.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./flow.css";
+import Titulo from "../../components/Titulo/Titulo";
+import SearchFlow from "../../components/Flow/SearchFlow/SearchFlow";
+import TemplateFlow from "../../components/Flow/TemplateFlow/TemplateFlow";
+import { buscarFluxoPeloNome } from "../../services/fluxo_service";
+
+const PageFluxo = () => {
+
+    const [fluxos, setFluxos] = useState([])
+
+    const handleSearchFlow = async (query) => {
+        const response = await buscarFluxoPeloNome(query);
+        setFluxos(response.data)
+    }
+
+    return (
+        <div className="flow">
+        
+            < Titulo 
+                titulo="Fluxos de Desenvolvimento" 
+                paragrafo=""
+            />
+            <SearchFlow buscar_fluxos = {handleSearchFlow} />
+            <TemplateFlow fluxos={fluxos}/>
+        </div>
+    )
+
+}
+
+export default PageFluxo; 
