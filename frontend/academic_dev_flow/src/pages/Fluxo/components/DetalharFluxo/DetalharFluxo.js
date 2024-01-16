9ffa815e88bbdd80061e1074cb0f871678b23f00
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-16 17:40:35 -0300

    refactor: implementando o componente Detalhar Fluxo #31

---
+++ frontend/academic_dev_flow/src/pages/Fluxo/components/DetalharFluxo/DetalharFluxo.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Empty } from "antd";
+import "./DetalharFluxo.css";
+import { useFormContext } from "../../context/Provider/FormProvider";
+
+const DetalharFluxo = () => {
+  const { hasDadosFluxo } = useFormContext();
+
+  if (!hasDadosFluxo || (!hasDadosFluxo.nome && !hasDadosFluxo.descricao)) {
+    return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} className="form-box"/>;
+  }
+
+  return (
+    <div className="component-detalhar-fluxo">
+      <h5> Nome: </h5>
+      <p> {hasDadosFluxo.nome} </p>
+
+      <h5> Descrição: </h5>
+      <p> {hasDadosFluxo.descricao}</p>
+    </div>
+  );
+};
+
+export default DetalharFluxo;
