bd352d7095a5edd19601afa4995d21dd5ea23f69
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-16 17:42:10 -0300

    refactor: finalizando a refatoração do componente Botao Novo Fluxo #31

---
+++ frontend/academic_dev_flow/src/pages/Fluxo/components/BotaoNovoFluxo/BotaoNovoFluxo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "antd";
+import {MdAdd} from 'react-icons/md';
+
+const BotaoNovoFluxo = ({funcao}) => {
+
+    return (
+
+        <div>
+            <Button type="primary" style={{
+                marginTop: "20px",
+                padding: "30px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "30px"
+            }}>
+                <MdAdd size="20px" />
+                Novo Fluxo
+            </Button>
+         
+        </div>
+    )
+}
+
+export default BotaoNovoFluxo;
