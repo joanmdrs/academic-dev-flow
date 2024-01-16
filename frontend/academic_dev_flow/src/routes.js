5d66673fd5b4607aa6ff0eb3a22c3046d4534be9
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-16 12:31:13 -0300

    refactor: organizando as chamadas das páginas no arquivo de rotas #31

--- frontend/academic_dev_flow/src/routes.js
+++ frontend/academic_dev_flow/src/routes.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes as Switch, Route} from 'react-router-dom';
-import Flow from './pages/flow/flow';
-import FlowSteps from './pages/flow/FlowSteps';
-import ViewFlow from './components/Flow/ViewFlow/ViewFlow';
-import Home from './pages/home/Home';
+import Home from './pages/Home/Home';
 import PageProjeto from './pages/Projeto/PageProjeto';
+import PageFluxo from './pages/Fluxo/PageFluxo';
 import PageMembro from './pages/Membro/PageMembro';
 import PageArtefato from './pages/Artefato/PageArtefato';
 
@@ -14,9 +12,7 @@ function Routes() {
         <Switch>
             <Route Component={Home} path='/' exact/>
             <Route Component={PageProjeto} path='/projetos' exact/>
-            <Route Component={Flow} path='/fluxos' exact/>
-            <Route Component={FlowSteps} path='/fluxos/novo' exact/>
-            <Route Component={ViewFlow} path='fluxos/visualizar/:id' exact/>
+            <Route Component={PageFluxo} path='/fluxos' exact/>
             <Route Component={PageMembro} path='/membros' exact/>
             <Route Component={PageArtefato} path='/artefatos' exact/>
         
