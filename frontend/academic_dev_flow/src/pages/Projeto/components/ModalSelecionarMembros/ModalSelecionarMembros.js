c4baf73a1f8bf7379a6cd3640e7dde98974425f2
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-30 20:32:37 -0300

    feat: implementando a estrutura da TabEquipe #33

---
+++ frontend/academic_dev_flow/src/pages/Projeto/components/ModalSelecionarMembros/ModalSelecionarMembros.js
@@ -0,0 +1,69 @@
+import { Button, Form, Input, Modal, Table } from "antd";
+import React, { useState } from "react";
+
+const ModalSelecionarMembros = ({status, onCancel, onOk, colunas}) => {
+
+    const [form] = Form.useForm();
+    const [parametro, setParametro] = useState('');
+    const [dados, setDados] = useState([])
+    const [hasResposta, setHasResposta] = useState(false)
+
+    const handleAlterarParametro = (event) => {
+        setParametro(event.target.value);
+    };
+
+    const handleResetar = () => {
+        form.resetFields()
+        setHasResposta(false)
+        setDados([])
+        setParametro('')
+    }
+
+    const handleSelecionar = () => {
+
+    }
+
+
+    return (
+        <Modal 
+            open={true}
+            onCancel={onCancel}
+            footer={
+                <div> 
+                    <Button type="primary" onClick={handleSelecionar}> SELECIONAR </Button>
+                    <Button onClick={onCancel}> FECHAR </Button>
+                </div>
+            }
+
+        >
+            <h4> Buscar Membro </h4>
+            <Form form={form}> 
+                <Form.Item>
+                    <Input  type="text" placeholder="Ex.: Joan "/>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" onClick={ async () => {
+                        const resposta = await onOk(parametro)
+                        setHasResposta(true)
+                        if(resposta.status === 200) {
+                            setDados(resposta.data.results)
+                        } else {
+                            setDados([])
+                    }
+                    }}> FILTRAR </Button>
+                    <Button onClick={handleResetar}> LIMPAR </Button>
+                </Form.Item>
+            </Form>
+
+            { hasResposta &&  
+                <Table
+                    columns={colunas}
+                    dataSource={dados}
+                />
+            }
+           
+        </Modal>
+    )
+}
+
+export default ModalSelecionarMembros;
--- frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabEquipe/TabEquipe.js
+++ frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabEquipe/TabEquipe.js
@@ -1,8 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import ListaDados from "../../../../../components/Listas/ListaDados/ListaDados";
+import BotaoAdicionar from "../../../../../components/Botoes/BotaoAdicionar/BotaoAdicionar";
+import BotaoExcluir from "../../../../../components/Botoes/BotaoExcluir/BotaoExcluir";
+import { Input } from "antd";
+import ModalSelecionarMembros from "../../../components/ModalSelecionarMembros/ModalSelecionarMembros";
+import { buscarMembroPeloNome } from "../../../../../services/membro_service";
+import { NotificationManager } from "react-notifications";
 
 const TabEquipe = () => {
 
+    const [isBotaoAdicionarVisivel, setIsBotaoAdicionarVisivel] = useState(false)
+    const [isBotaoExcluirVisivel, setIsBotaoExcluirVisivel] = useState(true)
+    const [isModalVisivel, setIsModalVisivel] = useState(false)
 
+    const COLUNAS_LISTA = [
+        {
+            title: (
+                <Input type="checkbox"/>
+            ),
+            key: "checkbox", 
+            dataIndex: "checkbox",
+            render: (_, record) => (
+                <Input type="checkbox"/>
+            )
+        },
+        {
+            title: "ID",
+            key: "id",
+            dataIndex: "id",
+        },
+        {
+            title: "Nome",
+            dataIndex: "nome",
+            key: "nome",
+        },
+        {
+            title: "CPF",
+            dataIndex: "cpf",
+            key: "cpf",
+        },
+    ];
+
+    const handleExibirModal = () => setIsModalVisivel(true)
+    const handleFecharModal = () => setIsModalVisivel(false)
+
+    const handleBuscarMembro = async (parametro) => {
+        try {
+            const resposta = await buscarMembroPeloNome(parametro);
+            if(resposta.status !== 200){
+                NotificationManager.error("Ocorreu um problema ao buscar os dados, contate o suporte!")
+            } else {
+            return resposta
+            }
+        } catch (error) {
+            NotificationManager.error("Ocorreu um problema ao buscar os dados, contate o suporte!")
+        } 
+    }
+    return (
+
+        <div className="box"> 
+
+            {isModalVisivel && 
+                < ModalSelecionarMembros 
+                    onCancel={handleFecharModal}
+                    colunas={COLUNAS_LISTA}
+                    status={isModalVisivel}
+                    onOk={handleBuscarMembro}
+
+                />
+            }
+
+            <div> 
+                <div className="box" style={{cursor: "pointer"}}> 
+                    <h4> VINCULAR ALUNO(S) </h4>
+                </div>
+                
+                <div> 
+                    <div className="group-buttons" style={{width: "100%", display: "flex", justifyContent: "flex-end"}}> 
+                        <BotaoAdicionar status={isBotaoAdicionarVisivel} funcao={handleExibirModal}/>
+                        <BotaoExcluir status={isBotaoExcluirVisivel}/>
+                    </div>
+
+                    <ListaDados 
+                        colunas={COLUNAS_LISTA}
+                    />
+                </div>
+            </div>
+
+
+            <div> 
+                <div className="box"> 
+                    <h4> VINCULAR PROFESSOR(ES)</h4>
+                </div>
+
+                <div> 
+                    <div className="group-buttons" style={{width: "100%", display: "flex", justifyContent: "flex-end"}}> 
+                        <BotaoAdicionar />
+                        <BotaoExcluir />
+                    </div>
+
+                    <ListaDados 
+                        colunas={COLUNAS_LISTA}
+                    />
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default TabEquipe;
