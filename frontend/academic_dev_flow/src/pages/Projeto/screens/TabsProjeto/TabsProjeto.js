99ae182f796e881bbdc34c0174efefd3bb4d8388
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-01-30 20:32:13 -0300

    feat: finalizando a definição da estrutura da TabsProjeto #33

--- frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabsProjeto.js
+++ frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabsProjeto.js
@@ -1,38 +1,67 @@
 import React, { useState } from "react";
+import "./TabsProjeto.css";
 import Titulo from "../../../../components/Titulo/Titulo";
-import { Tabs } from "antd";
+import { Button, Tabs } from "antd";
 import Item from "antd/es/list/Item";
 import TabProjeto from "./TabProjeto/TabProjeto";
 import TabEquipe from "./TabEquipe/TabEquipe";
 import TabFluxo from "./TabFluxo/TabFluxo";
+import BotaoBuscar from "../../../../components/Botoes/BotaoBuscar/BotaoBuscar";
+import BotaoAdicionar from "../../../../components/Botoes/BotaoAdicionar/BotaoAdicionar";
+import BotaoExcluir from "../../../../components/Botoes/BotaoExcluir/BotaoExcluir";
 
 const TabsProjeto = () => {
   
     const [current, setCurrent] = useState("1");
     return (    
-        <React.Fragment>
+        <div className="component-tabs-projeto">
           <Titulo 
             titulo="Projetos"
             paragrafo="Administração > Gerenciar projetos"
           />
-          <Tabs
-            style={{marginTop: "30px", padding: "20px"}} 
-            activeKey={current} 
-            onChange={setCurrent} 
-            className="form-box"
-          >
-            <Item tab="Projeto" key="1">
-              <TabProjeto />
-            </Item>
-            <Item tab="Equipe" key="2">
-              <TabEquipe />
-            </Item>
-            <Item tab="Fluxo" key="3">
-                <TabFluxo />
-            </Item>
-          </Tabs>
-  
-        </React.Fragment>
+
+          <div className="botoes-de-acao"> 
+            <BotaoBuscar nome="BUSCAR PROJETO" />
+            <div className="group-buttons"> 
+              <BotaoAdicionar />
+              <BotaoExcluir />
+            </div>
+          </div>
+
+          <div className="form-box"> 
+            <Tabs
+              size="large"
+              indicator={{
+                align: "center"
+              }}
+              style={{padding: "20px"}}
+              activeKey={current} 
+              onChange={setCurrent} 
+            > 
+              <Item tab="Projeto" key="1" colStyle={{width: "250px"}}>
+                <TabProjeto />
+              </Item>
+              <Item tab="Equipe" key="2" className="tab-item">
+                <TabEquipe />
+              </Item>
+              <Item tab="Fluxo" key="3" className="tab-item">
+                  <TabFluxo />
+              </Item>
+            </Tabs>
+
+            <div className="tabs-projeto-footer-botoes">
+              <Button type="primary">
+                SALVAR
+              </Button >
+
+              <Button type="primary" danger>
+                CANCELAR
+              </Button>
+            </div>
+          </div>
+          
+
+        </div>
         
     );
   }
