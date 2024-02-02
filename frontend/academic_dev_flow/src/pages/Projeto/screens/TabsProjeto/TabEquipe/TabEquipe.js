a696d9ca13364066a0be2e2130c7c797be1a5c6a
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-02 08:11:47 -0300

    feat: implementando o useEffect para atualizar os dados da equipe com base nos dados do projeto #33

--- frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabEquipe/TabEquipe.js
+++ frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabEquipe/TabEquipe.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import "./TabEquipe.css"
+import React, { useState, useEffect } from "react";
+import "./TabEquipe.css";
 import ListaDados from "../../../../../components/Listas/ListaDados/ListaDados";
 import BotaoAdicionar from "../../../../../components/Botoes/BotaoAdicionar/BotaoAdicionar";
 import BotaoExcluir from "../../../../../components/Botoes/BotaoExcluir/BotaoExcluir";
@@ -8,163 +8,164 @@ import ModalSelecionarMembros from "../../../components/ModalSelecionarMembros/M
 import { buscarMembroPeloId, buscarMembroPeloNome } from "../../../../../services/membro_service";
 import { NotificationManager } from "react-notifications";
 import { criarMembroProjeto, listarMembrosPorProjeto } from "../../../../../services/membro_projeto_service";
+import { useFormContext } from "../../../context/Provider/Provider";
 
 const TabEquipe = () => {
+  const { hasProjeto } = useFormContext();
+  const [isBotaoAdicionarVisivel, setIsBotaoAdicionarVisivel] = useState(false);
+  const [isBotaoExcluirVisivel, setIsBotaoExcluirVisivel] = useState(true);
+  const [isModalVisivel, setIsModalVisivel] = useState(false);
+  const [alunos, setAlunos] = useState([]);
+  const idProjeto = "2";
 
-    const [isBotaoAdicionarVisivel, setIsBotaoAdicionarVisivel] = useState(false)
-    const [isBotaoExcluirVisivel, setIsBotaoExcluirVisivel] = useState(true)
-    const [isModalVisivel, setIsModalVisivel] = useState(false)
-    const [alunos, setAlunos] = useState([])
-    const idProjeto = "2"
+  const COLUNAS_LISTA = [
+    {
+      title: "ID",
+      key: "id",
+      dataIndex: "id",
+    },
+    {
+      title: "Nome",
+      dataIndex: "nome",
+      key: "nome",
+    },
+    {
+      title: "Função",
+      dataIndex: "funcao",
+      key: "funcao",
+    },
+  ];
 
-    const COLUNAS_LISTA = [ 
-        {
-            title: "ID",
-            key: "id",
-            dataIndex: "id",
-        },
-        {
-            title: "Nome",
-            dataIndex: "nome",
-            key: "nome",
-        },
-        {
-            title: "Função",
-            dataIndex: "funcao",
-            key: "funcao",
-        },
-    ];
+  const handleExibirModal = () => setIsModalVisivel(true);
+  const handleFecharModal = () => setIsModalVisivel(false);
 
-    const handleExibirModal = () => setIsModalVisivel(true)
-    const handleFecharModal = () => setIsModalVisivel(false)
-
-    const handleBuscarMembro = async (parametro) => {
-        try {
-            const resposta = await buscarMembroPeloNome(parametro);
-            if(resposta.status !== 200){
-                NotificationManager.error("Ocorreu um problema ao buscar os dados, contate o suporte!")
-            } else {
-            return resposta
-            }
-        } catch (error) {
-            NotificationManager.error("Ocorreu um problema ao buscar os dados, contate o suporte!")
-        } 
+  const handleBuscarMembro = async (parametro) => {
+    try {
+      const resposta = await buscarMembroPeloNome(parametro);
+      if (resposta.status !== 200) {
+        NotificationManager.error("Ocorreu um problema ao buscar os dados, contate o suporte!");
+      } else {
+        return resposta;
+      }
+    } catch (error) {
+      NotificationManager.error("Ocorreu um problema ao buscar os dados, contate o suporte!");
     }
+  };
 
-    const handleListarMembrosPorProjeto = async (parametro) => {
-        try {
-            const resposta = await listarMembrosPorProjeto(parametro)
+  const handleListarMembrosPorProjeto = async (parametro) => {
+    try {
+      const resposta = await listarMembrosPorProjeto(parametro);
 
-            if(resposta.status === 200){
-                const membrosVinculados = resposta.data 
+      if (resposta.status === 200) {
+        const membrosVinculados = resposta.data;
 
-                if (membrosVinculados.length === 0) {
-                  setAlunos([])
-        
-                } else {
-        
-                  const promisesAlunos = membrosVinculados.map(async (membroProjeto) => {
-                    const respostaMembro = await buscarMembroPeloId(membroProjeto.membro);
-                    return {
-                      id: membroProjeto.id,
-                      projeto: idProjeto,
-                      membro: respostaMembro.data.id,
-                      nome: respostaMembro.data.nome,
-                      funcao: membroProjeto.funcao,
-                    };
-                  });
-          
-                  const resultados = await Promise.all(promisesAlunos);
-                  
-                  setAlunos(resultados);
-                }
-            }
-        } catch (error) {
-            
+        if (membrosVinculados.length === 0) {
+          setAlunos([]);
+        } else {
+          const promisesAlunos = membrosVinculados.map(async (membroProjeto) => {
+            const respostaMembro = await buscarMembroPeloId(membroProjeto.membro);
+            return {
+              id: membroProjeto.id,
+              projeto: idProjeto,
+              membro: respostaMembro.data.id,
+              nome: respostaMembro.data.nome,
+              funcao: membroProjeto.funcao,
+            };
+          });
+
+          const resultados = await Promise.all(promisesAlunos);
+
+          setAlunos(resultados);
         }
+      }
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    if (hasProjeto) {
+      handleListarMembrosPorProjeto(idProjeto);
     }
+  }, [hasProjeto]);
 
-    const handleSelecionarMembros = async (dados) => {
+  const handleSelecionarMembros = async (dados) => {
+    try {
+      const dadosEnviar = dados.map((item) => {
+        return {
+          projeto: idProjeto,
+          membro: item.id,
+        };
+      });
+      const resposta = await criarMembroProjeto(dadosEnviar);
 
-        try {
-            const dadosEnviar = dados.map((item) => {
-                return {
-                    projeto: idProjeto,
-                    membro: item.id
-                };
-            });
-            const resposta = await criarMembroProjeto(dadosEnviar)
+      if (resposta.status === 200) {
+        NotificationManager.success("Alunos vinculados ao projeto com sucesso !");
+        await handleListarMembrosPorProjeto(idProjeto);
+      } else {
+        NotificationManager.error("Falha ao vincular os alunos ao projeto, contate o suporte!");
+      }
+    } catch (error) {
+      console.log(error);
+      NotificationManager.error("Ocorreu um problema durante a operação, contate o suporte!");
+    }
+  };
 
-            if (resposta.status === 200){
-                NotificationManager.success("Alunos vinculados ao projeto com sucesso !")
-                await handleListarMembrosPorProjeto(idProjeto)
-            } else {
-                NotificationManager.error('Falha ao vincular os alunos ao projeto, contate o suporte!')
-            }
-        } catch (error) {
-            console.log(error)
-            NotificationManager.error("Ocorreu um problema durante a operação, contate o suporte!")
-        }
+  return (
+    <div className="box">
+      {isModalVisivel && (
+        <ModalSelecionarMembros
+          onCancel={handleFecharModal}
+          colunas={COLUNAS_LISTA}
+          status={isModalVisivel}
+          onOk={handleBuscarMembro}
+          onSelect={handleSelecionarMembros}
+        />
+      )}
 
-    }   
-
-    return (
-
-        <div className="box"> 
-
-            {isModalVisivel && 
-                < ModalSelecionarMembros 
-                    onCancel={handleFecharModal}
-                    colunas={COLUNAS_LISTA}
-                    status={isModalVisivel}
-                    onOk={handleBuscarMembro}
-                    onSelect={handleSelecionarMembros}
-                />
-            }
-
-            <div> 
-                <div className="box" style={{cursor: "pointer"}}> 
-                    <h4> VINCULAR ALUNO(S) </h4>
-                </div>
-                
-                <div> 
-                    <div className="group-buttons" style={{width: "100%", display: "flex", justifyContent: "flex-end"}}> 
-                        <BotaoAdicionar status={isBotaoAdicionarVisivel} funcao={handleExibirModal}/>
-                        <BotaoExcluir status={isBotaoExcluirVisivel}/>
-                    </div>
-
-                    <Table
-                        className="tab-equipe"
-                        columns={COLUNAS_LISTA} 
-                        dataSource={alunos}
-                        rowKey="id"
-                        rowSelection={{
-                            type: "checkbox"
-                        }}
-                    />
-                </div>
-            </div>
-
-
-            <div> 
-                <div className="box"> 
-                    <h4> VINCULAR PROFESSOR(ES)</h4>
-                </div>
-
-                <div> 
-                    <div className="group-buttons" style={{width: "100%", display: "flex", justifyContent: "flex-end"}}> 
-                        <BotaoAdicionar />
-                        <BotaoExcluir />
-                    </div>
-
-                    <ListaDados 
-                        colunas={COLUNAS_LISTA}
-                    />
-                </div>
-
-            </div>
+      <div>
+        <div className="box" style={{ cursor: "pointer" }}>
+          <h4> VINCULAR ALUNO(S) </h4>
         </div>
-    )
-}
+
+        <div>
+          <div
+            className="group-buttons"
+            style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}
+          >
+            <BotaoAdicionar status={isBotaoAdicionarVisivel} funcao={handleExibirModal} />
+            <BotaoExcluir status={isBotaoExcluirVisivel} />
+          </div>
+
+          <Table
+            className="tab-equipe"
+            columns={COLUNAS_LISTA}
+            dataSource={alunos}
+            rowKey="id"
+            rowSelection={{
+              type: "checkbox",
+            }}
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="box">
+          <h4> VINCULAR PROFESSOR(ES)</h4>
+        </div>
+
+        <div>
+          <div
+            className="group-buttons"
+            style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}
+          >
+            <BotaoAdicionar />
+            <BotaoExcluir />
+          </div>
+
+          <ListaDados colunas={COLUNAS_LISTA} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TabEquipe;
