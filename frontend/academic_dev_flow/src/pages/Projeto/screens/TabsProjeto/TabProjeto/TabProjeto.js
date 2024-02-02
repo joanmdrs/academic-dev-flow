9505f080cc783a311e287c86a61fa27bcd341b94
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-02 08:10:49 -0300

    feat: modificando a lógica da TabProjeto para exibir um spin enquanto os dados são carregados #33

--- frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabProjeto/TabProjeto.js
+++ frontend/academic_dev_flow/src/pages/Projeto/screens/TabsProjeto/TabProjeto/TabProjeto.js
@@ -1,61 +1,96 @@
-import { Form, Input, Select } from "antd";
+import { Form, Input, Select, Spin } from "antd";
 import React, { useEffect, useState } from "react";
+import { LoadingOutlined } from '@ant-design/icons';
 
-const { Option } = Select
+const { Option } = Select;
 
-const TabProjeto = ({valoresIniciais}) => {
+const TabProjeto = ({ valoresIniciais }) => {
+  const [estadoInterno, setEstadoInterno] = useState(valoresIniciais);
+  const [carregando, setCarregando] = useState(false);
 
-    const [estadoInterno, setEstadoInterno] = useState(valoresIniciais);
+  const [form] = Form.useForm();
 
-    // Use useEffect para monitorar as mudanças em valoresIniciais
-    useEffect(() => {
-      setEstadoInterno(valoresIniciais);
-      form.setFieldsValue(estadoInterno)
-    }, [estadoInterno, valoresIniciais]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setCarregando(true);
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+        setEstadoInterno(valoresIniciais);
+      } catch (error) {
+      } finally {
+        setCarregando(false);
+      }
+    };
 
-    const [form] = Form.useForm()
+    fetchData();
+  }, [valoresIniciais]);
+
     return (
-        <Form
-            form={form}
-            layout="horizontal"
-            style={{marginTop: "20px"}}
-            className="box"
-            initialValues={estadoInterno}
-          
-        >
-            <Form.Item label="Nome" name="nome">
-                <Input
-                    id="input-nome"
-                    name="nome"
-                />
-            </Form.Item>
 
-            <Form.Item label="Status" name="status" style={{maxWidth: 250}}>
-                    <Select
-                        id="status"
-                        name="status"
+       <React.Fragment>
+            { carregando ? (
+                   <div style={{ textAlign: "center", padding: "20px" }}>
+                        <Spin
+                            indicator={
+                            <LoadingOutlined
+                                style={{
+                                fontSize: 48, // Ajuste o tamanho do spinner aqui
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                }}
+                                spin
+                            />
+                            }
+                        />
+                    </div>
+                ) : 
+                (<React.Fragment>
+                    <Form
+                        form={form}
+                        layout="horizontal"
+                        style={{marginTop: "20px"}}
+                        className="box"
+                        initialValues={estadoInterno}
+                    
                     >
-                        <Option value="">Selecione</Option>
-                        <Option value="cancelado">Cancelado</Option>
-                        <Option value="em_andamento">Em andamento</Option>
-                        <Option value="concluido">Concluído</Option>
-                    </Select>
-            </Form.Item>
+                        <Form.Item label="Nome" name="nome">
+                            <Input
+                                id="input-nome"
+                                name="nome"
+                            />
+                        </Form.Item>
 
-            <Form.Item label="Data de Início" name="data_inicio" style={{ maxWidth: 250, float: 'left', marginRight: '10px' }}>
-                <Input id="data_inicio" name="data_inicio" type="date" />
-            </Form.Item>
+                        <Form.Item label="Status" name="status" style={{maxWidth: 250}}>
+                                <Select
+                                    id="status"
+                                    name="status"
+                                >
+                                    <Option value="">Selecione</Option>
+                                    <Option value="cancelado">Cancelado</Option>
+                                    <Option value="em_andamento">Em andamento</Option>
+                                    <Option value="concluido">Concluído</Option>
+                                </Select>
+                        </Form.Item>
 
-            <Form.Item label="Data de Término" name="data_fim" style={{ maxWidth: 250, float: 'left' }}>
-                <Input id="data_fim" name="data_fim" type="date" />
-            </Form.Item>
+                        <Form.Item label="Data de Início" name="data_inicio" style={{ maxWidth: 250, float: 'left', marginRight: '10px' }}>
+                            <Input id="data_inicio" name="data_inicio" type="date" />
+                        </Form.Item>
 
-            <Form.Item label="Descrição" name="descricao" style={{ clear: 'both' }}>
-                <Input.TextArea id="descricao" name="descricao" rows={6} />
-            </Form.Item>
+                        <Form.Item label="Data de Término" name="data_fim" style={{ maxWidth: 250, float: 'left' }}>
+                            <Input id="data_fim" name="data_fim" type="date" />
+                        </Form.Item>
 
-        </Form>
-    )
+                        <Form.Item label="Descrição" name="descricao" style={{ clear: 'both' }}>
+                            <Input.TextArea id="descricao" name="descricao" rows={6} />
+                        </Form.Item>
+
+                    </Form>
+                </React.Fragment>
+            ) }
+        </React.Fragment>      
+    ) 
+    
 
 }
 
