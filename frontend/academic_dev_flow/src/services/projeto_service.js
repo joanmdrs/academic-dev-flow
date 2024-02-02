92634d935e04f4db9f6e00dbdcd775924a07a721
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-02 10:41:08 -0300

    refactor: modificando o service de criar projeto #33

--- frontend/academic_dev_flow/src/services/projeto_service.js
+++ frontend/academic_dev_flow/src/services/projeto_service.js
@@ -1,15 +1,8 @@
 import api from "./api";
 
 export const criarProjeto = (dados) => {
-    const dados_projeto = {
-        nome: dados.nome,
-        descricao: dados.descricao,
-        status: dados.status,
-        data_inicio: (dados.data_inicio).format("YYYY-MM-DD"),
-        data_fim: (dados.data_fim).format("YYYY-MM-DD"),
-    }
     
-    const resposta  = api.post("/projeto/cadastrar/", dados_projeto)
+    const resposta  = api.post("/projeto/cadastrar/", dados)
     return resposta;
 }
 
