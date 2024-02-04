171c49de148872b59953d12e7a25b929729518d3
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-04 16:23:42 -0300

    feat: implementando a view e a url que realiza a atribuição do fluxo ao projeto #33

--- apps/projeto/urls.py
+++ apps/projeto/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('buscar/', BuscarProjetosPorNomeView.as_view(), name='buscar_projetos_por_nome'),
     path('excluir/<int:id>/', ExcluirProjetoView.as_view(), name='excluir_projeto'),
     path('atualizar/<int:id>/', AtualizarProjetoView.as_view(), name='atualizar_projeto'),
+    path('atualizar/fluxo/<int:id>/', AtualizarFluxoProjetoView.as_view(), name='atualizar_fluxo')
 ]
