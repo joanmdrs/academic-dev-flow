512f83a85af202c0d06d9f32f93647463aaa32c6
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-11 09:56:35 -0300

    apagando o app fluxo_etapa e registrando os models #35

--- apps/fluxo_etapa/serializers.py
+++
@@ -1,8 +0,0 @@
-# from rest_framework import serializers
-# from .models import FluxoEtapa
-
-# class FluxoEtapaSerializer(serializers.ModelSerializer):
-#     class Meta: 
-#         model = FluxoEtapa
-#         fields = '__all__'
-        
