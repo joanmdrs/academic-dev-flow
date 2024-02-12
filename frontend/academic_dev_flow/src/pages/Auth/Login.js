dde5058be7b7d03d4c63d0d3dcae70af7abb5067
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-12 14:08:55 -0300

    melhorando o layout da página de login e implementando a função handle que realiza o login #35

--- frontend/academic_dev_flow/src/pages/Auth/Login.js
+++ frontend/academic_dev_flow/src/pages/Auth/Login.js
@@ -2,11 +2,20 @@ import React from 'react';
 import "./Login.css";
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Form, Input, Button } from 'antd';
-import img from "../../assets/banner-login.png"
+import AuthService from '../../services/authService';
 
 const LoginForm = () => {
-  const onFinish = (values) => {
-    console.log('Received values:', values);
+
+  const handleLogin = async (values) => {   
+    const username = values.username
+    const password = values.password
+    const result = await AuthService.login(username, password);
+
+    if (result.success) {
+      console.log('Login successful:', result.data);
+    } else {
+      console.error('Login failed:', result.error);
+    }
   };
 
   return (
@@ -14,64 +23,53 @@ const LoginForm = () => {
 
         <div className='page-login-layout'> 
 
-          <div className='page-login-banner'> 
-            <img 
-              src='https://img.freepik.com/vetores-gratis/tecnologia-de-rede-futurista-azul_53876-100679.jpg?w=996&t=st=1707685507~exp=1707686107~hmac=d0c55903b0d96f9c2409d01edaaeaa013961167ef2864922eff7a97cc28adde3'
-            />
-            <h1>Bem vindo !</h1>
+          
+          <div className='page-login-form-header'> 
+            <h2> LOGIN </h2>
           </div>
 
-          <div className='page-login-form'> 
+          <div className='page-login-form-content'>
+            <Form
+              name="login-form"
+              layout='vertical'
+              onFinish={handleLogin}
+              initialValues={{ remember: true }}
+              >
+                <Form.Item
+                  name="username"
+                  rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                  <Input 
+                    style={{padding: "10px"}}
+                    prefix={<UserOutlined 
+                    className="site-form-item-icon" />} 
+                    placeholder='username'/>
 
-            <div className='page-login-form-layout'> 
-                <div className='page-login-form-header'> 
-                  <h2> LOGIN </h2>
-                </div>
-                <div className='page-login-form-content'>
-                  <Form
-                    name="login-form"
-                    layout='vertical'
-                    onFinish={onFinish}
-                    initialValues={{ remember: true }}
-                    >
-                      <Form.Item
-                        name="username"
-                        rules={[{ required: true, message: 'Please input your username!' }]}
-                      >
-                        <Input 
-                          style={{padding: "10px"}}
-                          prefix={<UserOutlined 
-                          className="site-form-item-icon" />} 
-                          placeholder='username'/>
+                </Form.Item>
 
-                      </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                  <Input.Password 
+                    style={{padding: "10px"}}
+                    prefix={<LockOutlined 
+                    className="site-form-item-icon" />} 
+                    placeholder='password'/>
+                </Form.Item>
 
-                      <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: 'Please input your password!' }]}
-                      >
-                        <Input.Password 
-                          style={{padding: "10px"}}
-                          prefix={<LockOutlined 
-                          className="site-form-item-icon" />} 
-                          placeholder='password'/>
-                      </Form.Item>
-
-                      <Form.Item>
-                        <Button type="primary" htmlType="submit" size='large'>
-                          Log in
-                        </Button>
-                      </Form.Item>
-                    </Form>
-                </div>
-            </div>
-            
-          </div>
-        </div>
-        
-
+                <Form.Item>
+                  <Button type="primary" htmlType="submit" size='large'>
+                    Log in
+                  </Button>
+                </Form.Item>
+              </Form>
+      </div>
     </div>
-      );
+            
+  </div>
+        
+);
 };
 
 export default LoginForm;
--- frontend/academic_dev_flow/src/services/authService.js
+++ frontend/academic_dev_flow/src/services/authService.js
@@ -3,14 +3,14 @@ import api from "./api";
 const AuthService = {
   login: async (username, password) => {
     try {
-      const response = await api.post("myauth/login/", {username, password})
+      const response = await api.post("myauth/login/", { username, password });
 
-      if (response.ok) {
-        const data = await response.json();
+      if (response.status === 200) {
+        const data = response.data; // Não precisa chamar response.json() porque o dado já está no formato JSON
         localStorage.setItem('token', data.token);
         return { success: true, data };
       } else {
-        const errorData = await response.json();
+        const errorData = response.data; // Mesma lógica aqui
         return { success: false, error: errorData };
       }
     } catch (error) {
