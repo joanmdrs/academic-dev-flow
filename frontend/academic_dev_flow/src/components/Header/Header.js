b366026938c72bd2ad05d6a4fe058e82ea5f7ce2
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-13 13:33:33 -0300

    implementando o menu dropdown no header, e fazendo a chamada para a função de logOut #37

--- frontend/academic_dev_flow/src/components/Header/Header.js
+++ frontend/academic_dev_flow/src/components/Header/Header.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { Layout } from 'antd';
+import React, { isValidElement } from 'react';
+import { Button, Layout } from 'antd';
 import logo_ufrn from "../../assets/logo-ufrn.png";
 import './Header.css';
+import { SlLogout } from "react-icons/sl";
+import MyDropdown from '../Dropdown/Dropdown';
+import { useAuth } from '../../hooks/AuthProvider';
 
 const { Header } = Layout;
 
+
 const MyHeader = () => {
+
+  const {logOut} = useAuth();
+
+  
+  const items = [
+    {
+      label: (
+        <Button style={{border: "none"}} onClick={() => logOut()}> 
+          Logout
+        </Button>
+      ),
+      key: '1',
+      icon: <SlLogout />,
+    },
+
+  ];
+
   return (
     <Header className="header">
-      <div className='logo-ufrn'> 
-        <img src={logo_ufrn} alt="Logo da UFRN" />
-      </div>      
+      <div className='logo-ufrn'>
+        <img src={logo_ufrn} alt="Logo da UFRN" className='logo-image' />
+      </div>
+
+      <div className='icone-perfil'>
+        <MyDropdown items={items} />
+      </div>
     </Header>
   );
 };
 
 export default MyHeader;
-
