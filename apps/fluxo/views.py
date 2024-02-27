34cde02ba3c67e70b9983b5f92dd0c942e451ec9
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-27 17:48:47 -0300

    refatorando o código das views de fluxo #37

--- apps/fluxo/views.py
+++ apps/fluxo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,94 +6,67 @@ from django.http import JsonResponse
 from .models import Fluxo
 from .serializers import FluxoSerializer
 from apps.etapa.views import CadastrarEtapaView 
+from rest_framework.permissions import IsAuthenticated
+from apps.api.permissions import IsAdminUserOrReadOnly 
 
-class CadastrarFluxoView(APIView):
+class BaseFluxoView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUserOrReadOnly]
+
+    def handle_exception(self, exc):
+        return Response({'error': str(exc)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class CadastrarFluxoView(BaseFluxoView):
     def post(self, request):
-        try:
-            serializer = FluxoSerializer(data=request.data)
-            
-            if(serializer.is_valid(raise_exception=True)):
-                serializer.save()
-                
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-class BuscarFluxoPeloNomeView(APIView):
-    def get(self, request):
-        try:
-            parametro = request.GET.get('name_fluxo', None)
-               
-            if parametro is not None:
-                fluxos = Fluxo.objects.filter(nome__icontains=parametro)
-            
-            else: 
-                fluxos = Fluxo.objects.all()
-                
-            if not fluxos:
-                return Response({'message': 'Nenhum fluxo encontrado', 'results': []}, status=status.HTTP_200_OK)
-                
-            serializer = FluxoSerializer(fluxos, many=True)
-            return Response({'message': 'Fluxos encontrados com sucesso!', 'results': serializer.data}, status=status.HTTP_200_OK)
-            
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-class BuscarFluxoPeloIdView(APIView):
-    def get(self, request, fluxo_id):
-        try:
-            fluxo = Fluxo.objects.get(pk=fluxo_id)
-            
-            serializer = FluxoSerializer(fluxo, many=False)
-            
-            return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-       
-class AtualizarFluxoView(APIView):
-    def patch(self, request, fluxo_id): 
-        try: 
-            
-            fluxo = Fluxo.objects.get(pk=fluxo_id) 
-            
-            serializer = FluxoSerializer(fluxo, data=request.data)
-            
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-    
-        except Exception as e: 
-             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-class ExcluirFluxoView(APIView):
-    def delete(self, request, fluxo_id):
+        serializer = FluxoSerializer(data=request.data)
         
-        try: 
-            
-            fluxo = Fluxo.objects.get(pk=fluxo_id)
-            
-            if fluxo is not None: 
-                fluxo.delete()
-                return Response({'detail': 'Fluxo excluído com sucesso!'}, status=status.HTTP_204_NO_CONTENT)
-            
-            else: 
-                return Response({'error': 'Objeto não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
-            
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)  
-    
-    
-class ListarFluxosView(APIView):
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BuscarFluxoPeloNomeView(BaseFluxoView):
     def get(self, request):
-        try:
+        parametro = request.GET.get('name_fluxo', None)
+        
+        if parametro is not None:
+            fluxos = Fluxo.objects.filter(nome__icontains=parametro)
+        else: 
             fluxos = Fluxo.objects.all()
             
-            serializer = FluxoSerializer(fluxos, many=True) 
+        if not fluxos:
+            return Response({'message': 'Nenhum fluxo encontrado', 'results': []}, status=status.HTTP_200_OK)
             
-            return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})   
-        except Exception as e: 
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            
+        serializer = FluxoSerializer(fluxos, many=True)
+        return Response({'message': 'Fluxos encontrados com sucesso!', 'results': serializer.data}, status=status.HTTP_200_OK)
+
+class BuscarFluxoPeloIdView(BaseFluxoView):
+    def get(self, request, fluxo_id):
+        fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
+        serializer = FluxoSerializer(fluxo, many=False)
+        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+
+class AtualizarFluxoView(BaseFluxoView):
+    def patch(self, request, fluxo_id): 
+        fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
+        serializer = FluxoSerializer(fluxo, data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ExcluirFluxoView(BaseFluxoView):
+    def delete(self, request, fluxo_id):
+        fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
+        
+        if fluxo is not None: 
+            fluxo.delete()
+            return Response({'detail': 'Fluxo excluído com sucesso!'}, status=status.HTTP_204_NO_CONTENT)
+        else: 
+            return Response({'error': 'Objeto não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+
+class ListarFluxosView(BaseFluxoView):
+    def get(self, request):
+        fluxos = Fluxo.objects.all()
+        serializer = FluxoSerializer(fluxos, many=True) 
+        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
--- frontend/academic_dev_flow/src/services/api.js
+++ frontend/academic_dev_flow/src/services/api.js
@@ -4,4 +4,20 @@ const api = axios.create({
     baseURL: "http://localhost:8000"
 })
 
+
+// Adiciona um interceptor para incluir o token em todas as requisições
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // Substitua com a chave correta
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
+
