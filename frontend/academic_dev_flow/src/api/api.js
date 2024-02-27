5fbc33c16b4e9245c58ac6ee3d3db5918851c356
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-27 19:21:19 -0300

    modificando a localização da api do frontend #37

--- frontend/academic_dev_flow/src/api/api.js
+++ frontend/academic_dev_flow/src/api/api.js
@@ -4,11 +4,9 @@ const api = axios.create({
     baseURL: "http://localhost:8000"
 })
 
-
-// Adiciona um interceptor para incluir o token em todas as requisições
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token"); // Substitua com a chave correta
+    const token = localStorage.getItem("token"); 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
