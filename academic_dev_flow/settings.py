3900471da6848d471c6c75bdc4963f2f88509cbf
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-02-27 17:45:22 -0300

    refatorando o código de autentitação e permissão #37

--- academic_dev_flow/settings.py
+++ academic_dev_flow/settings.py
@@ -63,13 +63,13 @@ APPEND_SLASH = False
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
         'apps.api.authentication.JSONWebTokenAuthentication',
-
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        'apps.api.permissions.IsAdminUserOrReadOnly',  
     ],
+
 }
 
 ROOT_URLCONF = 'academic_dev_flow.urls'
--- apps/api/authorization.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsAdminUserOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        # Verifica se o usuário autenticado é um administrador
-        return request.user and request.user.is_authenticated and request.user.is_staff
