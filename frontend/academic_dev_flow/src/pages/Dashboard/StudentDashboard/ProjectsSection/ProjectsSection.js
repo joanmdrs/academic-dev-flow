9c4a5a478f2744debb708a1213b4add4e53ecf3d
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-03 17:45:46 -0300

    modificando a listagem dos projetos #37

--- frontend/academic_dev_flow/src/pages/Dashboard/StudentDashboard/ProjectsSection/ProjectsSection.js
+++ frontend/academic_dev_flow/src/pages/Dashboard/StudentDashboard/ProjectsSection/ProjectsSection.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import ProjectCard from '../components/ProjectCard/ProjectCard'; 
 import StudentMenu from '../../../../components/Menus/StudentMenu/StudentMenu';
-import { Layout, Tabs } from 'antd';
+import { Button, Layout, Space, Table, Tabs } from 'antd';
 import MyHeader from '../../../../components/Header/Header';
 import "./ProjectsSection.css"
+import CustomBreadcrumb from '../../../../components/Breadcrumb/Breadcrumb';
+import { LuCalendarCheck2, LuCalendarX2 } from "react-icons/lu";
+import { TeamOutlined } from '@ant-design/icons';
+import { CiCircleCheck } from "react-icons/ci";
+import { TiFlowChildren } from "react-icons/ti";
+import { PiEye } from "react-icons/pi";
+
 const projectsData = [
   {
+    key: 1,
     id: 1,
-    title: 'Projeto A',
+    nome: 'Projeto A',
     description: 'Descrição do Projeto A.',
     inicio: '27/02/2024',
     fim: '27/03/2024',
@@ -15,66 +23,106 @@ const projectsData = [
     team: 'Equipe A',
   },
   {
+    key: 2,
     id: 2,
-    title: 'Projeto B',
+    nome: 'Projeto B',
     description: 'Descrição do Projeto B.',
     inicio: '27/02/2024',
     fim: '27/03/2024',
     status: 'Em andamento',
     team: 'Equipe B',
   },
-  // Adicione mais projetos conforme necessário
 ];
 
-const TAB_ITEMS = [
+const columns = [
   {
-    key: '1',
-    label: 'p',
-    children: 'Content of Tab Pane 1',
+    title: 'Nome',
+    dataIndex: 'nome',
+    key: 'nome',
   },
   {
-    key: '2',
-    label: 'Tab 2',
-    children: 'Content of Tab Pane 2',
+    title: (
+      <>
+        <CiCircleCheck style={{color: "#fffff"}} /> Status
+      </>
+    ),
+    dataIndex: 'status',
+    key: 'status'
+
   },
   {
-    key: '3',
-    label: 'Tab 3',
-    children: 'Content of Tab Pane 3',
+    title: (
+      <>
+        <LuCalendarCheck2 /> Início
+      </>
+    ),
+    dataIndex: 'inicio',
+    key: 'inicio',
   },
-]
+  {
+    title: (
+      <>
+        <LuCalendarX2/> Fim
+      </>
+    ),
+    dataIndex: 'fim',
+    key: 'fim',
+  },
+  {
+    title: (
+      <>
+        <TeamOutlined /> Membros
+      </>
+    ),
+    dataIndex: 'membros',
+    key: 'membros'
+  },
+  {
+    title: (
+      <>
+        <TiFlowChildren/> Fluxo
+      </>
+    ),
+    dataIndex: 'fluxo',
+    key: 'fluxo'
+  },
+  {
+    title: 'Ações',
+    key: 'action',
+    render: (_, record) => (
+      <Button>
+        Visualizar
+      </Button>
+    ),
+
+  }
+];
+
+const breadcrumbRoutes = [
+  { title: 'Home', path: '/aluno/home' },
+  { title: 'Projetos', path: '/aluno/projetos' },
+];
 
 const ProjectsSection = () => {
-
-  const [isOpenProject, setIsOpenProject] = useState(false)
-  const [selectedProject, setSelectedProject] = useState(null);
-
-  const handleProjectClick = (project) => {
-    setSelectedProject(project);
-  };
 
   return (
     <React.Fragment>
         <StudentMenu />
             <Layout>
                 <MyHeader />
+                <CustomBreadcrumb routes={breadcrumbRoutes} />
                 <div className="projects-section-layout">
                     <div className='projects-section-title'>
                       <h2> Meus Projetos </h2>  
                     </div>
                     <div className='projects-section-content'> 
-                      {projectsData.map((project) => (
-                          <ProjectCard key={project.id} project={project} />
-                      ))}
+                      <Table 
+                        className='meus-projetos-table'
+                        dataSource={projectsData} 
+                        columns={columns} />
                     </div>
                     
                 </div>
-
-                { !isOpenProject && 
-                  <div className='projects-section-content-details'> 
-                      <Tabs defaultActiveKey="1" items={TAB_ITEMS}  tabPosition='left'/>;
-                  </div>
-                }
             </Layout>
     </React.Fragment>
     
