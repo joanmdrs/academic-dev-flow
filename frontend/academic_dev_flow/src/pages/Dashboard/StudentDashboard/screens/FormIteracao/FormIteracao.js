303680a01d7cc4bf52eeb615fe2b9f6870ac57d7
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-05 13:14:33 -0300

    modificando o form de cadastro de iterações #39

--- frontend/academic_dev_flow/src/pages/Dashboard/StudentDashboard/screens/FormIteracao/FormIteracao.js
+++ frontend/academic_dev_flow/src/pages/Dashboard/StudentDashboard/screens/FormIteracao/FormIteracao.js
@@ -6,6 +6,12 @@ import { buscarEtapaPeloId } from "../../../../../services/etapaService";
 import { listarMembrosPorProjeto } from "../../../../../services/membroProjetoService";
 import { buscarMembroPeloId } from "../../../../../services/membroService";
 
+const baseStyle = {
+    display: "flex",
+    gap: "20px"
+};
+
+
 const OPTIONS_STATUS = [
     {
         value: 'planejamento',
@@ -88,23 +94,22 @@ const FormIteracao = ({onSubmit, onCancel}) => {
                 <Form.Item>
                     <h4> CADASTRAR ITERAÇÃO </h4>
                 </Form.Item>
-                <Form.Item label="Nome" name="nome">
-                    <Input type='text' name="nome" />
-                </Form.Item>
+                <div style={{...baseStyle}}>
+                    <Form.Item label="Nome" name="nome" style={{ flex: "1"}}>
+                        <Input type='text' name="nome" placeholder="Ex.: Iteração 1" />
+                    </Form.Item>
 
-                <Form.Item label="Número" name="numero">
-                    <Input type="number" name="numero" />
-                </Form.Item> 
+                    <Form.Item label="Número" name="numero" >
+                        <Input type="number" name="numero" />
+                    </Form.Item> 
 
-                <Form.Item label="Descrição" name="descricao">
-                    <Input.TextArea  rows={4} name="descricao"/>
-                </Form.Item>
+                    <Form.Item label="Status" name="status" style={{ flex: "1"}}>
+                        <Select options={OPTIONS_STATUS} defaultValue="Selecione"/>
+                    </Form.Item>
 
-                <Form.Item label="Status" name="status">
-                    <Select options={OPTIONS_STATUS} defaultValue="Selecione"/>
-                </Form.Item>
+                </div>              
 
-                <div style={{display: "flex", gap: "20px"}}>
+                <div style={{...baseStyle}}>
                     <Form.Item label="Data de Início" name="data_inicio">
                         <Input type="date" name="data_inicio"/>
                     </Form.Item>
@@ -112,20 +117,32 @@ const FormIteracao = ({onSubmit, onCancel}) => {
                     <Form.Item label="Data de Término" name="data_fim">
                         <Input type="date" name="data_fim"/>
                     </Form.Item>
+
+                    <Form.Item label="Fase" name="fase" style={{ flex: "1"}}>
+                        <Select options={optionsEtapas} defaultValue="Selecione" />
+                    </Form.Item>
+
+                    <Form.Item label="Gerente" name="gerente" style={{ flex: "1"}}>
+                        <Select options={optionsMembros} defaultValue="Selecione" />
+                    </Form.Item>
                 </div>
 
-                <Form.Item label="Fase" name="fase">
-                    <Select options={optionsEtapas} defaultValue="Selecione" />
+                <Form.Item label="Descrição" name="descricao">
+                    <Input.TextArea  rows={4} name="descricao"/>
                 </Form.Item>
 
-                <Form.Item label="Gerente" name="gerente">
-                    <Select options={optionsMembros} defaultValue="Selecione" />
-                </Form.Item>
+                <div style={{...baseStyle}}> 
+                    <Form.Item>
+                        <Button type="primary"> Salvar </Button>
+                    </Form.Item>
 
-                <Form.Item style={{display: "flex", gap: "20px"}}>
-                    <Button type="primary"> Salvar </Button>
-                    <Button onClick={onCancel}> Cancelar </Button>
-                </Form.Item>
+                    <Form.Item>
+                        <Button> Cancelar </Button>
+                    </Form.Item>
+                </div>
+                
+
+                
 
             </Form>
         </div>
