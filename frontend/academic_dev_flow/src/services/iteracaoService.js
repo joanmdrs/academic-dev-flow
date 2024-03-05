e48a32f8df0d986bb1271dc81e105c145f1ec219
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-05 16:29:32 -0300

    definindo o service de criar uma iteração #39

--- frontend/academic_dev_flow/src/services/iteracaoService.js
+++ frontend/academic_dev_flow/src/services/iteracaoService.js
@@ -1,4 +1,23 @@
+import { NotificationManager } from "react-notifications";
 import api from "../api/api";
+
+export const criarIteracao = async (dados) => {
+    try {
+        const response = await api.post('iteracao/cadastrar/', dados)
+
+        if (response.status === 200){
+            NotificationManager.success('Iteração criada com sucesso !')
+            return response
+        }
+        
+    } catch (error) {
+        console.log(error)
+        NotificationManager.error('Falha na operação, contate o suporte !')
+    }
+
+    const response = await api.post('iteracao/cadastrar/', {dados})
+    return response
+}
 
 export const listarIteracoesPorProjeto = async (idProjeto) => {
 
