57c400374baf2c33c9f89ff7641c146d2cfbcaa6
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-07 10:54:31 -0300

    implementando uma view que busca as tarefas de determinado projeto e ainda busca os dados dos membros atribuídos para aquela tarefa #42

--- apps/tarefa/urls.py
+++ apps/tarefa/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('buscar/<int:id>/', BuscarTarefaPeloIdView.as_view(), name="buscar_tarefa_pelo_id"),
     path('listar/<int:id_projeto>/', ListarTarefasPorProjetoView.as_view(), name="listar_tarefas_por_projeto"),
     path('atualizar/<int:id>/', AtualizarTarefaView.as_view(), name="atualizar_tarefa"),
-    path('excluir/<int:id>/', ExcluirTarefaView.as_view(), name="excluir_tarefa")
+    path('excluir/<int:id>/', ExcluirTarefaView.as_view(), name="excluir_tarefa"),
+    path('listar_tarefas/<int:id_projeto>/', ListarTarefasView.as_view(), name='listar_tarefas')
 ]
--- apps/tarefa/views.py
+++ apps/tarefa/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from django.http import HttpResponseNotAllowed, JsonResponse
 from .models import Tarefa
 from .serializers import TarefaSerializer
+from apps.membro_projeto.models import MembroProjeto
+from apps.membro.models import Membro
 from apps.fluxo.models import Fluxo
 from rest_framework.permissions import IsAuthenticated
 from apps.api.permissions import IsAdminUserOrReadOnly 
@@ -38,6 +40,45 @@ class BuscarTarefaPeloIdView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
             
+class ListarTarefasView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    def get(self, request, id_projeto): 
+        try:
+            
+            tarefas = Tarefa.objects.filter(projeto_id=id_projeto)  # Buscamos as tarefas do projeto
+            tarefas_info = []
+
+            for tarefa in tarefas:
+                # Obtemos os dados dos objetos do relacionamento entre membro e projeto
+                membros_projeto = MembroProjeto.objects.filter(projeto_id=id_projeto, id__in=tarefa.membros.all())
+                
+                membros_info = []
+                
+                for membro_projeto in membros_projeto:
+                    membros_info.append({
+                        'id_membro_projeto': membro_projeto.id,
+                        'id_membro': membro_projeto.membro.id,
+                        'nome_membro': membro_projeto.membro.nome,
+                        'grupo_membro': membro_projeto.membro.grupo
+                    })
+
+                tarefas_info.append({
+                    'id': tarefa.id,
+                    'nome': tarefa.nome,
+                    'data_criacao': tarefa.data_criacao,
+                    'prazo': tarefa.prazo,
+                    'descricao': tarefa.descricao,
+                    'concluida': tarefa.concluida,
+                    'projeto': tarefa.projeto_id,
+                    'membros': membros_info,
+                })
+            return JsonResponse(tarefas_info, safe=False, json_dumps_params={'ensure_ascii': False})
+        
+                
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 class ListarTarefasPorProjetoView(APIView): 
     permission_classes = [IsAuthenticated]
     
