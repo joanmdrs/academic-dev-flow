f73ddf824590de12e2a73ff0517bf1b48037f4ac
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-07 14:22:01 -0300

    adicionando a função que alimenta as opções de membros a serem atribuídos a tarefa #42

--- frontend/academic_dev_flow/src/pages/Dashboard/Aluno/pages/Tarefas/FormTarefa/FormTarefa.js
+++ frontend/academic_dev_flow/src/pages/Dashboard/Aluno/pages/Tarefas/FormTarefa/FormTarefa.js
@@ -1,34 +1,66 @@
 import { Button, Form, Input, Select } from 'antd'
 import { useForm } from 'antd/es/form/Form'
 import React, { useEffect, useState } from 'react'
+import { useFormContext } from '../../../context/Provider/Provider';
+import { listarMembrosPeloIdProjeto } from '../../../../../../services/membroProjetoService';
 
 const baseStyle = {
     display: "flex",
-    gap: "20px"
+    gap: "20px",
+    flexDirection: "row"
 };
 
 
 const FormTarefa = () => {
 
     const [form] = useForm()
+    const {dadosProjeto} = useFormContext()
     const [optionsMembros, setOptionsMembro] = useState(null)
 
+    useEffect(() => {
+        const fetchData = async () => {
+            if (dadosProjeto != null ){
+                await handleGetMembros()
+                
+            }
+        }
+
+        fetchData()
+
+    }, [dadosProjeto])
+
+    const handleGetMembros = async () => {
+        const response = await listarMembrosPeloIdProjeto(dadosProjeto.id)
+
+        const resultados = response.data.map((item) => {
+
+            return {
+                value: item.id_membro_projeto,
+                label: `${item.nome_membro} (${item.grupo_membro})`
+            }
+        })
+
+        setOptionsMembro(resultados)
+    }
 
     return (
 
         <div>
-            <Form form={form} layout='vertical'>
+            <Form form={form} layout='vertical' className='global-form'>
+                <Form.Item>
+                    <h4> CADASTRAR TAREFA </h4>
+                </Form.Item>
 
-                <div style={{baseStyle}}> 
-                    <Form.Item label="Nome" name="nome">
+                <div style={{...baseStyle}}> 
+                    <Form.Item label="Nome" name="nome" style={{ flex: "1"}}>
                         <Input type='text' name='nome' />
                     </Form.Item>
 
-                    <Form.Item label="Prazo (em dias)" name="prazo">
+                    <Form.Item label="Prazo (em dias)" name="prazo" >
                         <Input type='number' name='prazo'/>
                     </Form.Item>
 
-                    <Form.Item label="Atribuir à" name="membros">
+                    <Form.Item label="Atribuir à" name="membros" style={{ flex: "1"}}>
                         <Select
                             mode="multiple"
                             allowClear
@@ -49,7 +81,7 @@ const FormTarefa = () => {
 
                 <Form.Item>
                     <Button type="primary" htmlType='submit'> Salvar </Button>
-                    <Button> Cancelar </Button>
+                    <Button style={{marginLeft: "10px"}}> Cancelar </Button>
                 </Form.Item>
             </Form>
         </div>
