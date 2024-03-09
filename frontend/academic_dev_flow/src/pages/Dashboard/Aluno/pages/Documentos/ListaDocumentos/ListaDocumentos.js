134a94711dc7801cbdb16938f2d976e0e56fc86c
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-09 10:27:10 -0300

    implementando a função que busca os documentos de determinado repositório por meio da api do github #41

--- frontend/academic_dev_flow/src/pages/Dashboard/Aluno/pages/Documentos/ListaDocumentos/ListaDocumentos.js
+++ frontend/academic_dev_flow/src/pages/Dashboard/Aluno/pages/Documentos/ListaDocumentos/ListaDocumentos.js
@@ -1,15 +1,66 @@
-import { Tabs } from "antd";
-import React from "react";
+// import { Tabs } from "antd";
+// import React, { useEffect, useState } from "react";
+// import { octokit } from "../../../../../../api/apiGitHubService";
+// import Markdown from 'react-markdown';
+// import remarkGfm from 'remark-gfm';
 
-const ListaDocumentos = () => {
+// const ListaDocumentos = () => {
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             await handleGetDocumentos();
+//         };
 
+//         fetchData();
+//     }, []);
 
-    return (
-        <div> 
-        </div>
-    )
+//     const [documentos, setDocumentos] = useState([]);
 
+//     const handleGetDocumentos = async () => {
+//         try {
+//             const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+//                 owner: 'joanmdrs',
+//                 repo: 'sigcli',
+//                 path: 'docs',
+//             });
 
-}
+//             if (response.status === 200) {
+//                 const promises = response.data.map(async (item) => {
+//                     const response1 = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+//                         owner: 'joanmdrs',
+//                         repo: 'sigcli',
+//                         path: item.path,
+//                     });
 
-export default ListaDocumentos
+//                     const decodedContent = decodeURIComponent(escape(atob(response1.data.content)));
+//                     console.log("Documento: ", decodedContent);
+
+//                     return { path: item.path, content: decodedContent }; // Retorna o conteúdo do documento
+//                 });
+
+//                 // Aguarde a resolução de todas as promessas antes de prosseguir
+//                 const results = await Promise.all(promises);
+//                 setDocumentos(results);
+//             }
+//         } catch (error) {
+//             console.error("Erro na solicitação:", error);
+//         }
+//     };
+
+//     return (
+//         <div>
+//             {/* Se quiser renderizar a lista de documentos */}
+//             <ul>
+//                 {documentos.map((doc, index) => (
+//                     <li key={index}>
+//                         <strong>{doc.path}</strong>: 
+//                         <Markdown remarkPlugins={[remarkGfm]}>{doc.content}</Markdown>
+//                     </li>
+//                 ))}
+//             </ul>
+
+            
+//         </div>
+//     );
+// };
+
+// export default ListaDocumentos;
