f0ed48aefbb95fb87bc6d01dcb8fbe7599fad96d
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-10 11:16:48 -0300

    criando um contexto global #41

---
+++ frontend/academic_dev_flow/src/context/ProjetoContext.js
@@ -0,0 +1,26 @@
+// src/context/ProjetoContext.js
+import React, { createContext, useContext, useState } from "react";
+
+const ProjetoContext = createContext();
+
+export const useProjetoContext = () => useContext(ProjetoContext);
+
+export const ProjetoProvider = ({ children }) => {
+  const [dadosProjeto, setDadosProjeto] = useState(null);
+  const [dadosIteracao, setDadosIteracao] = useState(null);
+  const [dadosTarefa, setDadosTarefa] = useState(null);
+  const [tarefasSelecionadas, setTarefasSelecionadas] = useState([]);
+
+  return (
+    <ProjetoContext.Provider
+      value={{
+        dadosProjeto, setDadosProjeto,
+        dadosIteracao, setDadosIteracao,
+        dadosTarefa, setDadosTarefa,
+        tarefasSelecionadas, setTarefasSelecionadas,
+      }}
+    >
+      {children}
+    </ProjetoContext.Provider>
+  );
+};
