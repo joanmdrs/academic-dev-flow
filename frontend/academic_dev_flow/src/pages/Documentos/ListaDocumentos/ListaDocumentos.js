1588a6d9b48ef7493c26bddb267fa1b6ca0f5c53
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-14 11:06:34 -0300

    definindo a lógica do componente lista de documentos #41

--- frontend/academic_dev_flow/src/pages/Documentos/ListaDocumentos/ListaDocumentos.js
+++ frontend/academic_dev_flow/src/pages/Documentos/ListaDocumentos/ListaDocumentos.js
@@ -1,11 +1,16 @@
-import { Button, Table, Tabs } from "antd";
-import React, { useEffect, useState } from "react";
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
+import { Button, Card, Empty, Table, Tabs } from "antd";
+import "./ListaDocumentos.css"
+import React, { useState } from "react";
 import { PiEye } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import { GoCommentDiscussion } from "react-icons/go";
 import { buscarDocumentos } from "../../../api/apiGitHubService"
+import { FcInfo } from "react-icons/fc";
+import { MdInfo } from "react-icons/md";
+import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
+import { IoClose } from "react-icons/io5";
+
+
 const ListaDocumentos = () => {
 
     const COLUNAS_TABELA_DOCUMENTOS = [
@@ -39,6 +44,7 @@ const ListaDocumentos = () => {
 
 
     const [documentos, setDocumentos] = useState([]);
+    const [primeiroAcesso, setPrimeiroAcesso] = useState(true)
     const [exibirDocumentos, setExibirDocumentos] = useState(false)
 
     const handleGetDocumentos = async () => {
@@ -61,12 +67,16 @@ const ListaDocumentos = () => {
             })
 
             setDocumentos(listaFormatada)
-            setExibirDocumentos(true)
 
 
             // const decodedContent = decodeURIComponent(escape(atob(response.data)));
             // setDocumentos(decodedContent)
-        }  
+        } 
+
+        setPrimeiroAcesso(false)
+        setExibirDocumentos(true)
+
+
     }
 
 
@@ -75,9 +85,47 @@ const ListaDocumentos = () => {
 
         <div>
 
-            <Button onClick={() => handleGetDocumentos()}> Listar Documentos </Button>
+            {primeiroAcesso && <Card
+                color="red"
+                bordered={false}
+                style={{
+                    width: "100%",
+                    marginBottom: "20px",
+                    backgroundColor: "#00BCD4",
+                    color: "#fff",
+                    padding: "0 !important",
 
-            { exibirDocumentos && (<Table columns={COLUNAS_TABELA_DOCUMENTOS} dataSource={documentos} />)}
+
+                }}
+            >
+                 <p> Para visualizar os documentos do seu projeto, criados no repositório do GitHub, clique no botão 'Listar Documentos'. </p>
+            </Card>}
+
+            
+            {   exibirDocumentos ? 
+                (<Button danger  icon={<IoClose/>} onClick={() => setExibirDocumentos(false)}> Fechar </Button>)
+
+                : (<Button type="primary" icon={<HiOutlineClipboardDocumentList/>} onClick={() => handleGetDocumentos()}> Listar Documentos </Button>)
+
+            }
+
+            
+
+            { exibirDocumentos ?
+
+                <>
+                    { documentos.length !== 0 ? 
+                        <Table className="table-lista-documentos" columns={COLUNAS_TABELA_DOCUMENTOS} dataSource={documentos} />
+                        :  <Empty 
+                            description="Não foram encontrados documentos para o seu projeto"
+                            image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                    
+                    }
+                </>
+                : null
+            }
+
+
 
            
         </div>
@@ -85,13 +133,3 @@ const ListaDocumentos = () => {
 };
 
 export default ListaDocumentos;
-
-//  {/* Se quiser renderizar a lista de documentos */}
-//  <ul>
-//  {documentos.map((doc, index) => (
-//      <li key={index}>
-//          <strong>{doc.path}</strong>: 
-//          <Markdown remarkPlugins={[remarkGfm]}>{doc.content}</Markdown>
-//      </li>
-//  ))}
-// </ul>
