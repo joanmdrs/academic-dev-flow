5b2dda7f0eae738f029cc8c9e50f427b468ff021
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-15 16:10:00 -0300

    implementando o menu de opções no componente VisualizarDocumento #41

--- frontend/academic_dev_flow/src/pages/Documentos/VisualizarDocumento/VisualizarDocumento.js
+++ frontend/academic_dev_flow/src/pages/Documentos/VisualizarDocumento/VisualizarDocumento.js
@@ -1,56 +1,100 @@
 import React, { useEffect, useState } from "react";
-import "./VisualizarDocumento.css"
+import "./VisualizarDocumento.css";
 import { buscarDocumentos } from "../../../api/apiGitHubService";
 import Markdown from "react-markdown";
-import remarkGfm from 'remark-gfm'
-import { Button } from "antd";
+import remarkGfm from 'remark-gfm';
+import { Button, Layout, Menu } from "antd";
 import BotaoVoltar from "../../../components/Botoes/BotaoVoltar/BotaoVoltar";
 import Loading from "../../../components/Loading/Loading";
+import { MenuUnfoldOutlined, MenuFoldOutlined, FileTextOutlined, StarOutlined, CommentOutlined } from '@ant-design/icons';
+
+const { Sider, Content } = Layout;
 
 const VisualizarDocumento = ({ documento, onBack }) => {
-
     const [decodedDoc, setDecodedDoc] = useState('');
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState('document');
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
 
     const handleGetDocumento = async (path) => {
-
-        const response = await buscarDocumentos(path)
-
+        const response = await buscarDocumentos(path);
         if (response.status === 200) {
             const decodedContent = decodeURIComponent(escape(atob(response.data.content)));
             setDecodedDoc(decodedContent);
         }
-
-    }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
-
             if (documento) {
-                await handleGetDocumento(documento.caminho)
+                await handleGetDocumento(documento.caminho);
             }
-            setLoading(false)
-        }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
 
-        fetchData()
-
-    }, [])
 
     if (loading) {
-        return <Loading />
+        return <Loading />;
     }
 
     return (
-        <React.Fragment>
-            {documento && 
-                <div className="global-form">
+        <Layout style={{ minHeight: '100vh' }} className="screen-view-document">
+            <Sider style={{marginRight: "20px"}} theme="light" collapsed={collapsed} className="global-form">
+                <Button
+                    type="primary"
+                    onClick={toggleCollapsed}
+                    style={{
+                    marginBottom: 16,
+                    }}
+                >
+                    {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                </Button>
+                <Menu defaultSelectedKeys={['1']} mode="inline" theme="light" inlineCollapsed={collapsed}>
+                    <Menu.Item key="1" icon={<FileTextOutlined />} onClick={() => setCurrentPage('document')}>
+                        Documento
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<StarOutlined />} onClick={() => setCurrentPage('score')}>
+                        Pontuação
+                    </Menu.Item>
+                    <Menu.Item key="3" icon={<CommentOutlined />} onClick={() => setCurrentPage('comments')}>
+                        Comentários
+                    </Menu.Item>
+                </Menu>
 
+            </Sider>
+
+                
+            <Layout className="site-layout" >
+                <Content className="global-form">
                     <BotaoVoltar funcao={onBack} />
-                    <Markdown className="conteudo-markdown" remarkPlugins={[remarkGfm]}>{decodedDoc}</Markdown>
-                </div>
-            }
-        </React.Fragment>
+
+                    {currentPage === 'document' && (
+                        <Markdown className="conteudo-markdown" remarkPlugins={[remarkGfm]}>
+                            {decodedDoc}
+                        </Markdown>
+                    )}
+                    {currentPage === 'score' && (
+                        <div>
+                            {/* Componente de Pontuação */}
+                            <h1>Aqui será a área de pontuação</h1>
+                        </div>
+                    )}
+                    {currentPage === 'comments' && (
+                        <div>
+                            {/* Componente de Comentários */}
+                            <h1>Aqui será a área de comentários</h1>
+                        </div>
+                    )}
+                </Content>
+            </Layout>
+        </Layout>
     );
-}
+};
 
 export default VisualizarDocumento;
