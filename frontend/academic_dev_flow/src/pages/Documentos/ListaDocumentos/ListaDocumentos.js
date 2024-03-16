17094e44acd30da4b32357fb620d83092b0a0237
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-16 13:53:45 -0300

    criando um novo arquivo que lista os documentos criados #41

--- frontend/academic_dev_flow/src/pages/Documentos/ListaDocumentos/ListaDocumentos.js
+++ frontend/academic_dev_flow/src/pages/Documentos/ListaDocumentos/ListaDocumentos.js
@@ -1,93 +1,15 @@
-import { Button, Empty, Table } from "antd";
-import "./ListaDocumentos.css"
-import React, { useState } from "react";
-import { PiEye } from "react-icons/pi";
-import { Link } from "react-router-dom";
-import { GoCommentDiscussion } from "react-icons/go";
-import { buscarDocumentos } from "../../../api/apiGitHubService";
-import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
-import { IoCloseCircleOutline } from "react-icons/io5";
-import { IoDocumentOutline } from "react-icons/io5";
-import { FcFolder } from "react-icons/fc";
-import VisualizarDocumento from "../VisualizarDocumento/VisualizarDocumento";
+import React from "react";
 
-const ListaDocumentos = ({docs, onSearchDocs}) => {
-    const [currentPage, setCurrentPage] = useState('default')
-    const [documentoSelecionado, setDocumentoSelecionado] = useState(null);
-    const [currentPath, setCurrentPath] = useState('docs');
+const ListaDocumentos = ({docs}) => {
 
-    const COLUNAS_TABELA_DOCUMENTOS = [
-        {
-            title: 'Nome',
-            dataIndex: 'nome',
-            key: 'nome',
-            render: (_, record) => (
-                <span style={{display: "flex", justifyContent: "baseline", gap: "10px"}}> 
-                    {record.tipo === "file" ? <IoDocumentOutline size="20px" /> : <FcFolder size="20px"/> }
 
-                    {record.nome}
-                </span>
-            ),
-        },
-        {
-            title: (
-                <> <GoCommentDiscussion/> Comentários  </>
-            ),
-            dataIndex: 'comments',
-            key: 'comments'
-        },
-        {
-            title: (
-              <>
-                <PiEye /> Ações
-              </>
-            ),
-            key: 'action',
-            render: (_, record) => (
-              <Button onClick={() => handleVisualizarDocumento(record)}>Visualizar</Button>
-            ),
-        }
-    ];
-
-    const handleVisualizarDocumento = (record) => {
-        if (record.tipo === "dir") {
-            setCurrentPath(`${currentPath}/${record.nome}`);
-            onSearchDocs(record.caminho);
-        } else {
-            setDocumentoSelecionado(record);
-            setCurrentPage('visualizar');
-        }
-    }
 
     return (
-
         <div> 
-
-            {currentPage === "default" && (
-
-                <React.Fragment>
-
-                    {
-                        docs.length !== 0 ? (
-                            <Table bordered="true" className="table-lista-documentos" columns={COLUNAS_TABELA_DOCUMENTOS} dataSource={docs} />
-                        ) : (
-                            <Empty description="Não foram encontrados documentos para o seu projeto" image={Empty.PRESENTED_IMAGE_SIMPLE} />
-                        ) 
-                    }
-
-                </React.Fragment>
-            )}
-
-            {currentPage === "visualizar" && (
-                <React.Fragment>
-                    {documentoSelecionado && (
-                        <VisualizarDocumento documento={documentoSelecionado} onBack={()=> setCurrentPage('default')} />
-                    )}
-                </React.Fragment>
-            )}
-                
+            
         </div>
-    );
-};
 
-export default ListaDocumentos;
+    )
+}
+
+export default ListaDocumentos
