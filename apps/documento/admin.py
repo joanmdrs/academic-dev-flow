4d81c959b04adad3b7c507d7a88e3003608c070c
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-16 10:28:15 -0300

    modificando o admin de documento #41

--- apps/documento/admin.py
+++ apps/documento/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Documento
 
 class DocumentoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_criacao')
+    list_display = ('titulo', 'status', 'caminho', 'data_criacao')
 
 admin.site.register(Documento, DocumentoAdmin)
