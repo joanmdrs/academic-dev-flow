e1d3ca926f5ae7754b0b409b4384907e19cbc0ae
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-19 08:42:24 -0300

    customizando o display de user no painel administrativo #43

--- apps/usuario/admin.py
+++ apps/usuario/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Usuario 
 
-admin.site.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')
+
+
+admin.site.register(Usuario, UsuarioAdmin)
