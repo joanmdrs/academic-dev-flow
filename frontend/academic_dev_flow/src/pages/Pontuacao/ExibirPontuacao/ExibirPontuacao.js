a14450532649c6d64af68efe46578c7e63f381e0
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-23 17:31:03 -0300

    finalizando a implementação da seção que gerencia a pontuação dos documentos criados no projeto #43

--- frontend/academic_dev_flow/src/pages/Pontuacao/ExibirPontuacao/ExibirPontuacao.js
+++ frontend/academic_dev_flow/src/pages/Pontuacao/ExibirPontuacao/ExibirPontuacao.js
@@ -1,42 +1,61 @@
-import "./ExibirPontuacao.css"
+import "./ExibirPontuacao.css";
 import React from "react";
 import { useProjetoContext } from "../../../context/ProjetoContext";
-import { Button } from "antd";
+import { Button, Empty } from "antd";
 import { FaTrash } from "react-icons/fa6";
 import { FaEdit } from "react-icons/fa";
 
-const ExibirPontuacao = ({onEdit, onDelete}) => {
+const ExibirPontuacao = ({ onEdit, onDelete }) => {
+  const { dadosPontuacao, userGroup } = useProjetoContext();
 
-    const {dadosPontuacao, userGroup} = useProjetoContext()
-
-    return (
-
-        <div className="global-form">
-            {   userGroup === "Professores" && (
-                    <div className="score-buttons"> 
-                        <Button onClick={onEdit} type="primary" ghost> <FaEdit /> Editar </Button>
-                        <Button onClick={onDelete} danger> <FaTrash /> Excluir </Button>
-                    </div>
-                )  
-            }
-            <div className="score-container">
-                <div className="score-note"> 
-                    <h2> Nota: {dadosPontuacao.nota} </h2> 
-                </div>
-                <div className="score-comment">
-                    <h4>Comentário(s):</h4> 
-                    <p> {dadosPontuacao.comentario} </p>
-                    
-                </div>
-                <div className="score-autor"> 
-                    <h4> Professor: <span style={{fontWeight: '400'}}> {dadosPontuacao.nome_autor}  </span></h4> 
-                </div>
+  return (
+    <div className="global-form">
+      {dadosPontuacao !== null && (
+        <div>
+          {userGroup === "Professores" && (
+            <div className="score-buttons">
+              <Button onClick={onEdit} type="primary" ghost>
+                {" "}
+                <FaEdit /> Editar{" "}
+              </Button>
+              <Button onClick={onDelete} danger>
+                {" "}
+                <FaTrash /> Excluir{" "}
+              </Button>
             </div>
+          )}
+
+          {(userGroup === "Alunos" && dadosPontuacao.disponivel) ||
+          userGroup === "Professores" ? (
+            <div className="score-container">
+              <div className="score-note">
+                <h2> Nota: {dadosPontuacao.nota} </h2>
+              </div>
+              <div className="score-comment">
+                <h4>Comentário(s):</h4>
+                <p> {dadosPontuacao.comentario} </p>
+              </div>
+              <div className="score-autor">
+                <h4>
+                  {" "}
+                  Professor:{" "}
+                  <span style={{ fontWeight: "400" }}>
+                    {" "}
+                    {dadosPontuacao.nome_autor}{" "}
+                  </span>
+                </h4>
+              </div>
+            </div>
+          ) : (
+            <Empty
+              description="As notas não estão disponíveis"
+              image={Empty.PRESENTED_IMAGE_SIMPLE}
+            />
+          )}
         </div>
+      )}
+    </div>
+  );
+};
 
-        
-        
-    )
-}
-
-export default ExibirPontuacao
+export default ExibirPontuacao;
--- frontend/academic_dev_flow/src/pages/Pontuacao/index.js
+++ frontend/academic_dev_flow/src/pages/Pontuacao/index.js
@@ -1,71 +1,134 @@
 import React, { useEffect, useState } from "react";
 import FormPontuacao from "./FormPontuacao/FormPontuacao";
 import { useProjetoContext } from "../../context/ProjetoContext";
-import { atualizarDocumento, buscarDocumentoPeloId } from "../../services/documentoService";
+import {
+  atualizarDocumento,
+  buscarDocumentoPeloId,
+} from "../../services/documentoService";
 import ExibirPontuacao from "./ExibirPontuacao/ExibirPontuacao";
-import { atualizarPontuacao, buscarPontuacaoPeloId, registrarPontuacao } from "../../services/pontuacaoService";
+import {
+  atualizarPontuacao,
+  buscarPontuacaoPeloId,
+  excluirPontuacao,
+  registrarPontuacao,
+} from "../../services/pontuacaoService";
 import Loading from "../../components/Loading/Loading";
-import { Empty } from "antd";
+import { Empty, Modal } from "antd";
 
 const GerenciarPontuacao = () => {
-    const { dadosDocumento, dadosPontuacao, setDadosPontuacao, autor, userGroup } = useProjetoContext();
-    const [loading, setLoading] = useState(true);
-    const [actionForm, setActionForm] = useState('create')
+  const {
+    dadosDocumento,
+    setDadosDocumento,
+    dadosPontuacao,
+    setDadosPontuacao,
+    autor,
+    userGroup,
+  } = useProjetoContext();
+  const [loading, setLoading] = useState(true);
+  const [actionForm, setActionForm] = useState("create");
 
-    const handleHasScore = async () => {
-        const response = await buscarDocumentoPeloId(dadosDocumento.id);
-        const idScore = response.data.pontuacao;
-        if (idScore) {
-            const response1 = await buscarPontuacaoPeloId(idScore);
-            setDadosPontuacao(response1.data);
-        }
-    };
-
-    useEffect(() => {
-        const fetchData = async () => {
-            if (dadosDocumento) {
-                await handleHasScore();
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, [dadosDocumento, loading]);
-
-    const handleEdit = () => {
-        setActionForm('update')
+  const handleHasScore = async () => {
+    const response = await buscarDocumentoPeloId(dadosDocumento.id);
+    const idScore = response.data.pontuacao;
+    if (idScore) {
+      const response1 = await buscarPontuacaoPeloId(idScore);
+      setDadosPontuacao(response1.data);
     }
+  };
 
-    const handleSavePontuacao = async (dados) => {
+  const handleRefreshDocument = async () => {
+    const response = await buscarDocumentoPeloId(dadosDocumento.id)
+    setDadosDocumento(response.data)
+  }
 
-        if (actionForm === 'create') {
-            dados['autor'] = autor;
-            const response = await registrarPontuacao(dados);
-            const newData = { pontuacao: response.data.id };
-            await atualizarDocumento(dadosDocumento.id, newData);
-           
-        } else if (actionForm === 'update'){
-            await atualizarPontuacao(dadosPontuacao.id, dados)
-            setActionForm('create')
-        }
-        setLoading(true)
+  useEffect(() => {
+    const fetchData = async () => {
+      if (dadosDocumento) {
         await handleHasScore();
+        setLoading(false);
+      }
     };
+    fetchData();
+  }, [dadosDocumento, loading]);
 
-    if (loading) {
-        return <Loading />;
+  const handleReload = async () => {
+    setLoading(true)
+    await handleRefreshDocument()
+    await handleHasScore()
+  }
+
+  const handleEdit = () => {
+    setActionForm("update");
+  };
+
+  const handleCreateScore = async (dados) => {
+    dados["autor"] = autor;
+    const response = await registrarPontuacao(dados);
+    const newData = { pontuacao: response.data.id };
+    await atualizarDocumento(dadosDocumento.id, newData);
+  }
+
+  const handleUpdateScore = async (dados) => {
+    await atualizarPontuacao(dadosPontuacao.id, dados);
+    setActionForm("create");
+  }
+
+  const handleSaveScore = async (dados) => {
+    if (actionForm === "create") {
+        await handleCreateScore(dados)
+    } else if (actionForm === "update") {
+        await handleUpdateScore(dados)
     }
+    await handleReload()
+  };
 
-    return (
+  const handleDeleteScore = async () => {
+    await excluirPontuacao(dadosPontuacao.id)
+    setDadosPontuacao(null)
+    await handleReload()
+  }
+
+const handleDelete = () => {
+    Modal.confirm({
+        title: 'Confirmar exclusão',
+        content: 'Tem certeza que deseja excluir a pontuação ?',
+        okText: 'Sim',
+        cancelText: 'Não',
+        onOk: async () =>  await handleDeleteScore()
+    });
+};
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      {userGroup === "Alunos" ? (
         <div>
-            {userGroup === 'Alunos' ? (
-                <div>
-                    {(dadosDocumento.pontuacao !== undefined && dadosDocumento.pontuacao !== null)  ? <ExibirPontuacao /> : <Empty description="O professor ainda não registrou notas." image={Empty.PRESENTED_IMAGE_SIMPLE} />}
-                </div>
-            ) : (
-                <div>{(dadosDocumento.pontuacao !== undefined && dadosDocumento.pontuacao !== null && actionForm !== 'update' ) ? <ExibirPontuacao onEdit={handleEdit} /> : <FormPontuacao onSubmit={handleSavePontuacao} />}</div>
-            )}
+          {dadosDocumento.pontuacao !== undefined &&
+          dadosDocumento.pontuacao !== null ? (
+            <ExibirPontuacao />
+          ) : (
+            <Empty
+              description="O professor ainda não registrou notas."
+              image={Empty.PRESENTED_IMAGE_SIMPLE}
+            />
+          )}
         </div>
-    );
+      ) : (
+        <div>
+          {dadosDocumento.pontuacao !== undefined &&
+          dadosDocumento.pontuacao !== null &&
+          actionForm !== "update" ? (
+            <ExibirPontuacao onEdit={handleEdit} onDelete={handleDelete} />
+          ) : (
+            <FormPontuacao onSubmit={handleSaveScore} />
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default GerenciarPontuacao;
