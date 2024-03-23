260ad94265bf6a224d80899e479720854bbef793
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-23 15:39:35 -0300

    modificando a lógica de implementação dos componentes da seção pontuação #43

--- frontend/academic_dev_flow/src/pages/Pontuacao/index.js
+++ frontend/academic_dev_flow/src/pages/Pontuacao/index.js
@@ -4,33 +4,41 @@ import { useProjetoContext } from "../../context/ProjetoContext";
 import { atualizarDocumento, buscarDocumentoPeloId } from "../../services/documentoService";
 import ExibirPontuacao from "./ExibirPontuacao/ExibirPontuacao";
 import { buscarPontuacaoPeloId, registrarPontuacao } from "../../services/pontuacaoService";
+import Loading from "../../components/Loading/Loading";
 
 const GerenciarPontuacao = () => {
 
     const {dadosDocumento, setDadosPontuacao, autor} = useProjetoContext()
     const [hasScore, setHasScore] = useState(false)
     const [actionForm, setActionForm] = useState('create')
+    const [loading, setLoading] = useState(true)
+
+    const handleHasScore = async () => {
+        const response = await buscarDocumentoPeloId(dadosDocumento.id)
+        const idScore = response.data.pontuacao
+
+        console.log(idScore)
+        if (idScore !== null && idScore !== undefined) { 
+
+            const response1 = await buscarPontuacaoPeloId(idScore)
+            console.log(response1.data)
+            setDadosPontuacao(response1.data)
+            setHasScore(true)
+        }
+    }
 
     useEffect (() => {
         const fetchData = async () => {
             if (dadosDocumento){
-                await handleHasScore(dadosDocumento.id)
+                await handleHasScore()
+                setLoading(false)
             }
         }
-
         fetchData()
-    }, [])
+    }, [dadosDocumento])
 
-    const handleHasScore = async (id) => {
-        const response = await buscarDocumentoPeloId(id)
-        const idScore = response.data.pontuacao
-
-        if (idScore !== null && idScore !== undefined) { 
-
-            setHasScore(true)
-            const response1 = await buscarPontuacaoPeloId(idScore)
-            setDadosPontuacao(response1.data)
-        }
+    if (loading) {
+        return <Loading />
     }
 
     const handleCreateScore = async (dados) => {
@@ -45,6 +53,8 @@ const GerenciarPontuacao = () => {
             const newData = { pontuacao: response.data.id }
             await atualizarDocumento(dadosDocumento.id, newData)
         } 
+
+        await handleHasScore()
     }
 
     return (
@@ -57,11 +67,8 @@ const GerenciarPontuacao = () => {
                     <FormPontuacao onSubmit={handleSavePontuacao}/>
 
                 )
-
             }
             
-
-
         </div>
     )
 
