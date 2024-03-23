a5df2b7378d654319de11509d46d9c722a94c8d7
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-23 14:26:55 -0300

    finalizando o FormPontuacao #43

--- frontend/academic_dev_flow/src/pages/Pontuacao/FormPontuacao/FormPontuacao.js
+++ frontend/academic_dev_flow/src/pages/Pontuacao/FormPontuacao/FormPontuacao.js
@@ -1,26 +1,34 @@
 import { Button, Checkbox, Form, Input } from "antd";
 import React from "react";
+import { useProjetoContext } from "../../../context/ProjetoContext";
 
 const FormPontuacao = ({onSubmit, onCancel}) => {
     
-
+    const {dadosDocumento} = useProjetoContext()
     return (
-        <Form layout="vertical" onFinish={onSubmit}>
+        <Form className="global-form" onFinish={onSubmit}>
+
+            <Form.Item>
+                <h4 style={{margin: 0}}> 
+                    REGISTRAR PONTUAÇÃO - <span style={{color: "var(--primary-color)"}}>{dadosDocumento.titulo} </span>
+                </h4>
+            </Form.Item>
+
             <Form.Item label="Nota" name="nota">
-                <Input name="nota" />
+                <Input name="nota" style={{width: 'fit-content'}} />
             </Form.Item>
 
-            <Form.Item label="Comentário" name="comentario">
-                <Input name="comentario" />
+            <Form.Item label="Comentário" name="comentario"  >
+                <Input.TextArea name="comentario" rows={4} style={{width: '50%'}} />
             </Form.Item>
 
-            <Form.Item label="Disponibilizar" name="disponivel">
+            <Form.Item label="Liberar" name="disponivel">
                 <Checkbox name="disponivel" />
             </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit"> Salvar </Button>
-                <Button onClick={onCancel}> Cancelar </Button>
+            <Form.Item >
+                <Button style={{marginRight: '10px'}} type="primary" htmlType="submit"> Salvar </Button>
+                <Button  onClick={onCancel}> Cancelar </Button>
             </Form.Item>
         </Form>
     )
