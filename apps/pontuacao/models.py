0bbd1c24fb826700078bb19252453d7276f5d39c
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-23 09:24:20 -0300

    definindo o serializer e modificando o model #43

--- apps/pontuacao/models.py
+++ apps/pontuacao/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.membro_projeto.models import MembroProjeto
+from django.utils import timezone
 
 class Pontuacao(models.Model):
     
@@ -7,7 +8,7 @@ class Pontuacao(models.Model):
     data_atribuicao = models.DateTimeField(default=timezone.now)
     comentario = models.TextField(blank=True, null=True)
     autor = models.ForeignKey(MembroProjeto, on_delete=models.CASCADE)
-
+    disponivel = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Nota: {self.nota} - Data de Atribuição: {self.data_atribuicao}"
