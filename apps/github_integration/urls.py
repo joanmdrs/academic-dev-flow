948f044059db6942fda5164e3a3805b60f86cdce
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-26 14:07:15 -0300

    implementando a função que lista os commits de determinado usuário #45

--- apps/github_integration/urls.py
+++ apps/github_integration/urls.py
@@ -2,11 +2,13 @@
 
 from django.urls import path
 from . import views
+from .views import *
 
 app_name = "github_integration"
 
 urlpatterns = [
     path('list_repositories/', views.list_user_repositories, name='list_repositories'),
     path('list_issues/', views.list_issues, name='list_issues'),
-    path('create_issue/', views.create_issue, name='create_issue')
+    path('create_issue/', views.create_issue, name='create_issue'),
+    path('list_commits_by_user/', views.user_commits_view, name='list_commits')
 ]
--- apps/github_integration/views.py
+++ apps/github_integration/views.py
@@ -8,11 +8,11 @@ from django.http import HttpResponse
 
 
 def list_user_repositories(request):
-    github_client = get_github_client()  # Obtenha o cliente PyGithub
-    user = github_client.get_user()  # Obtenha o usuário autenticado
-    repositories = [repo.name for repo in user.get_repos()]  # Obtenha os nomes dos repositórios
+    github_client = get_github_client() 
+    user = github_client.get_user()  
+    repositories = [repo.name for repo in user.get_repos()]  
 
-    return JsonResponse({'repositories': repositories})  # Retorne os nomes dos repositórios em formato JSON
+    return JsonResponse({'repositories': repositories}) 
 
 def create_issue(request):
     try:
@@ -38,11 +38,32 @@ def list_issues(request):
                 'title': issue.title,
                 'body': issue.body,
                 'created_at': issue.created_at.isoformat(),
-                # Adicione outros campos conforme necessário
             })
         
         return JsonResponse(issues_list, safe=False, status=status.HTTP_200_OK)
     
     
+    except GithubException as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+def user_commits_view(request):
+    try:
+        g = get_github_client()
+
+        repo = g.get_repo("joanmdrs/sigcli")
+
+        user = g.get_user("allangbr")
+
+        user_commits = []
+        for commit in repo.get_commits(author=user):
+            user_commits.append({
+                'sha': commit.sha,
+                'message': commit.commit.message,
+                'author': commit.commit.author.name,
+                'date': commit.commit.author.date.isoformat()
+            })
+
+        return JsonResponse(user_commits, safe=False)
+
     except GithubException as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
