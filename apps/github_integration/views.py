da4905b6a68a621aaf2067738de860eafb359000
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-03-26 10:55:45 -0300

    criando uma view que lista os repositórios com base na instância client do github #45

--- apps/github_integration/views.py
+++ apps/github_integration/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .github_auth import get_github_client
 
-# Create your views here.
+def list_user_repositories(request):
+    github_client = get_github_client()  # Obtenha o cliente PyGithub
+    user = github_client.get_user()  # Obtenha o usuário autenticado
+    repositories = [repo.name for repo in user.get_repos()]  # Obtenha os nomes dos repositórios
+
+    return JsonResponse({'repositories': repositories})  # Retorne os nomes dos repositórios em formato JSON
