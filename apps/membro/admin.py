04c86a4c5a11f2146a33b0179c3b22ae0e087904
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-01 14:23:33 -0300

    modificando o model do app membro #47

--- apps/membro/admin.py
+++ apps/membro/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Membro
 
 class MembroAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'cpf', 'data_nascimento', 'sexo', 'telefone', 'email')
+    list_display = ('id', 'nome', 'data_nascimento', 'telefone', 'email')
     search_fields = ('nome',)
     
 admin.site.register(Membro, MembroAdmin)
--- apps/membro/models.py
+++ apps/membro/models.py
@@ -4,11 +4,12 @@ from apps.usuario.models import Usuario
 class Membro(models.Model):
     
     nome = models.CharField(max_length=200)
-    cpf = models.CharField(max_length=20)
     data_nascimento = models.DateField("Data de nascimento")
-    sexo = models.CharField(max_length=20)
     telefone = models.CharField(max_length=20)
     email = models.EmailField(max_length=200)
+    github = models.CharField(max_length=20, null=True, blank=True)
+    linkedin = models.CharField(max_length=200, null=True, blank=True)
+    lattes = models.CharField(max_length=200, null=True, blank=True)
     grupo = models.CharField(max_length=100, blank=True, null=True)
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, null=True)
     
