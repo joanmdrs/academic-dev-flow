cf0105910d71b05bd02d2af4be0e879110f0bd13
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-02 17:02:13 -0300

    modificando o menu admin #47

--- frontend/academic_dev_flow/src/components/Menus/MenuAdmin/MenuAdmin.js
+++ frontend/academic_dev_flow/src/components/Menus/MenuAdmin/MenuAdmin.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Menu } from 'antd';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
 import { HiOutlineClipboardList } from "react-icons/hi";
 import { BiGroup } from "react-icons/bi";
 import { SiAzureartifacts } from "react-icons/si";
-import Sider from 'antd/es/layout/Sider';
+import Sider from "antd/es/layout/Sider";
 import { RiFlowChart } from "react-icons/ri";
 
 const { SubMenu } = Menu;
@@ -13,32 +13,35 @@ const MenuAdmin = () => {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <Sider width={250} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-      <div className="demo-logo-vertical">
-        Academic Dev Flow
-      </div>
+    <Sider
+      width={250}
+      collapsible
+      collapsed={collapsed}
+      a
+      onCollapse={(value) => setCollapsed(value)}
+    >
+      <div className="demo-logo-vertical">Academic Dev Flow</div>
       <Menu
         theme="dark"
         mode="inline"
         style={{
-          marginTop: "20px"
+          marginTop: "20px",
         }}
       >
         <Menu.Item
-          className='item-menu'
-          key='projeto'
-          icon={<HiOutlineClipboardList style={{ fontSize: '20px' }} />}
+          className="item-menu"
+          key="projeto"
+          icon={<HiOutlineClipboardList style={{ fontSize: "20px" }} />}
         >
-          <Link to='/admin/projetos'>Projetos</Link>
+          <Link to="/admin/projetos">Projetos</Link>
         </Menu.Item>
-        
+
         <SubMenu
-          className='item-menu'
+          className="item-menu"
           key="fluxo"
-          icon={<RiFlowChart style={{ fontSize: '20px' }} />}
+          icon={<RiFlowChart style={{ fontSize: "20px" }} />}
           title="Fluxos"
         >
-
           <Menu.Item key="gerenciar-fluxos">
             <Link to="/admin/fluxos/gerenciar">Gerenciar fluxos</Link>
           </Menu.Item>
@@ -46,25 +49,30 @@ const MenuAdmin = () => {
           <Menu.Item key="etapas">
             <Link to="/admin/etapas">Gerenciar etapas</Link>
           </Menu.Item>
+        </SubMenu>
 
+        <SubMenu
+          className="item-menu"
+          key="membro"
+          icon={<BiGroup style={{ fontSize: "20px" }} />}
+          title="Membros"
+        >
+          <Menu.Item key="gerenciar-fluxos">
+            <Link to="/admin/membros/gerenciar">Gerenciar membros</Link>
+          </Menu.Item>
+
+          <Menu.Item key="etapas">
+            <Link to="/admin/membros/vincular-projeto">Vincular projeto</Link>
+          </Menu.Item>
         </SubMenu>
 
         <Menu.Item
-          className='item-menu'
-          key="membro"
-          icon={<BiGroup style={{ fontSize: '20px' }} />}
+          className="item-menu"
+          key="artefato"
+          icon={<SiAzureartifacts style={{ fontSize: "15px" }} />}
         >
-          <Link to="/admin/membros">Membros</Link>
+          <Link to="/admin/artefatos">Artefatos</Link>
         </Menu.Item>
-
-        <Menu.Item
-          className='item-menu'
-          key='artefato'
-          icon={<SiAzureartifacts style={{ fontSize: '15px' }} />}
-        >
-          <Link to='/admin/artefatos'>Artefatos</Link>
-        </Menu.Item>
-
       </Menu>
     </Sider>
   );
