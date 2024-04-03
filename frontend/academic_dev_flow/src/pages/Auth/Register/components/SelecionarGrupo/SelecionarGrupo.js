61cecd4a11b0a2f69dff6f16ba290cb823ae1e95
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-03 14:38:50 -0300

    criando a screen SelecionarGrupo #47

---
+++ frontend/academic_dev_flow/src/pages/Auth/Register/components/SelecionarGrupo/SelecionarGrupo.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./SelecionarGrupo.css"
+import { Button } from "antd";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { useRegisterContexto } from "../../context/RegisterContexto";
+
+
+const SelecionarGrupo = () => {
+
+    const [itemSelecionado, setItemSelecionado] = useState(null);
+    const {setGrupoUsuario} = useRegisterContexto()
+
+    const handleSelecionarGrupo = (id) => { 
+
+        if (id === itemSelecionado) {
+            setItemSelecionado(null)
+        } else {
+            setItemSelecionado(id);
+        }
+    };
+
+    const handleProsseguir = () => {
+        setGrupoUsuario(itemSelecionado)
+    }
+
+    return (
+        <div className="screen-selecionar-grupo">
+
+            <div>
+                <h2> Qual o seu perfil de usuário ? </h2>
+            </div>
+
+            <div className="grupos">
+                <div 
+                    id="Docentes" 
+                    onClick={() => handleSelecionarGrupo("Docentes")} 
+                    className={`item-grupo ${itemSelecionado === "Docentes" ? "item-selecionado" : ""}`}> 
+                    Professor 
+                </div>
+                <div 
+                    id="Discentes" 
+                    onClick={() => handleSelecionarGrupo("Discentes")} 
+                    className={`item-grupo ${itemSelecionado === "Discentes" ? "item-selecionado" : ""}`}> 
+                    Estudante 
+                </div>
+                <div 
+                    id="Colaboradores" 
+                    onClick={() => handleSelecionarGrupo("Colaboradores")} 
+                    className={`item-grupo ${itemSelecionado === "Colaboradores" ? "item-selecionado" : ""}`}> 
+                    Colaborador 
+                </div>
+            </div>
+
+            {itemSelecionado && (
+                <div>
+                <Button type="primary" onClick={handleProsseguir}>
+                    Prosseguir <IoIosArrowRoundForward />
+                </Button>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default SelecionarGrupo
