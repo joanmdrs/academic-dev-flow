6c8bd23722506f063b7a624fabfaab8bd0d6353f
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-06 18:05:52 -0300

    organizando o FormTipo #49

--- frontend/academic_dev_flow/src/pages/Tipo/components/FormTipo/FormTipo.js
+++ frontend/academic_dev_flow/src/pages/Tipo/components/FormTipo/FormTipo.js
@@ -1,43 +1,64 @@
 import React, { useEffect, useState } from 'react'
 import {Form, Input, Button} from 'antd'
 import { FaArrowsRotate } from "react-icons/fa6";
+import { gerarCorAleatoria, lightenDarkenColor } from '../../../../services/utils';
 
 const FormTipo = () => {
 
     const [cor, setCor] = useState('');
+    const [corClara, setCorClara] = useState('')
 
     useEffect(() => {
         const fetchData = async () => {
-            gerarCorAleatoria()
+            handleGerarCor()
         }
 
         fetchData()
     }, [])
 
-    const gerarCorAleatoria = () => {
-        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        setCor(randomColor);
-    };
+    const handleGerarCor = () => {
+        const color = gerarCorAleatoria()
+        const lightColor = lightenDarkenColor(color, 60)
+        setCor(color)
+        setCorClara(lightColor)
+    }
+
+
+    const handleAlterarCor = ({target: {value}}) => {
+        setCor(value)
+    }
 
     return (
-        <Form>
-            <Form.Item label="Nome" name="nome">
-                <Input type='text' name='nome'/> 
-            </Form.Item>
+        <Form className='global-form' layout='vertical'>
 
-            <Form.Item label='Descrição' name='descricao'>
-                <Input type='text' name='descricão' />
-            </Form.Item>
+            <div style={{display: 'flex', gap: '20px', alignItems: 'flex-end'}}> 
+                <Form.Item label="Nome" name="nome">
+                    <Input type='text' name='nome'/> 
+                </Form.Item>
 
-            <Form.Item label='Cor' name='cor'>
-                <Button style={{backgroundColor: `${cor}`}} onClick={gerarCorAleatoria}> <FaArrowsRotate /> </Button>
-                <Input type='text' name='cor' value={cor}/>
-            </Form.Item>
+                <Form.Item label='Descrição' name='descricao'>
+                    <Input type='text' name='descricão' />
+                </Form.Item>
 
-            <Form.Item> 
-                <Button type='primary' htmlType='submit'> Salvar </Button>
-                <Button type='dashed'> Cancelar </Button>
-            </Form.Item>
+                <div style={{display: 'flex', alignItems: 'flex-end', gap: '10px'}}> 
+                    <Form.Item label='Cor'>
+                        <Button 
+                            style={{backgroundColor: `${cor}`, color: `${corClara}`, border: `1px solid ${corClara}`}} 
+                            onClick={handleGerarCor}
+                        > 
+                            <FaArrowsRotate /> 
+                        </Button>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input type='text' name='cor' value={cor} onChange={handleAlterarCor}/>
+                    </Form.Item>
+                </div>
+
+                <Form.Item> 
+                    <Button type='primary' htmlType='submit'> Salvar </Button>
+                    <Button type='dashed'> Cancelar </Button>
+                </Form.Item>
+            </div>
 
         </Form>
     )
--- frontend/academic_dev_flow/src/services/utils.js
+++ frontend/academic_dev_flow/src/services/utils.js
@@ -38,3 +38,27 @@ export const handleError = (error, errorMessage) => {
   NotificationManager.error(errorMessage);
   return { error: errorMessage };
 };
+
+export const gerarCorAleatoria = () => {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor
+};
+
+export const lightenDarkenColor = (col, amt) => {
+  var usePound = false;
+  if (col[0] === '#') {
+      col = col.slice(1);
+      usePound = true;
+  }
+  var num = parseInt(col, 16);
+  var r = (num >> 16) + amt;
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+  var b = ((num >> 8) & 0x00FF) + amt;
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+  var g = (num & 0x0000FF) + amt;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
+}
