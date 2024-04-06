cd86d99532b5e92147b02ec7d9e02fe2841b5981
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-06 15:52:54 -0300

    criando as mensagens padrão de notificações no sistema #49

---
+++ frontend/academic_dev_flow/src/services/messages.js
@@ -0,0 +1,13 @@
+// Mensagens de sucesso 
+
+export const SUCCESS_MESSAGE_ON_CREATION = "Cadastro realizado com sucesso!"
+export const SUCCESS_MESSAGE_ON_UPDATE = "Informações atualizadas com sucesso!"
+export const SUCCESS_MESSAGE_ON_DELETION = "Exclusão realizada com sucesso!"
+
+// Mensagens de erro 
+export const ERROR_MESSAGE_DEFAULT = "A operação falhou, contate o suporte!"
+export const ERROR_MESSAGE_ON_SEARCHING = "Falha ao buscar os dados, contate o suporte!"
+export const ERROR_MESSAGE_ON_CREATION = "Falha durante o cadastro, contate o suporte!"
+export const ERROR_MESSAGE_ON_UPDATE = "Falha ao atualizar as informações, contate o suporte!"
+export const ERROR_MESSAGE_ON_DELETION = "Falha durante a exclusão, contate o suporte!"
+
