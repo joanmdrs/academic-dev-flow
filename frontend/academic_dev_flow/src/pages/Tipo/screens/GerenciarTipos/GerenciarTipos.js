f01e1f7dbb118809b836835fde7bc5072b909d55
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-06 19:44:21 -0300

    definindo a estrutura básica da pageGerenciarTipos #49

--- frontend/academic_dev_flow/src/pages/Tipo/screens/GerenciarTipos/GerenciarTipos.js
+++ frontend/academic_dev_flow/src/pages/Tipo/screens/GerenciarTipos/GerenciarTipos.js
@@ -1,12 +1,45 @@
 import React from "react";
 import FormTipo from "../../components/FormTipo/FormTipo";
+import Titulo from "../../../../components/Titulo/Titulo";
+import BotaoBuscar from "../../../../components/Botoes/BotaoBuscar/BotaoBuscar";
+import BotaoAdicionar from "../../../../components/Botoes/BotaoAdicionar/BotaoAdicionar";
+import BotaoExcluir from "../../../../components/Botoes/BotaoExcluir/BotaoExcluir";
+import ModalDeBusca from "../../../../components/Modals/ModalDeBusca/ModalDeBusca";
 
 const GerenciarTipos = () => {
 
     
 
     return (
-        <FormTipo />
+
+        <div> 
+
+            <Titulo 
+                titulo='Tipos'
+                paragrafo='Tipos > Gerenciar tipos'
+            />
+
+            <div className="button-menu"> 
+                <BotaoBuscar nome="BUSCAR TIPO" />
+                <div className="grouped-buttons">
+                    <BotaoAdicionar />
+                    <BotaoExcluir/>
+                </div>
+            </div>
+
+            <ModalDeBusca  
+                titulo="Buscar membro" 
+                label="Nome do membro"
+                name="name-membro"
+                
+            />
+
+            <div className="global-div"> 
+                <FormTipo />
+            </div>
+
+
+        </div>
     )
 }
 
