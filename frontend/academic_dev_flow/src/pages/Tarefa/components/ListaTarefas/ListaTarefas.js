cd824b3448e532d797ce2b825a4ff410c410529d
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-07 17:30:10 -0300

    criando a estrutura do componente que lista as tarefas #49

--- frontend/academic_dev_flow/src/pages/Tarefa/components/ListaTarefas/ListaTarefas.js
+++ frontend/academic_dev_flow/src/pages/Tarefa/components/ListaTarefas/ListaTarefas.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Table} from 'antd'
+import { listarTarefas } from "../../../../services/tarefaService";
 
 const ListaTarefas = () => {
 
+    const COLUNAS_TABELA_TAREFAS = [
+        {
+            title: 'Nome',
+            dataIndex: 'nome',
+            key: 'nome'
+        },
+        {
+            title: 'Data de Início',
+            dataIndex: 'data_inicio',
+            key: 'data_inicio'
+        },
+        {
+            title: 'Data de Término (Previsão)',
+            dataIndex: 'data_termino',
+            key: 'data_termino'
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            key: 'status'
+        }
+    ]
+
+    const [tarefas, setTarefas] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            await handleListarTarefas()
+        }
+
+        fetchData()
+    }, [])
+
+    const handleListarTarefas = async () => {
+        const response = await listarTarefas()
+        setTarefas(response.data)
+
+    }
+
 
     return (
-        <Table />
+        <Table
+            dataSource={tarefas}
+            columns={COLUNAS_TABELA_TAREFAS}
+        />
     )
 }
 
