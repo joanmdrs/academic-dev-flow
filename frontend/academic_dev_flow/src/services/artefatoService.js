a987dea00f3f82cdd0ef7c8d8fd64958310c942a
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-08 09:50:41 -0300

    atualizando os services de artefato #49

--- frontend/academic_dev_flow/src/services/artefatoService.js
+++ frontend/academic_dev_flow/src/services/artefatoService.js
@@ -1,26 +1,48 @@
 import api from "../api/api" 
+import { ERROR_MESSAGE_ON_CREATION, ERROR_MESSAGE_ON_DELETION, ERROR_MESSAGE_ON_SEARCHING, ERROR_MESSAGE_ON_UPDATE, SUCCESS_MESSAGE_ON_CREATION, SUCCESS_MESSAGE_ON_DELETION, SUCCESS_MESSAGE_ON_UPDATE } from "./messages"
+import { handleError, handleSuccess } from "./utils"
 
 export const criarArtefato = async (dados) => {
-    const resposta = await api.post('/artefato/cadastrar/', dados)
-    return resposta
+    try {
+        const response = await api.post('/artefato/cadastrar/', dados)
+        return handleSuccess(response, SUCCESS_MESSAGE_ON_CREATION)
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_CREATION)
+    }   
 }
 
 export const buscarArtefatoPeloNome =  async (parametro) => {
-    const resposta = await api.get(`/artefato/buscar/?name=${encodeURIComponent(parametro)}`)
-    return resposta
+    try {
+        const response = await api.get('artefato/buscar/', {params: {nome: parametro}})
+        return response
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
+    }
 }
 
-export const atualizarArtefato = async (dados, id) => {
-    const resposta = await api.patch(`/artefato/atualizar/${encodeURIComponent(id)}/`, dados)
-    return resposta
+export const atualizarArtefato = async (id, dados) => {
+    try {
+        const response = await api.patch(`/artefato/atualizar/${encodeURIComponent(id)}/`, dados)
+        return handleSuccess(response, SUCCESS_MESSAGE_ON_UPDATE)
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_UPDATE)
+    }
 }
 
 export const excluirArtefato = async (id) => {
-    const resposta = await api.delete(`/artefato/excluir/${encodeURIComponent(id)}/`)
-    return resposta
+    try {
+        const response = await api.delete(`/artefato/excluir/${encodeURIComponent(id)}/`)
+        return handleSuccess(response, SUCCESS_MESSAGE_ON_DELETION)
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_DELETION)
+    }
 }
 
 export const listarArtefatos = async () => {
-    const resposta = await api.get('/artefato/listar/')
-    return resposta
+    try {
+        const response = await api.get('/artefato/listar/')
+        return response
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
+    }
 }
