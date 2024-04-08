2521fba7edfeeeabd9f4fb0a91da6047d264d274
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-08 09:30:59 -0300

    modificando o model de artefato #49

--- apps/artefato/models.py
+++ apps/artefato/models.py
@@ -1,9 +1,27 @@
 from django.db import models
+from apps.projeto.models import Projeto
+from apps.iteracao.models import Iteracao
 
 class Artefato(models.Model):
     
-    nome = models.CharField(max_length=200)
-    descricao = models.TextField()
+    STATUS_CHOICES = [
+        ('criado', 'Criado'),
+        ('rascunho', 'Em rascunho'),
+        ('revisao', 'Pendente de revisão'),
+        ('aprovado', 'Aprovado'),
+        ('finalizado', 'Finalizado')
+    ]
+    
+    nome = models.CharField(max_length=255) 
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES, null=True, blank=True)
+    data_criacao = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    descricao = models.TextField(null=True, blank=True)
+
+    id_github = models.CharField(null=True, blank=True)   
+    path_github = models.CharField(null=True, blank=True)
+    
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE, null=True, blank=True)
+    iteracao = models.ForeignKey(Iteracao, on_delete=models.SET_NULL, null=True, blank=True)
     
     def __str__(self):
         return self.nome
