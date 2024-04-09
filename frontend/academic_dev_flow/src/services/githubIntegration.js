b724edec2595840236c1cb88f683561c2ee73687
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-09 19:22:44 -0300

    implementando o sevice que busca o conteúdo de um arquivo no repositório #49

--- frontend/academic_dev_flow/src/services/githubIntegration.js
+++ frontend/academic_dev_flow/src/services/githubIntegration.js
@@ -1,5 +1,5 @@
 import api from "../api/api";
-import { ERROR_MESSAGE_ON_CREATION_THE_CONTENT, SUCCESS_MESSAGE_ON_CREATION_THE_CONTENT } from "./messages";
+import { ERROR_MESSAGE_ON_CREATION_THE_CONTENT, ERROR_MESSAGE_ON_SEARCHING, ERROR_MESSAGE_ON_SEARCHING_THE_CONTENT, SUCCESS_MESSAGE_ON_CREATION_THE_CONTENT } from "./messages";
 import { handleError, handleSuccess } from "./utils";
 
 export const createContent = async (dados) => {
@@ -22,6 +22,23 @@ export const createContent = async (dados) => {
             return handleError(error, 'Um arquivo com o mesmo caminho já existe. Escolha outro caminho.');
         } else {
             return handleError(error, ERROR_MESSAGE_ON_CREATION_THE_CONTENT);
+        }
+    }
+}
+
+export const getContent = async (parametros) => {
+    try {
+        const response = await api.get('github_integration/get_content/', {params: {
+            github_token: parametros.github_token,
+            repository: parametros.github_token,
+            path: parametros.path 
+        }})
+        return response
+    } catch (error) {
+        if (error.response && error.response.status === 404){
+            return handleError(error, ERROR_MESSAGE_ON_SEARCHING_THE_CONTENT)
+        } else {
+            return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
         }
     }
 }
