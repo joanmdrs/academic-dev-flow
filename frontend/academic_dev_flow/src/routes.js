2068641aa131d21a7938b77b9b9accc44c8614cc
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-09 19:56:35 -0300

    criando a rota que visualiza um artefato #49

--- frontend/academic_dev_flow/src/routes.js
+++ frontend/academic_dev_flow/src/routes.js
@@ -19,6 +19,7 @@ import Register from "./pages/Auth/Register";
 import PageGerenciarTipos from "./pages/Tipo";
 import ScreenGerenciarTarefas from "./pages/Tarefa/screens/GerenciarTarefas";
 import ScreenGerenciarArtefatos from "./pages/Artefato/screens/GerenciarArtefatos";
+import VisualizarArtefato from "./pages/Artefato/screens/VisualizarArtefato/VisualizarArtefato";
 
 function Routes() {
   return (
@@ -36,6 +37,7 @@ function Routes() {
         <Route path="/admin/membros/gerenciar" Component={ScreenGerenciarMembros} exact />
         <Route path="/admin/membros/vincular-projeto" Component={ScreenVincularMembroAoProjeto} exact/>
         <Route path="/admin/artefatos" Component={ScreenGerenciarArtefatos} exact />
+        <Route path="/admin/artefatos/visualizar-artefato/" Component={VisualizarArtefato} exact/>
         <Route path="/admin/tarefas" Component={ScreenGerenciarTarefas} exact/>
         <Route path="/admin/tipos" Component={PageGerenciarTipos} exact/>
       </Route>
