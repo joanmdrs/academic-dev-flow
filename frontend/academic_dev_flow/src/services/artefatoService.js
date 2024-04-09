9b8bcb9aa81b1e27f54ae8ed78b882a8d64e45d3
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-09 15:47:44 -0300

    implementando o service que filtra os artefatos pelo nome e pelo projeto #49

--- frontend/academic_dev_flow/src/services/artefatoService.js
+++ frontend/academic_dev_flow/src/services/artefatoService.js
@@ -1,6 +1,6 @@
 import api from "../api/api" 
-import { ERROR_MESSAGE_ON_CREATION, ERROR_MESSAGE_ON_DELETION, ERROR_MESSAGE_ON_SEARCHING, ERROR_MESSAGE_ON_UPDATE, SUCCESS_MESSAGE_ON_CREATION, SUCCESS_MESSAGE_ON_DELETION, SUCCESS_MESSAGE_ON_UPDATE } from "./messages"
-import { handleError, handleSuccess } from "./utils"
+import { ERROR_MESSAGE_ON_CREATION, ERROR_MESSAGE_ON_DELETION, ERROR_MESSAGE_ON_SEARCHING, ERROR_MESSAGE_ON_UPDATE, INFO_MESSAGE_MANDATORY_PARAMETERS, INFO_MESSAGE_ON_SEARCHING, SUCCESS_MESSAGE_ON_CREATION, SUCCESS_MESSAGE_ON_DELETION, SUCCESS_MESSAGE_ON_UPDATE } from "./messages"
+import { handleError, handleInfo, handleSuccess } from "./utils"
 
 export const criarArtefato = async (dados) => {
 
@@ -54,5 +54,23 @@ export const listarArtefatos = async () => {
         return response
     } catch (error) {
         return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
+    }
+}
+
+export const filtrarArtefatosPeloNomeEPeloProjeto = async (nomeArtefato, idProjeto) => {
+    try {
+        const response = await api.get('artefato/filtrar/nome-projeto/', {params: {nome_tarefa: nomeArtefato, id_projeto: idProjeto}})
+        if (response.status === 204) {
+            return handleInfo(response, INFO_MESSAGE_ON_SEARCHING)
+        }
+        return response 
+    } catch (error) {
+
+        if (error.response && error.response.status === 400){
+            return handleError(error, INFO_MESSAGE_MANDATORY_PARAMETERS)
+        } else {
+            return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
+        }
+
     }
 }
