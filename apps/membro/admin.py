39b56302eba5401bb0526b61bae710c63aaff032
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-11 14:44:24 -0300

    registrando o model UsuarioGithub no painel admin do django #49

--- apps/membro/admin.py
+++ apps/membro/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 from .models import Membro
+from .models import UsuarioGithub 
 
 class MembroAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'data_nascimento', 'telefone', 'email')
     search_fields = ('nome',)
     
+class UsuarioGithubAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email_github', 'usuario_github')
+    search_fields = ('usuario_github',)
+    
 admin.site.register(Membro, MembroAdmin)
+admin.site.register(UsuarioGithub, UsuarioGithubAdmin)
