b7199e0cee0c33149c82f579d768a886eecca873
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-12 14:13:31 -0300

    definindo a lógica que verifica se os arquivos existem ou não no banco de dados #49

--- frontend/academic_dev_flow/src/pages/Artefato/components/ListaArquivos/ListaArquivos.js
+++ frontend/academic_dev_flow/src/pages/Artefato/components/ListaArquivos/ListaArquivos.js
@@ -1,5 +1,12 @@
-import { Space, Table } from "antd";
-import React from "react";
+import { Button, Space, Table } from "antd";
+import React, { useEffect, useState } from "react";
+import { verificarExistenciaArquivo } from "../../../../services/artefatoService";
+import { handleError } from "../../../../services/utils";
+import { ERROR_MESSAGE_ON_SEARCHING } from "../../../../services/messages";
+import { IoCheckmarkCircle } from "react-icons/io5";
+import { IoCloseCircle } from "react-icons/io5";
+import { IoMdTrash } from "react-icons/io";
+import { FaArrowsRotate } from "react-icons/fa6";
 
 const ListaArquivos = ({dadosArquivos, carregando}) => {
 
@@ -15,19 +22,68 @@ const ListaArquivos = ({dadosArquivos, carregando}) => {
             key: 'path'
         },
         {
+            title: 'Status',
+            dataIndex: 'status',
+            key: 'status',
+            align: 'center',
+            render: (_, record) => (
+                <Space>
+                    { record.existe ? 
+                        <span> <IoCheckmarkCircle color="green" size="15px"/></span>
+                        : <span> <IoCloseCircle color="red"  size="20px" /></span>
+                    }
+                </Space>
+            )
+        },
+        {
             title: 'Ações',
             dataIndex: 'action',
             key: 'action',
             render: (_, record) => (
-                <Space size="middle">
-                    <a>Sicronizar</a>
+                <Space> 
+                    { record.existe ? 
+                        <a> <IoMdTrash /> Excluir </a>
+                        : <a> <FaArrowsRotate /> Sicronizar </a>
+                    }
                 </Space>
             )
         }
     ]
 
+    const [arquivos, setArquivos] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (dadosArquivos){
+                await handleVerificarExistenciaArquivo()
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    const handleVerificarExistenciaArquivo = async () => {
+
+        try {
+            const dados = await Promise.all(dadosArquivos.map(async (arquivo) => {
+                const resArtefato = await verificarExistenciaArquivo(arquivo.sha)
+    
+                if (!resArtefato.error) {
+                    const existe = resArtefato.data.existe
+                    return { ...arquivo, existe }
+                }
+                return arquivo
+            }))
+
+            setArquivos(dados)
+
+        } catch (error) {
+            return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
+        }
+    }
+
     return (    
-        <Table className="style-table" loading={carregando} rowKey="sha" columns={COLUNAS_ARQUIVOS} dataSource={dadosArquivos}/>
+        <Table className="style-table" loading={carregando} rowKey="sha" columns={COLUNAS_ARQUIVOS} dataSource={arquivos}/>
     )
 }
 
