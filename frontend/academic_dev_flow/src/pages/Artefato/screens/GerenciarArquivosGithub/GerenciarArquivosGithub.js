c6163eb981cf3d39427894d7c661b9098b3dc436
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-12 11:00:15 -0300

    criando a lógica funcional da screen gerenciar arquivos do github #49

--- frontend/academic_dev_flow/src/pages/Artefato/screens/GerenciarArquivosGithub/GerenciarArquivosGithub.js
+++ frontend/academic_dev_flow/src/pages/Artefato/screens/GerenciarArquivosGithub/GerenciarArquivosGithub.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Titulo from "../../../../components/Titulo/Titulo";
-import { Button } from "antd";
+import { Button, Spin } from "antd";
 import { FaGithub, FaSearch } from "react-icons/fa";
 import FormListarArquivos from "../../components/FormListarArquivos/FormListarArquivos";
 import { listContents } from "../../../../services/githubIntegration";
+import ListaArquivos from "../../components/ListaArquivos/ListaArquivos";
+import { NotificationManager } from "react-notifications";
 
 const GerenciarArquivosGithub = () => {
 
-    const handleListarArquivos = async (parametros) =>{
-        const response = await listContents(parametros)
+    const [arquivos, setArquivos] = useState([]);
+    const [isFormVisivel, setIsFormVisivel] = useState(true);
+    const [exibirLista, setExibirLista] = useState(false);
+    const [carregando, setCarregando] = useState(false);
 
-        if (!response.error){
-            console.log(response.data)
+    const handleListarArquivos = async (parametros) => {
+        NotificationManager.info("Essa operação pode demorar alguns segundos, aguarde enquanto obtemos os dados.")
+
+        setCarregando(true);
+        setExibirLista(true);
+        const response = await listContents(parametros);
+
+        if (!response.error) {
+            console.log(response.data);
+            setArquivos(response.data);
         }
-    }
+        setCarregando(false)
+    };
 
     return (
         <React.Fragment>
@@ -22,13 +35,30 @@ const GerenciarArquivosGithub = () => {
                 paragrafo='Arquivos > Gerenciar arquivos'
             />
 
-            <div className="global-form"> 
-                <FormListarArquivos onSearch={handleListarArquivos}/>
+            <div className="button-menu">  
+                <Button 
+                    type="primary"
+                    icon={<FaSearch />}
+                    onClick={() => setIsFormVisivel(!isFormVisivel)}
+                >
+                    Filtrar
+                </Button>
             </div>
 
+            { isFormVisivel && (
+                <div className="global-div"> 
+                    <FormListarArquivos onSearch={handleListarArquivos}/>
+                </div>
+            )}
+
+            { exibirLista && (
+                <div className="global-div">
+                    <ListaArquivos dadosArquivos={arquivos} carregando={carregando}/>
+                </div>)
+            } 
 
         </React.Fragment> 
     )
 }
 
-export default GerenciarArquivosGithub
+export default GerenciarArquivosGithub;
