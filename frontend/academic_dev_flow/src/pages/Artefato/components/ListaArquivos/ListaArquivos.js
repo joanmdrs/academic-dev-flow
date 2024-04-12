e456ebb309070a05032aded5ffa2531a9063cde4
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-12 11:00:38 -0300

    definindo o component lista arquivos do github #49

---
+++ frontend/academic_dev_flow/src/pages/Artefato/components/ListaArquivos/ListaArquivos.js
@@ -0,0 +1,34 @@
+import { Space, Table } from "antd";
+import React from "react";
+
+const ListaArquivos = ({dadosArquivos, carregando}) => {
+
+    const COLUNAS_ARQUIVOS = [
+        {
+            title: "Nome arquivo",
+            dataIndex: 'name',
+            key: 'name'
+        },
+        {
+            title: "Path do arquivo",
+            dataIndex: 'path',
+            key: 'path'
+        },
+        {
+            title: 'Ações',
+            dataIndex: 'action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a>Sicronizar</a>
+                </Space>
+            )
+        }
+    ]
+
+    return (    
+        <Table loading={carregando} rowKey="sha" columns={COLUNAS_ARQUIVOS} dataSource={dadosArquivos}/>
+    )
+}
+
+export default ListaArquivos
