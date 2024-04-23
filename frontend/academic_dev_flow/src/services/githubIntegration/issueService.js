4706f770fbf8aafc534bb7c052dd37ebff55cd63
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-23 09:39:38 -0300

    definindo os services que lidam com as views das issues do github #49

---
+++ frontend/academic_dev_flow/src/services/githubIntegration/issueService.js
@@ -0,0 +1,35 @@
+import api from "../../api/api"
+import { handleError, handleSuccess } from "../utils"
+import { 
+    ERROR_MESSAGE_ON_CREATION_THE_ISSUE, 
+    ERROR_MESSAGE_ON_LIST_ISSUES, 
+    ERROR_MESSAGE_ON_UPDATE_THE_ISSUE, 
+    SUCCESS_MESSAGE_ON_CREATION_THE_ISSUE, 
+    SUCCESS_MESSAGE_ON_UPDATE_THE_ISSUE, } from "../messages"
+
+export const createIssue = async (dados) => {
+    try {
+        const response = await api.post('github_integration/issues/create_issue/', dados)
+        return handleSuccess(response, SUCCESS_MESSAGE_ON_CREATION_THE_ISSUE)
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_CREATION_THE_ISSUE)
+    }
+}
+
+export const updateIssue = async (numberIssue, dados) => {
+    try {
+        const response = await api.put(`github_integration/issues/update_issue/${numberIssue}/`, dados)
+        return handleSuccess(response, SUCCESS_MESSAGE_ON_UPDATE_THE_ISSUE)
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_UPDATE_THE_ISSUE)
+    }
+}
+
+export const listIssues = async () => {
+    try {
+        const response = await api.get('github_integration/issues/list_issues/')
+        return response
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_LIST_ISSUES)
+    }
+}
