c71cc44664d8e236741088538606456d4c3b6a6d
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-24 16:00:09 -0300

    criando a screen Gerenciar Labels #49

---
+++ frontend/academic_dev_flow/src/pages/Tarefa/screens/GerenciarLabels/GerenciarLabels.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Titulo from "../../../../components/Titulo/Titulo";
+import { Button } from "antd";
+import { FaPlus, FaSearch } from "react-icons/fa";
+
+const GerenciarLabels = () => {
+
+    return (
+
+        <React.Fragment>
+            <Titulo 
+                titulo='Labels'
+                paragrafo='Labels > Gerenciar labels'
+            />
+        </React.Fragment>
+    )
+}
+
+export default GerenciarLabels
