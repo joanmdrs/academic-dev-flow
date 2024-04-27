6e9e9bd39465a59fd078b9fa42a6010f80c9e95a
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-27 20:56:07 -0300

    implementando uma view que lista todas as iterações #49

--- apps/iteracao/views.py
+++ apps/iteracao/views.py
@@ -119,3 +119,21 @@ class ExcluirIteracaoView(APIView):
 
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+class ListarIteracoesView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        try:
+            
+            iteracoes = Iteracao.objects.all()
+            
+            if iteracoes.exists():
+                serializer = IteracaoSerializer(iteracoes, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            
+            return Response(data=[], status=status.HTTP_204_NO_CONTENT)
+        
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
