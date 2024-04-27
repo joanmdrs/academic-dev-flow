fbdc79e9441ff0622cbad1005740aae0f771ef57
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-04-27 20:57:27 -0300

    refatorando o form de iteração #49

--- frontend/academic_dev_flow/src/pages/Iteracao/components/FormIteracao/FormIteracao.js
+++ frontend/academic_dev_flow/src/pages/Iteracao/components/FormIteracao/FormIteracao.js
@@ -1,45 +1,21 @@
 import { Button, Form, Input, Select } from "antd";
 import React, { useEffect, useState } from "react";
 import { useForm } from "antd/es/form/Form";
-import { useProjetoContext } from "../../../../context/ProjetoContext";
 import { listarEtapasPorFluxo } from "../../../../services/fluxoEtapaService";
 import { buscarEtapaPeloId } from "../../../../services/etapaService";
 import { listarMembrosPorProjeto } from "../../../../services/membroProjetoService";
 import { buscarMembroPeloId } from "../../../../services/membroService";
 import { atualizarIteracao, criarIteracao } from "../../../../services/iteracaoService";
 import Loading from "../../../../components/Loading/Loading";
+import { optionsStatusIteracoes } from "../../../../services/optionsStatus";
+import { useContextoIteracao } from "../../context/contextoIteracao";
 
-const baseStyle = {
-    display: "flex",
-    gap: "20px"
-};
+const FormIteracao = ({onSubmit, onCancel, onReload, additionalFields}) => {
 
-const OPTIONS_STATUS = [
-    {
-        value: 'planejamento',
-        label: 'Em Planejamento'
-    },
-    {
-        value: 'andamento',
-        label: 'Em andamento'
-    },
-    {
-        value: 'concluida',
-        label: 'Concluída'
-    },
-    {
-        value: 'cancelada',
-        label: 'Cancelada'
-    }
-]
-
-const FormIteracao = ({onReload}) => {
-
-    const { dadosProjeto, dadosIteracao } = useProjetoContext();
+    const { dadosProjeto, dadosIteracao } = useContextoIteracao();
     const [form] = useForm();
     const [optionsEtapas, setOptionsEtapas] = useState([]);
     const [optionsMembros, setOptionsMembros] = useState([]);
-    const [loading, setLoading] = useState(true)
     
 
     const handleGetEtapas = async () => {
@@ -86,88 +62,72 @@ const FormIteracao = ({onReload}) => {
                 await handleGetEtapas();
                 await handleGetMembros();
 
-                if (dadosIteracao) {
-                    form.setFieldsValue(dadosIteracao);
-                }
-
-                setLoading(false)
-
+            } else {
+                setOptionsEtapas([])
+                setOptionsMembros([])
             }
-                
         };
 
         fetchData();
     }, [dadosProjeto, dadosIteracao, form]);
-
-    const handleSaveIteracao = async (dados) => {    
-
-        dados['projeto'] = dadosProjeto.id
-        const acaoForm = (dadosIteracao === null) ? 'create' : 'update'
-
-        if (acaoForm === 'create'){
-
-            await criarIteracao(dados)
-        } else if (acaoForm === 'update') {
-            await atualizarIteracao(dadosIteracao.id, dados)
-        }
-        onReload()
-    }
-
-    if(loading) {
-        return <Loading />
-    }
-
     
     return (
-        <div>
-            <Form layout="vertical" className="global-form" form={form} onFinish={handleSaveIteracao}>
+            <Form layout="vertical" className="global-form" form={form} onFinish={onSubmit}>
                 <Form.Item>
                     <h4> CADASTRAR ITERAÇÃO </h4>
                 </Form.Item>
-                <div style={{...baseStyle}}>
-                    <Form.Item label="Nome" name="nome" style={{ flex: "1"}}>
-                        <Input type='text' name="nome" placeholder="Ex.: Iteração 1" />
-                    </Form.Item>
 
-                    <Form.Item label="Número" name="numero" >
-                        <Input type="number" name="numero" />
-                    </Form.Item> 
-
-                    <Form.Item label="Status" name="status" style={{ flex: "1"}}>
-                        <Select options={OPTIONS_STATUS} defaultValue="Selecione"/>
-                    </Form.Item>
-
-                </div>              
-
-                <div style={{...baseStyle}}>
-                    <Form.Item label="Data de Início" name="data_inicio">
-                        <Input type="date" name="data_inicio"/>
-                    </Form.Item>
-
-                    <Form.Item label="Data de Término" name="data_fim">
-                        <Input type="date" name="data_fim"/>
-                    </Form.Item>
-
-                    <Form.Item label="Fase" name="fase" style={{ flex: "1"}}>
-                        <Select options={optionsEtapas} defaultValue="Selecione" />
-                    </Form.Item>
-
-                    <Form.Item label="Gerente" name="gerente" style={{ flex: "1"}}>
-                        <Select options={optionsMembros} defaultValue="Selecione" />
-                    </Form.Item>
-                </div>
-
-                <Form.Item label="Descrição" name="descricao">
-                    <Input.TextArea  rows={4} name="descricao"/>
+                <Form.Item>
+                    {additionalFields}
                 </Form.Item>
 
-                <div style={{...baseStyle}}> 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit"> Salvar </Button>
-                    </Form.Item>
+                <div style={{display: 'flex', gap: '20px'}}> 
+                    <div style={{flex: '2'}}> 
+                        <Form.Item label="Nome" name="nome" style={{ flex: "1"}}>
+                            <Input type='text' name="nome" placeholder="nome" />
+                        </Form.Item>
+
+                        <Form.Item label="Descrição" name="descricao">
+                            <Input.TextArea  rows={6} name="descricao" placeholder="descrição ..."/>
+                        </Form.Item>
+
+                        <div style={{display: 'flex', gap: '10px'}}> 
+                            <Form.Item label="Número" name="numero" >
+                                <Input type="number" name="numero" style={{width: 'fit-content'}} placeholder="número" />
+                            </Form.Item> 
+
+                            <Form.Item label="Data de Início" name="data_inicio">
+                                <Input type="date" name="data_inicio" style={{width: 'fit-content'}}/>
+                            </Form.Item>
+
+                            <Form.Item label="Data de Término" name="data_fim">
+                                <Input type="date" name="data_fim" style={{width: 'fit-content'}}/>
+                            </Form.Item>
+                        </div>
+                    </div>
+                
+                    <div style={{flex: '1'}}> 
+                        <Form.Item label="Status" name="status" style={{ flex: "1"}}>
+                            <Select options={optionsStatusIteracoes} defaultValue="Selecione"/>
+                        </Form.Item>
+                
+                        <Form.Item label="Fase" name="fase" style={{ flex: "1"}}>
+                            <Select options={optionsEtapas} defaultValue="Selecione" />
+                        </Form.Item>
+
+                        <Form.Item label="Líder" name="lider" style={{ flex: "1"}}>
+                            <Select options={optionsMembros} defaultValue="Selecione" />
+                        </Form.Item>
+                    </div>
                 </div>
+                      
+          
+                <Form.Item>
+                    <Button style={{marginRight: '10px'}} type="primary" htmlType="submit"> Salvar </Button>
+                    <Button onClick={onCancel}> Cancelar </Button>
+                </Form.Item>
+                
             </Form>
-        </div>
     )
 }
 
