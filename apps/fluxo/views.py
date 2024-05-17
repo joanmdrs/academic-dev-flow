1f4de8b0c371d6e7ea1f5c5a4f9eed59d0cde3eb
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-05-17 13:57:56 -0300

    refatorando as views de fluxo para permitir que alunos e professores também possam acessar #53

--- apps/fluxo/views.py
+++ apps/fluxo/views.py
@@ -7,68 +7,97 @@ from .models import Fluxo
 from .serializers import FluxoSerializer
 from apps.etapa.views import CadastrarEtapaView 
 from rest_framework.permissions import IsAuthenticated
-from apps.api.permissions import IsAdminUserOrReadOnly 
 
-class BaseFluxoView(APIView):
+class CadastrarFluxoView(APIView):
     permission_classes = [IsAuthenticated]
-
-    def handle_exception(self, exc):
-        return Response({'error': str(exc)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-class CadastrarFluxoView(BaseFluxoView):
     def post(self, request):
-        serializer = FluxoSerializer(data=request.data)
+        try:
+            
+            serializer = FluxoSerializer(data=request.data)
+            
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class BuscarFluxoPeloNomeView(BaseFluxoView):
+class BuscarFluxoPeloNomeView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
-        parametro = request.GET.get('name_fluxo', None)
+        try:
         
-        if parametro is not None:
-            fluxos = Fluxo.objects.filter(nome__icontains=parametro)
-        else: 
-            fluxos = Fluxo.objects.all()
+            parametro = request.GET.get('name_fluxo', None)
             
-        if not fluxos:
-            return Response({'message': 'Nenhum fluxo encontrado', 'results': []}, status=status.HTTP_200_OK)
+            if parametro is not None:
+                fluxos = Fluxo.objects.filter(nome__icontains=parametro)
+            else: 
+                fluxos = Fluxo.objects.all()
+                
+            if not fluxos:
+                return Response({'message': 'Nenhum fluxo encontrado', 'results': []}, status=status.HTTP_200_OK)
+                
+            serializer = FluxoSerializer(fluxos, many=True)
             
-        serializer = FluxoSerializer(fluxos, many=True)
-        return Response({'message': 'Fluxos encontrados com sucesso!', 'results': serializer.data}, status=status.HTTP_200_OK)
+            return Response({'message': 'Fluxos encontrados com sucesso!', 'results': serializer.data}, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class BuscarFluxoPeloIdView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, fluxo_id):
-        fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
-        serializer = FluxoSerializer(fluxo, many=False)
-        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        try:
+            
+            fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
+            serializer = FluxoSerializer(fluxo, many=False)
+            return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class AtualizarFluxoView(BaseFluxoView):
+class AtualizarFluxoView(APIView):
+    permission_classes = [IsAuthenticated]
     def patch(self, request, fluxo_id): 
-        fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
-        serializer = FluxoSerializer(fluxo, data=request.data)
+        try: 
         
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
+            serializer = FluxoSerializer(fluxo, data=request.data)
+            
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class ExcluirFluxoView(BaseFluxoView):
+class ExcluirFluxoView(APIView):
+    permission_classes = [IsAuthenticated]
     def delete(self, request, fluxo_id):
-        fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
+        try:
+            
+            fluxo = get_object_or_404(Fluxo, pk=fluxo_id)
+            
+            if fluxo is not None: 
+                fluxo.delete()
+                return Response({'detail': 'Fluxo excluído com sucesso!'}, status=status.HTTP_204_NO_CONTENT)
+            else: 
+                return Response({'error': 'Objeto não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
         
-        if fluxo is not None: 
-            fluxo.delete()
-            return Response({'detail': 'Fluxo excluído com sucesso!'}, status=status.HTTP_204_NO_CONTENT)
-        else: 
-            return Response({'error': 'Objeto não encontrado!'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-class ListarFluxosView(BaseFluxoView):
+class ListarFluxosView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
-        fluxos = Fluxo.objects.all()
-        serializer = FluxoSerializer(fluxos, many=True) 
-        return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        try:
+            
+            fluxos = Fluxo.objects.all()
+            serializer = FluxoSerializer(fluxos, many=True) 
+            return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
