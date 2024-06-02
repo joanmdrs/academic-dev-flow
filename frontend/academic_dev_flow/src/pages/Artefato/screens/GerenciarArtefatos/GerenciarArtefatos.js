17ab589f7714445f912a5f125075e6eff3807a85
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-06-02 16:21:07 -0300

    finalizando a refatoração da screen gerenciar artefatos #53

--- frontend/academic_dev_flow/src/pages/Artefato/screens/GerenciarArtefatos/GerenciarArtefatos.js
+++ frontend/academic_dev_flow/src/pages/Artefato/screens/GerenciarArtefatos/GerenciarArtefatos.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Button} from 'antd'
+import {Button, Spin} from 'antd'
 import Titulo from "../../../../components/Titulo/Titulo";
 import { FaPlus, FaSearch } from "react-icons/fa";
 import FormGenericBusca from "../../../../components/Forms/FormGenericBusca/FormGenericBusca";
@@ -10,11 +10,24 @@ import SelectProjeto from "../../components/SelectProjeto/SelectProjeto"
 import InputsAdmin from "../../components/InputsAdmin/InputsAdmin"
 import { useContextoArtefato } from "../../context/ContextoArtefato";
 import { createContent, deleteContent } from "../../../../services/githubIntegration";
-import { atualizarArtefato, criarArtefato, filtrarArtefatosPeloNomeEPeloProjeto } from "../../../../services/artefatoService";
+import { atualizarArtefato, criarArtefato, excluirArtefato, filtrarArtefatosPeloNomeEPeloProjeto } from "../../../../services/artefatoService";
 import { buscarProjetoPeloId } from "../../../../services/projetoService";
 import ModalExcluirArtefato from "../../components/ModalExcluirArtefato/ModalExcluirArtefato";
 import { useContextoGlobalProjeto } from "../../../../context/ContextoGlobalProjeto";
 import { buscarUsuarioPeloIdMembroProjeto } from "../../../../services/membroService";
+
+const StyleSpin = {
+    position: 'fixed', 
+    top: 0, 
+    left: 0, 
+    width: '100%', 
+    height: '100%', 
+    backgroundColor: 'rgba(255, 255, 255, 0.5)', 
+    zIndex: 9999, 
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+};
 
 const GerenciarArtefatos = () => {
 
@@ -25,6 +38,7 @@ const GerenciarArtefatos = () => {
     const [artefatoExcluir, setArtefatoExcluir] = useState(null)
     const {dadosArtefato, setDadosArtefato, setArtefatos} = useContextoArtefato()
     const {dadosProjeto, setDadosProjeto} = useContextoGlobalProjeto()
+    const [isSaving, setIsSaving] = useState(false)
 
     const navigate = useNavigate()
 
@@ -35,6 +49,9 @@ const GerenciarArtefatos = () => {
 
     const handleCancelar = () => {  
         setIsFormVisivel(false)
+        setIsSaving(false)
+        setDadosArtefato(null)
+        setDadosProjeto(null)
     }
 
     const handleReload = () => {
@@ -87,15 +104,18 @@ const GerenciarArtefatos = () => {
     }
 
     const handleSalvarArtefato = async (dados) => {
-
-        const resAutor = await handleBuscarAutor(dados.membro)
+        setIsSaving(true)
+        if  (dados.membro !== undefined){
+            const resAutor = await handleBuscarAutor(dados.membro)
+            dados['author_name'] = resAutor.data.nome
+            dados['author_email'] = resAutor.data.email_github
+        }
+        
 
         dados['projeto'] = dadosProjeto.id
         dados['github_token'] = dadosProjeto.token
         dados['repository'] = dadosProjeto.nome_repo
         dados['content'] = dados.descricao
-        dados['author_name'] = resAutor.data.nome
-        dados['author_email'] = resAutor.data.email_github
 
         if (acaoForm === 'criar') {
             const response = await createContent(dados)
@@ -105,9 +125,11 @@ const GerenciarArtefatos = () => {
                 handleReload()
             } 
         } else if (acaoForm === 'atualizar'){
+            console.log(dados)
             await atualizarArtefato(dadosArtefato.id, dados)
             handleReload()
         }
+        setIsSaving(false)
     }
 
     const handleBuscarArtefato = async (dados) => {
@@ -134,6 +156,7 @@ const GerenciarArtefatos = () => {
         const resProjeto = await buscarProjetoPeloId(record.projeto)
         const projeto = resProjeto.data
         const parametros = {
+            id_artefato: record.id,
             github_token: projeto.token,
             repository: projeto.nome_repo,
             path: record.path_file
@@ -142,9 +165,17 @@ const GerenciarArtefatos = () => {
     }
 
     const handleExcluirArtefato = async (parametro) => {
+        handleFecharModal()
+        setIsSaving(true)
         const objeto = artefatoExcluir
         objeto['commit_message'] = parametro
-        await deleteContent(objeto)
+
+        const response = await deleteContent(objeto)
+        if(!response.error){
+            await excluirArtefato(objeto.id_artefato)
+        }
+        handleReload()
+        setIsSaving(false)
     }
 
     const handleAtualizarStatusArtefato = async (record, value) => {
@@ -192,25 +223,49 @@ const GerenciarArtefatos = () => {
 
                 {isFormVisivel && acaoForm === 'criar' && (
                     <React.Fragment> 
+                        {isSaving && ( 
+                            <div style={StyleSpin}>
+                                <Spin size="large" />
+                            </div>
+                        )}
                         <FormArtefato 
                             onSubmit={handleSalvarArtefato} 
                             onCancel={handleCancelar}
                             selectProjeto={<SelectProjeto />} 
-                            inputsAdmin={<InputsAdmin/>} /> 
+                            inputsAdmin={<InputsAdmin/>} 
+                        /> 
                     </React.Fragment>
                 )}
 
                 {isFormVisivel && acaoForm === 'atualizar' && (
-                    <FormArtefato onSubmit={handleSalvarArtefato} onCancel={handleCancelar} />
+                    <React.Fragment> 
+                        {isSaving && ( 
+                            <div style={StyleSpin}>
+                                <Spin size="large" />
+                            </div>
+                        )}
+                        <FormArtefato 
+                            onSubmit={handleSalvarArtefato} 
+                            onCancel={handleCancelar} 
+                        />
+                    </React.Fragment>
                 )}
+                
 
-                {!isFormVisivel  && (
-                    <ListaArtefatos    
-                        onView={handleVisualizarArtefato} 
-                        onEdit={handleAtualizarArtefato} 
-                        onDelete={handlePrepararParaExcluirArtefato}
-                        onUpdateStatus={handleAtualizarStatusArtefato}
-                    />
+                {!isFormVisivel && (
+                    <React.Fragment>
+                        {isSaving && ( 
+                            <div style={StyleSpin}>
+                                <Spin size="large" />
+                            </div>
+                        )}
+                        <ListaArtefatos    
+                            onView={handleVisualizarArtefato} 
+                            onEdit={handleAtualizarArtefato} 
+                            onDelete={handlePrepararParaExcluirArtefato}
+                            onUpdateStatus={handleAtualizarStatusArtefato}
+                        />
+                    </React.Fragment>
                 )}
                 
             </div>
