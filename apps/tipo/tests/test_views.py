5982cc9bacc05a975ffbcb35f3aa1d060c883234
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-08-17 22:18:47 -0300

    implementando os testes do model tipo #61

--- apps/tipo/tests/test_views.py
+++ apps/tipo/tests/test_views.py
@@ -3,11 +3,10 @@ from rest_framework import status
 from django.urls import reverse
 from apps.tipo.models import Tipo
 from apps.tipo.serializers import TipoSerializer
-from apps.usuario.models import Usuario  # Importar o modelo de usuário personalizado
+from apps.usuario.models import Usuario  
 
 class TipoTests(APITestCase):
     def setUp(self):
-        # Criação de um usuário autenticado para os testes usando o modelo personalizado
         self.user = Usuario.objects.create_user(username='admin', password='password123')
         self.client.force_authenticate(user=self.user)
 
@@ -22,14 +21,12 @@ class TipoTests(APITestCase):
     def test_cadastrar_tipo(self):
         data = {'nome': 'Novo Tipo', 'cor': '#000000'}
         response = self.client.post(self.url_cadastrar, data, format='json')
-        print(response.data)  # Adicionado para depuração
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['nome'], 'Novo Tipo')
         self.assertEqual(response.data['cor'], '#000000')
 
     def test_buscar_tipo_pelo_nome(self):
         response = self.client.get(self.url_buscar, {'nome': 'Tipo Teste'})
-        print(response.data)  # Adicionado para depuração
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['nome'], 'Tipo Teste')
@@ -37,32 +34,27 @@ class TipoTests(APITestCase):
     def test_atualizar_tipo(self):
         data = {'nome': 'Tipo Atualizado'}
         response = self.client.patch(self.url_atualizar, data, format='json')
-        print(response.data)  # Adicionado para depuração
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['nome'], 'Tipo Atualizado')
 
     def test_excluir_tipo(self):
         response = self.client.delete(self.url_excluir)
-        print(response.data)  # Adicionado para depuração
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(Tipo.objects.filter(id=self.tipo.id).exists())
 
     def test_listar_tipos(self):
         response = self.client.get(self.url_listar)
-        print(response.data)  # Adicionado para depuração
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)  # Verifica se há pelo menos um Tipo na resposta
+        self.assertEqual(len(response.data), 1) 
         self.assertEqual(response.data[0]['nome'], 'Tipo Teste')
         
     def test_atualizar_tipo_inexistente(self):
         url_atualizar_inexistente = reverse('tipo:atualizar_tipo', kwargs={'id': 9999})
         data = {'nome': 'Tipo Inexistente'}
         response = self.client.patch(url_atualizar_inexistente, data, format='json')
-        print(response.data)  # Adicionado para depuração
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_excluir_tipo_inexistente(self):
         url_excluir_inexistente = reverse('tipo:excluir_tipo', kwargs={'id': 9999})
         response = self.client.delete(url_excluir_inexistente)
-        print(response.data)  # Adicionado para depuração
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
