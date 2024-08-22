a4a2120e2029cf5a5b84b3ca03801e7444d4fc3a
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-08-22 14:30:21 -0300

    adicionando o contexto na chamada a API nos services de projeto

--- frontend/academic_dev_flow/src/services/projetoService.js
+++ frontend/academic_dev_flow/src/services/projetoService.js
@@ -4,43 +4,43 @@ import { handleError, handleInfo, handleSuccess } from "./utils";
 
 export const criarProjeto = (dados) => {
     
-    const resposta  = api.post("/projeto/cadastrar/", dados)
+    const resposta  = api.post("/academicflow-api/projeto/cadastrar/", dados)
     return resposta;
 }
 
 export const buscarProjetoPeloNome = async (parametro) => {
-    const resposta = await api.get(`/projeto/buscar/?name=${encodeURIComponent(parametro)}`);
+    const resposta = await api.get(`/academicflow-api/projeto/buscar/?name=${encodeURIComponent(parametro)}`);
     return resposta;
 };
 
 export const buscarProjetoPeloId = async (idProjeto) => {
-    const resposta = await api.get(`/projeto/buscar_por_id/${encodeURIComponent(idProjeto)}/`);
+    const resposta = await api.get(`/academicflow-api/projeto/buscar_por_id/${encodeURIComponent(idProjeto)}/`);
     return resposta
 }
 
 export const buscarProjetosPorListaIds = async (listaIds) => {
-    const response = await api.get('/projeto/buscar_por_lista_ids/', {params: {ids: listaIds}});
+    const response = await api.get('/academicflow-api/projeto/buscar_por_lista_ids/', {params: {ids: listaIds}});
     return response;
 }
 
 export const excluirProjeto = (id) => {
-    const resposta = api.delete(`/projeto/excluir/${encodeURIComponent(id)}/`);
+    const resposta = api.delete(`/academicflow-api/projeto/excluir/${encodeURIComponent(id)}/`);
     return resposta;
 }
 
 export const atualizarProjeto = (dados, id) => {
-    const resposta = api.patch(`/projeto/atualizar/${encodeURIComponent(id)}/`, dados);
+    const resposta = api.patch(`/academicflow-api/projeto/atualizar/${encodeURIComponent(id)}/`, dados);
     return resposta
 }
 
 export const atualizarFluxoProjeto = (idFluxo, idProjeto ) => {
-    const resposta = api.patch(`/projeto/atualizar/fluxo/${encodeURIComponent(idProjeto)}/`, { fluxo_id: idFluxo})
+    const resposta = api.patch(`/academicflow-api/projeto/atualizar/fluxo/${encodeURIComponent(idProjeto)}/`, { fluxo_id: idFluxo})
     return resposta
 }
 
 export const listarProjetos = async () => {
     try {
-        const response = await api.get('projeto/listar/')
+        const response = await api.get('/academicflow-api/projeto/listar/')
         if (response.status === 204) {
             return handleInfo(response, INFO_MESSAGE_ON_SEARCHING)
         }
