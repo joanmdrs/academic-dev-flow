1ed6629d79a0a650e9ecdb200ddc825072a9b398
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-08-22 14:11:00 -0300

    ajustando algumas configurações #61

--- frontend/academic_dev_flow/src/index.js
+++ frontend/academic_dev_flow/src/index.js
@@ -9,7 +9,7 @@ import 'react-notifications/lib/notifications.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter bas>
       <App />
     </BrowserRouter>
   </React.StrictMode>
