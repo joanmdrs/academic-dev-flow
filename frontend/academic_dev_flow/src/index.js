d56db361650fffbd8b4a491fc359165891c4a455
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-09-17 19:02:33 -0300

    corrigindo a basename da url

--- frontend/academic_dev_flow/src/index.js
+++ frontend/academic_dev_flow/src/index.js
@@ -9,7 +9,7 @@ import 'react-notifications/lib/notifications.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/academicflow'>
+    <Router basename='/academic-dev-flow'>
       <App />
     </Router>
   </React.StrictMode>
