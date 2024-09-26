33c22ba0d7edde5cdb3d3c78813c5a1e119e6633
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-09-26 16:09:58 -0300

    implementando a view que atualiza uma categoria_funcao_membro

--- apps/funcao_membro/urls.py
+++ apps/funcao_membro/urls.py
@@ -5,6 +5,8 @@ app_name = 'funcao-membro'
 
 urlpatterns = [
     path('categoria/cadastrar/', CadastrarCategoriaFuncaoMembroView.as_view(), name='cadastrar_categoria_funcao_membro'),
+    path('categoria/atualizar/', AtualizarCategoriaFuncaoMembroView.as_view(), name='atualizar_categoria_funcao_membro'),
+    
     
 ]
 
--- apps/funcao_membro/views.py
+++ apps/funcao_membro/views.py
@@ -16,7 +16,6 @@ from django.core.exceptions import ObjectDoesNotExist
 class CadastrarCategoriaFuncaoMembroView(APIView):
     permission_classes = [IsAuthenticated]
     def post(self, request):
-        
         try:
             categoria_funcao_membro = request.data.get('categoria_funcao_membro', None)
             
@@ -28,4 +27,34 @@ class CadastrarCategoriaFuncaoMembroView(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
             
         except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class AtualizarCategoriaFuncaoMembroView(APIView): 
+    permission_classes = [IsAuthenticated]
+    def patch(self, request): 
+        try:
+            id_categoria = request.GET.get('id_categoria', None)
+            data_categoria = request.data.get('data_categoria', None)
+            
+            if id_categoria is None: 
+                return Response({'error': 'ID da categoria não fornecido'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            if data_categoria is None: 
+                return Response({'error': 'Os dados atualizados da categoria não foram fornecidos'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            obj_categoria = CategoriaFuncaoMembro.objects.get(id=id_categoria)
+            
+            categoria_serializer = CategoriaFuncaoMembroSerializer(obj_categoria, data=data_categoria, partial=True)
+            
+            if not categoria_serializer.is_valid(): 
+                return Response({'error': 'Dados da categoria são inválidos!'}, status=status.HTTP_400_BAD_REQUEST)
+            
+            categoria_serializer.save()
+            
+            return Response(categoria_serializer.data, status=status.HTTP_200_OK)
+        
+        except CategoriaFuncaoMembro.DoesNotExist:
+            return Response({'error': 'Objeto CategoriaFuncaoMembro não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Exception as e: 
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
