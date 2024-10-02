a15ecf6ecbdc0b3d05da547b0227b0c97ce2a13d
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-10-02 09:51:15 -0300

    refatorando a view que para a contagem do tempo da tarefa

--- apps/tarefa/views_intervalo_tempo.py
+++ apps/tarefa/views_intervalo_tempo.py
@@ -46,18 +46,27 @@ class PararContagemTempoView(APIView):
 
     def post(self, request):
         try:
+            id_membro_projeto = request.data.get('id_membro_projeto')
+            id_tarefa = request.data.get('id_tarefa')
             
-            membro_projeto = get_object_or_404(MembroProjeto, pk=request.data.get('membro_projeto_id'))
-            tarefa = get_object_or_404(Tarefa, pk=request.data.get('tarefa_id'))
+            if not id_membro_projeto or not id_tarefa:
+                return Response(
+                    {'error': 'O ID da Tarefa ou o ID do MembroProjeto não foram fornecidos'}, 
+                    status=status.HTTP_400_BAD_REQUEST)
+                
+            membro_projeto = get_object_or_404(MembroProjeto, id=id_membro_projeto)
+            tarefa = get_object_or_404(Tarefa, id=id_tarefa)
             
-            intervalo = tarefa.parar_contagem_tempo(membro_projeto=membro_projeto)
+            if tarefa and membro_projeto:
+                intervalo = tarefa.parar_contagem_tempo(membro_projeto=membro_projeto)
+                if intervalo:
+                    serializer = IntervaloTempoSerializer(intervalo, many=False)
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response({'error': 'Não foi possível parar a contagem de tempo.'}, status=status.HTTP_400_BAD_REQUEST)
             
-            if intervalo:
-                serializer = IntervaloTempoSerializer(intervalo, many=False)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response({'error': 'Não foi possível parar a contagem de tempo.'}, status=status.HTTP_400_BAD_REQUEST)
-    
-
-        
+        except MembroProjeto.DoesNotExist():
+            return Response({'error': 'MembroProjeto não localizado'}, status=status.HTTP_404_NOT_FOUND)
+        except Tarefa.DoesNotExist():
+            return Response({'error': 'Tarefa não localizada'}, status=status.HTTP_404_NOT_FOUND)       
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
