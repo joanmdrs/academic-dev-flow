55f96cce7c4e255aefc921b67e8a245866f430d4
Joan Medeiros <joanmedeiros2018@gmail.com>
2024-11-21 11:18:32 -0300

    corrigindo endpoint

--- frontend/academic_dev_flow/src/index.js
+++ frontend/academic_dev_flow/src/index.js
@@ -9,7 +9,7 @@ import 'react-notifications/lib/notifications.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/academic-dev-flow'>
+    <Router basename='/academicflow'>
       <App />
     </Router>
   </React.StrictMode>
