281601cd62e15ed84957a401f3e77011377e37f6
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-01-09 21:36:55 -0300

    adicionando o swagger a api #65

--- academic_dev_flow/urls.py
+++ academic_dev_flow/urls.py
@@ -17,9 +17,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Minha API",
+        default_version='v1',
+        description="Documentação da API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('academicflow-api/admin/', admin.site.urls),
+    path('academicflow-api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('academicflow-api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('academicflow-api/projeto/', include('apps.projeto.urls', namespace='projeto')),
     path('academicflow-api/fluxo/', include('apps.fluxo.urls', namespace='fluxo')),
     path('academicflow-api/etapa/', include('apps.etapa.urls', namespace='etapa')),
