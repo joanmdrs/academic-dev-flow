47ed52572ce171cd3d722377a3516accfed7f661
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-04-28 21:19:40 -0300

    construindo a lógica de funcionamento da screen Chat #72

--- frontend/academic_dev_flow/src/pages/Chat/screens/PainelChat/PainelChat.js
+++ frontend/academic_dev_flow/src/pages/Chat/screens/PainelChat/PainelChat.js
@@ -1,34 +1,93 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./PainelChat.css"
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 import { FaPlus } from "react-icons/fa6";
 import FormChat from "../../components/FormChat/FormChat";
 import SelectProject from "../../../../components/SelectProject/SelectProject";
 import FormChatModal from "../../components/FormChat/FormChat";
 import { useContextoGlobalUser } from "../../../../context/ContextoGlobalUser/ContextoGlobalUser";
 import RenderEmpty from "../../../../components/Empty/Empty";
-import { atualizarChat, cadastrarChat } from "../../../../services/chatService";
+import { atualizarChat, buscarChatsDoUsuario, cadastrarChat, excluirChat } from "../../../../services/chatService";
 import { useContextoChat } from "../../context/ContextoChat";
+import ListChats from "../../components/ListChats/ListChats";
+import { useContextoGlobalProjeto } from "../../../../context/ContextoGlobalProjeto/ContextoGlobalProjeto";
+import SpinLoading from "../../../../components/SpinLoading/SpinLoading";
 
 const PainelChat = () => {
 
     const {usuario} = useContextoGlobalUser()
-    const {dadosChat} = useContextoChat()
+    const {dadosChat, setDadosChat} = useContextoChat()
+    const {dadosProjeto, setDadosProjeto} = useContextoGlobalProjeto()
     const [isFormVisible, setIsFormVisible] = useState(false)
-
     const [actionForm, setActionForm] = useState('create')
+    const [chats, setChats] = useState([])
+    const [selectedChat, setSelectedChat] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
+    const handleBuscarChats = async () => {
+        const response = await buscarChatsDoUsuario(usuario.id)
+        if(!response.error){
+            setChats(response.data)
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (usuario.id){
+                await handleBuscarChats()
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    const handleReload = async () => {
+        await handleBuscarChats()
+        setIsFormVisible(false)
+        setSelectedChat(null)
+        setDadosProjeto(null)
+        setDadosChat(null)
+        setActionForm('create')
+    }
+    
     const handleAdicionarChat = () => {
         setIsFormVisible(true)
+        setSelectedChat(null)
+        setDadosProjeto(null)
+        setDadosChat(null)
+        setActionForm('create')
+    }
+
+    const handleEditarChat = (record) => {
+        setIsFormVisible(true)
+        setActionForm('update')
+        setDadosChat(record)
+        setSelectedChat(null)
     }
 
     const handleSalvarChat = async (data) => {
+        console.log(data)
+        console.log(dadosChat)
 
         if (actionForm === 'create'){
             await cadastrarChat(data)
         } else if (actionForm === 'update'){
             await atualizarChat(dadosChat.id, data)
         }
+        await handleReload()
+    }
+
+    const handleExcluirChat = async (idChat) => {
+        Modal.confirm({
+            title: 'Confirmar exclusão',
+            content: 'Você está seguro de que deseja excluir o chat ? Esta ação implicará na exclusão de todas as mensagens relacionados ao chat. ',
+            okText: 'Sim',
+            cancelText: 'Não',
+            onOk: async () => {
+                await excluirChat(idChat)
+                await handleReload()
+            }
+        });
     }
 
     const handleCloseModal = () => setIsFormVisible(false)
@@ -36,27 +95,44 @@ const PainelChat = () => {
 
     return (
         <div className="painel-chat"> 
-            <div className="coluna-chat"> 
+            {isLoading ? (
+                <SpinLoading />
+            ) : (
+                <React.Fragment>
+                    <div className="coluna-chat"> 
+                        <Button type="primary" icon={<FaPlus />} onClick={handleAdicionarChat}> 
+                            Criar Chat
+                        </Button> 
 
-                <Button type="primary" icon={<FaPlus />} onClick={handleAdicionarChat}> 
-                    Criar Chat
-                </Button>
-
-                    
-            </div>
+                        <ListChats data={chats} onEdit={handleEditarChat} onDelete={handleExcluirChat} />    
+                    </div>
 
 
-            <div className="coluna-mensagens"> 
-                <RenderEmpty title="Você ainda não possui nenhum Chat" />
-            </div>
+                    <div className="coluna-mensagens"> 
+                        { chats.length === 0 && (
+                            <RenderEmpty title="Você não possui nenhum chat criado." />
+                        )}
 
-            <FormChatModal 
-                visible={isFormVisible}
-                action={actionForm}
-                onOk={handleSalvarChat}
-                onCancel={handleCloseModal}
-                selectProject={<SelectProject idMembro={usuario.id} />}
-            />
+                        {chats.length !== 0 && selectedChat === null ? (
+                            <RenderEmpty title="Selecione um chat para exibir as mensagens" />
+                        ) : (
+                            <div>
+                                Suas Mensagens
+                            </div>
+                        )}
+
+                    </div>
+
+                    <FormChatModal 
+                        visible={isFormVisible}
+                        action={actionForm}
+                        onOk={handleSalvarChat}
+                        onCancel={handleCloseModal}
+                        selectProject={<SelectProject idMembro={usuario.id} />}
+                    />
+                </React.Fragment>
+            )}
+            
 
         </div>
     )
