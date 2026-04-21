c6ac4a57100d4d7e49fc26ff1efabcbcff65967f
Joan Medeiros <joanmedeiros2018@gmail.com>
2026-04-21 17:18:05 -0300

    feat: adicionando o grupo de usuário ao painel administrativo

--- apps/usuario/admin.py
+++ apps/usuario/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from .models import Usuario 
 
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username')
+    list_display = ('id', 'username', 'get_groups')
+    
+    def get_groups(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+    
+    get_groups.short_description = 'Grupos'
 
 
 admin.site.register(Usuario, UsuarioAdmin)
