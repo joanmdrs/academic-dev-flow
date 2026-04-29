03047eb1bed89b7afa6c06917ed3365f5352c156
Joan Medeiros <joanmedeiros2018@gmail.com>
2026-04-28 21:13:17 -0300

    feat: criando uma view que permite realizar exclusão em massa

--- apps/fluxo_etapa/views.py
+++ apps/fluxo_etapa/views.py
@@ -8,6 +8,7 @@ from .models import FluxoEtapa, TransicaoFluxo
 from .serializers import FluxoEtapaSerializer, TransicaoFluxoSerializer
 from rest_framework.permissions import IsAuthenticated
 from apps.api.permissions import IsAdminUserOrReadOnly 
+from rest_framework.decorators import action
 
 class FluxoEtapaViewSet(viewsets.ModelViewSet):
     serializer_class = FluxoEtapaSerializer
@@ -25,6 +26,31 @@ class FluxoEtapaViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+        
+    @action(detail=False, methods=['delete'], url_path='bulk-delete')
+    def bulk_delete(self, request):
+        ids = request.data.get('ids', [])
+
+        if not ids:
+            return Response(
+                {'error': 'IDs não fornecidos'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        queryset = FluxoEtapa.objects.filter(id__in=ids)
+
+        if not queryset.exists():
+            return Response(
+                {'error': 'Itens não encontrados'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        queryset.delete()
+
+        return Response(
+            {'detail': 'Itens excluídos com sucesso'},
+            status=status.HTTP_204_NO_CONTENT
+        )
         
     
 class TransicaoFluxoViewSet(viewsets.ModelViewSet):
