1a54d8a2b5efa9aa5df3341a3055d9e2a7f872e4
Joan Medeiros <joanmedeiros2018@gmail.com>
2026-04-28 21:16:12 -0300

    feat: criando o service de transicoes

--- frontend/academic_dev_flow/src/services/fluxoEtapaService.js
+++ frontend/academic_dev_flow/src/services/fluxoEtapaService.js
@@ -58,3 +58,48 @@ export const desvincularEtapaFluxo = async (ids) => {
         return handleError(error, 'Erro ao excluir itens.')
     }
 }
+
+export const criarTransicao = async (data) => {
+    try {
+        const response = await api.post("transicoes/", data)
+        return handleSuccess(response, "Transição criada com sucesso!")
+    } catch (error) {
+        return handleError(error, "Erro ao criar transição.")
+    }
+}
+
+export const atualizarTransicao = async (id, data) => {
+    try {
+        const response = await api.patch(`transicoes/${id}/`, data)
+        return handleSuccess(response, "Transição atualizada com sucesso!")
+    } catch (error) {
+        return handleError(error, "Erro ao atualizar transição.")
+    }
+}
+
+export const listarTransicoes = async () => {
+    try {
+        const response = await api.get("transicoes/")
+        return response
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
+    }
+}
+
+export const listarTransicoesPorFluxo = async (idFluxo) => {
+    try {
+        const response = await api.get(`transicoes/?id_fluxo=${idFluxo}`)
+        return response
+    } catch (error) {
+        return handleError(error, ERROR_MESSAGE_ON_SEARCHING)
+    }
+}
+
+export const excluirTransicao = async (id) => {
+    try {
+        const response = await api.delete(`transicoes/${id}/`)
+        return handleSuccess(response, "Transição excluída com sucesso!")
+    } catch (error) {
+        return handleError(error, "Erro ao excluir transição.")
+    }
+}
